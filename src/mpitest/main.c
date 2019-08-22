4c37bbce8b8c573118758932ebeaee33b671dafe
jpekkila <johannes.pekkila@aalto.fi>
2019-08-22 14:46:04 +0300

    Basic MPI communication working

--- src/mpitest/main.c
+++ src/mpitest/main.c
@@ -27,57 +27,57 @@
 
 #include "astaroth.h"
 #include "autotest.h"
+#include "renderer.h"
 
 #include <mpi.h>
 
 // From Astaroth Standalone
 #include "config_loader.h"
 #include "model/host_memory.h"
+#include "model/model_boundconds.h"
 
 static void
 distribute_mesh(const AcMesh* src, AcMesh* dst)
 {
+    MPI_Barrier(MPI_COMM_WORLD);
+
     MPI_Datatype datatype = MPI_FLOAT;
     if (sizeof(AcReal) == 8)
         datatype = MPI_DOUBLE;
 
-    int process_id, num_processes;
-    MPI_Comm_rank(MPI_COMM_WORLD, &process_id);
+    int pid, num_processes;
+    MPI_Comm_rank(MPI_COMM_WORLD, &pid);
     MPI_Comm_size(MPI_COMM_WORLD, &num_processes);
 
     const size_t count = acVertexBufferSize(dst->info);
     for (int i = 0; i < NUM_VTXBUF_HANDLES; ++i) {
 
-        // Communicate to self
-        if (process_id == 0) {
+        if (pid == 0) {
+            // Communicate to self
             assert(src);
             assert(dst);
             memcpy(&dst->vertex_buffer[i][0], //
                    &src->vertex_buffer[i][0], //
                    count * sizeof(src->vertex_buffer[i][0]));
-        }
-        // Communicate to others
-        for (int j = 1; j < num_processes; ++j) {
-            if (process_id == 0) {
+
+            // Communicate to others
+            for (int j = 1; j < num_processes; ++j) {
                 assert(src);
 
-                // Send
-                // TODO RECHECK THESE j INDICES
-                const size_t src_idx = j * dst->info.int_params[AC_mx] *
-                                       dst->info.int_params[AC_my] * src->info.int_params[AC_nz] /
-                                       num_processes;
+                const size_t src_idx = acVertexBufferIdx(
+                    0, 0, j * src->info.int_params[AC_nz] / num_processes, src->info);
 
                 MPI_Send(&src->vertex_buffer[i][src_idx], count, datatype, j, 0, MPI_COMM_WORLD);
             }
-            else {
-                assert(dst);
+        }
+        else {
+            assert(dst);
 
-                // Recv
-                const size_t dst_idx = 0;
-                MPI_Status status;
-                MPI_Recv(&dst->vertex_buffer[i][dst_idx], count, datatype, 0, 0, MPI_COMM_WORLD,
-                         &status);
-            }
+            // Recv
+            const size_t dst_idx = 0;
+            MPI_Status status;
+            MPI_Recv(&dst->vertex_buffer[i][dst_idx], count, datatype, 0, 0, MPI_COMM_WORLD,
+                     &status);
         }
     }
 }
@@ -85,47 +85,100 @@ distribute_mesh(const AcMesh* src, AcMesh* dst)
 static void
 gather_mesh(const AcMesh* src, AcMesh* dst)
 {
+    MPI_Barrier(MPI_COMM_WORLD);
+    printf("Gathering mesh...\n");
     MPI_Datatype datatype = MPI_FLOAT;
     if (sizeof(AcReal) == 8)
         datatype = MPI_DOUBLE;
 
-    int process_id, num_processes;
-    MPI_Comm_rank(MPI_COMM_WORLD, &process_id);
+    int pid, num_processes;
+    MPI_Comm_rank(MPI_COMM_WORLD, &pid);
     MPI_Comm_size(MPI_COMM_WORLD, &num_processes);
 
     size_t count = acVertexBufferSize(src->info);
 
     for (int i = 0; i < NUM_VTXBUF_HANDLES; ++i) {
         // Communicate to self
-        if (process_id == 0) {
+        if (pid == 0) {
             assert(src);
             assert(dst);
             memcpy(&dst->vertex_buffer[i][0], //
                    &src->vertex_buffer[i][0], //
-                   count * sizeof(AcReal));
-        }
+                   count * sizeof(src->vertex_buffer[i][0]));
 
-        // Communicate to others
-        for (int j = 1; j < num_processes; ++j) {
-            if (process_id == 0) {
+            for (int j = 1; j < num_processes; ++j) {
                 // Recv
-                // const size_t dst_idx = j * acVertexBufferCompdomainSize(dst->info);
-                const size_t dst_idx = j * dst->info.int_params[AC_mx] *
-                                       dst->info.int_params[AC_my] * dst->info.int_params[AC_nz] /
-                                       num_processes;
+                const size_t dst_idx = acVertexBufferIdx(
+                    0, 0, j * dst->info.int_params[AC_nz] / num_processes, dst->info);
 
                 assert(dst_idx + count <= acVertexBufferSize(dst->info));
                 MPI_Status status;
                 MPI_Recv(&dst->vertex_buffer[i][dst_idx], count, datatype, j, 0, MPI_COMM_WORLD,
                          &status);
             }
-            else {
-                // Send
-                const size_t src_idx = 0;
+        }
+        else {
+            // Send
+            const size_t src_idx = 0;
 
-                assert(src_idx + count <= acVertexBufferSize(src->info));
-                MPI_Send(&src->vertex_buffer[i][src_idx], count, datatype, 0, 0, MPI_COMM_WORLD);
-            }
+            assert(src_idx + count <= acVertexBufferSize(src->info));
+            MPI_Send(&src->vertex_buffer[i][src_idx], count, datatype, 0, 0, MPI_COMM_WORLD);
+        }
+    }
+}
+
+static void
+communicate_halos(AcMesh* submesh)
+{
+    MPI_Barrier(MPI_COMM_WORLD);
+    printf("Communicating bounds...\n");
+    MPI_Datatype datatype = MPI_FLOAT;
+    if (sizeof(AcReal) == 8)
+        datatype = MPI_DOUBLE;
+
+    int pid, num_processes;
+    MPI_Comm_rank(MPI_COMM_WORLD, &pid);
+    MPI_Comm_size(MPI_COMM_WORLD, &num_processes);
+
+    const size_t count = submesh->info.int_params[AC_mx] * submesh->info.int_params[AC_my] * NGHOST;
+
+    for (int i = 0; i < NUM_VTXBUF_HANDLES; ++i) {
+        { // Front
+            // ...|ooooxxx|... -> xxx|ooooooo|...
+            const size_t src_idx = acVertexBufferIdx(0, 0, submesh->info.int_params[AC_nz],
+                                                     submesh->info);
+            const size_t dst_idx = acVertexBufferIdx(0, 0, 0, submesh->info);
+            const int send_pid   = (pid + 1) % num_processes;
+            const int recv_pid   = (pid + num_processes - 1) % num_processes;
+
+            MPI_Request request;
+            MPI_Isend(&submesh->vertex_buffer[i][src_idx], count, datatype, send_pid, i,
+                      MPI_COMM_WORLD, &request);
+            fflush(stdout);
+
+            MPI_Status status;
+            MPI_Recv(&submesh->vertex_buffer[i][dst_idx], count, datatype, recv_pid, i,
+                     MPI_COMM_WORLD, &status);
+
+            MPI_Wait(&request, &status);
+        }
+        { // Back
+            // ...|ooooooo|xxx <- ...|xxxoooo|...
+            const size_t src_idx = acVertexBufferIdx(0, 0, NGHOST, submesh->info);
+            const size_t dst_idx = acVertexBufferIdx(0, 0, NGHOST + submesh->info.int_params[AC_nz],
+                                                     submesh->info);
+            const int send_pid   = (pid + num_processes - 1) % num_processes;
+            const int recv_pid   = (pid + 1) % num_processes;
+
+            MPI_Request request;
+            MPI_Isend(&submesh->vertex_buffer[i][src_idx], count, datatype, send_pid,
+                      NUM_VTXBUF_HANDLES + i, MPI_COMM_WORLD, &request);
+
+            MPI_Status status;
+            MPI_Recv(&submesh->vertex_buffer[i][dst_idx], count, datatype, recv_pid,
+                     NUM_VTXBUF_HANDLES + i, MPI_COMM_WORLD, &status);
+
+            MPI_Wait(&request, &status);
         }
     }
 }
@@ -135,29 +188,31 @@ main(void)
 {
     MPI_Init(NULL, NULL);
 
-    int num_processes, process_id;
+    int num_processes, pid;
     MPI_Comm_size(MPI_COMM_WORLD, &num_processes);
-    MPI_Comm_rank(MPI_COMM_WORLD, &process_id);
+    MPI_Comm_rank(MPI_COMM_WORLD, &pid);
 
     char processor_name[MPI_MAX_PROCESSOR_NAME];
     int name_len;
     MPI_Get_processor_name(processor_name, &name_len);
-    printf("Processor %s. Process %d of %d.\n", processor_name, process_id, num_processes);
+    printf("Processor %s. Process %d of %d.\n", processor_name, pid, num_processes);
 
     AcMeshInfo mesh_info;
     load_config(&mesh_info);
     update_config(&mesh_info);
+    assert(mesh_info.int_params[AC_nz] % num_processes == 0); // MUST BE DIVISIBLE!
 
     AcMesh* main_mesh     = NULL;
     ModelMesh* model_mesh = NULL;
-    if (process_id == 0) {
+    if (pid == 0) {
         main_mesh = acmesh_create(mesh_info);
         acmesh_init_to(INIT_TYPE_RANDOM, main_mesh);
         model_mesh = modelmesh_create(mesh_info);
         acmesh_to_modelmesh(*main_mesh, model_mesh);
+        boundconds(mesh_info, model_mesh);
+        modelmesh_to_acmesh(*model_mesh, main_mesh);
     }
 
-    /*
     AcMeshInfo submesh_info = mesh_info;
     submesh_info.int_params[AC_nz] /= num_processes;
     update_config(&submesh_info);
@@ -166,16 +221,36 @@ main(void)
 
     /////////////////////
     distribute_mesh(main_mesh, submesh);
+    communicate_halos(submesh);
     gather_mesh(submesh, main_mesh);
     /////////////////////////
     acmesh_destroy(submesh);
-    */
 
-    if (process_id == 0) {
+    fflush(stdout);
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    //////////// RENDER
+    /*
+    if (pid == 0) {
+        acInit(main_mesh->info);
+        acLoad(*main_mesh);
+        assert(main_mesh);
+        renderer_init(main_mesh->info.int_params[AC_mx], main_mesh->info.int_params[AC_my]);
+        while (1) {
+            check_input(1.f / 60.f);
+            renderer_draw(*main_mesh);
+        }
+        renderer_quit();
+        acQuit();
+    }
+    */
+    /////////////
+
+    if (pid == 0) {
         assert(main_mesh);
         assert(model_mesh);
-        acPrintMeshInfo(main_mesh->info);
         bool is_acceptable = verify_meshes(*model_mesh, *main_mesh);
+        printf("%s\n", is_acceptable ? "Alles gut!" : "FUCK!");
         modelmesh_destroy(model_mesh);
         acmesh_destroy(main_mesh);
     }
