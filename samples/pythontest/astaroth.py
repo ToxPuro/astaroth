594b9abacc0ce343d434fb7ec23f00f170443726
jpekkila <johannes.pekkila@protonmail.com>
2020-09-20 23:49:37 +0300

    The proof-of-concept Python interface now compiles and runs successfully. It looks quite ugly though and it's not very pythonic. Could be improved in that aspect.

--- samples/pythontest/astaroth.py
+++ samples/pythontest/astaroth.py
@@ -9,17 +9,28 @@ NUM_VTXBUF_HANDLES = 8
 
 AC_DOUBLE_PRECISION = True
 
+AC_nx = 0
+AC_ny = 1
+AC_nz = 2
+
 c_real = c_float
 if AC_DOUBLE_PRECISION:
     c_real = c_double
 
 class AcMeshInfo(Structure):
     _fields_ = [("int_params", NUM_INT_PARAMS * c_int),
-                ("int_params", 3 * NUM_INT3_PARAMS * c_int),
+                ("int3_params", 3 * NUM_INT3_PARAMS * c_int),
                 ("real_params", NUM_REAL_PARAMS * c_real),
                 ("real3_params", 3 * NUM_REAL3_PARAMS * c_real)]
 
 info = AcMeshInfo()
-device = astaroth.acDeviceCreate(0, info);
+info.int_params[AC_nx] = 128
+info.int_params[AC_ny] = 128
+info.int_params[AC_nz] = 128
+astaroth.acUpdateBuiltinParams(pointer(info))
+astaroth.acPrintMeshInfo(info)
+
+device = c_void_p(1)
+astaroth.acDeviceCreate(0, info, byref(device));
 astaroth.acDevicePrintInfo(device)
-astarth.acDeviceDestroy(device)
+astaroth.acDeviceDestroy(device)
