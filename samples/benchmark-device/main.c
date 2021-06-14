46a573ccb97f8ab39d2140819a6dfc06a779d82f
jpekkila <johannes.pekkila@protonmail.com>
2021-06-14 12:55:07 +0300

    Autoformat

--- samples/benchmark-device/main.c
+++ samples/benchmark-device/main.c
@@ -7,70 +7,72 @@
 
 #define NSAMPLES (100)
 
-int main(int argc, char** argv)
+int
+main(int argc, char** argv)
 {
-	AcMeshInfo info;
-	acLoadConfig(AC_DEFAULT_CONFIG, &info);
+    AcMeshInfo info;
+    acLoadConfig(AC_DEFAULT_CONFIG, &info);
 
-	// Set mesh dimensions
-	if (argc != 4) {
-		fprintf(stderr, "Usage: ./benchmark-device <nx> <ny> <nz>\n");
-		return EXIT_FAILURE;
-	} else {
-		info.int_params[AC_nx] = atoi(argv[1]);
-		info.int_params[AC_ny] = atoi(argv[2]);
-		info.int_params[AC_nz] = atoi(argv[3]);
-		acHostUpdateBuiltinParams(&info);
-	}
+    // Set mesh dimensions
+    if (argc != 4) {
+        fprintf(stderr, "Usage: ./benchmark-device <nx> <ny> <nz>\n");
+        return EXIT_FAILURE;
+    }
+    else {
+        info.int_params[AC_nx] = atoi(argv[1]);
+        info.int_params[AC_ny] = atoi(argv[2]);
+        info.int_params[AC_nz] = atoi(argv[3]);
+        acHostUpdateBuiltinParams(&info);
+    }
 
-	// Alloc
-	AcMesh model, candidate;
-	acHostMeshCreate(info, &model);
-	acHostMeshCreate(info, &candidate);
+    // Alloc
+    AcMesh model, candidate;
+    acHostMeshCreate(info, &model);
+    acHostMeshCreate(info, &candidate);
 
-	// Init
-	acHostMeshRandomize(&model);
-	acHostMeshApplyPeriodicBounds(&model);
+    // Init
+    acHostMeshRandomize(&model);
+    acHostMeshApplyPeriodicBounds(&model);
 
-	// Verify that the mesh was loaded and stored correctly
-	acInit(info);
-	acLoad(model);
-	acStore(&candidate);
-	acVerifyMesh("Load/Store", model, candidate);
+    // Verify that the mesh was loaded and stored correctly
+    acInit(info);
+    acLoad(model);
+    acStore(&candidate);
+    acVerifyMesh("Load/Store", model, candidate);
 
-	// Verify that boundconds work correctly
-	acBoundcondStep();
-	acStore(&candidate);
-	acHostMeshApplyPeriodicBounds(&model);
-	acVerifyMesh("Boundconds", model, candidate);
+    // Verify that boundconds work correctly
+    acBoundcondStep();
+    acStore(&candidate);
+    acHostMeshApplyPeriodicBounds(&model);
+    acVerifyMesh("Boundconds", model, candidate);
 
-	// Verify that integration works correctly
-	const AcReal dt = FLT_EPSILON;
-	acIntegrate(dt);
-	acBoundcondStep();
-	acStore(&candidate);
+    // Verify that integration works correctly
+    const AcReal dt = FLT_EPSILON;
+    acIntegrate(dt);
+    acBoundcondStep();
+    acStore(&candidate);
 
-	acHostIntegrateStep(model, dt);
-	acHostMeshApplyPeriodicBounds(&model);
-	acVerifyMesh("Integration", model, candidate);
+    acHostIntegrateStep(model, dt);
+    acHostMeshApplyPeriodicBounds(&model);
+    acVerifyMesh("Integration", model, candidate);
 
-	// Warmup
-	for (int i = 0; i < NSAMPLES/10; ++i)
-		acIntegrate(dt);
+    // Warmup
+    for (int i = 0; i < NSAMPLES / 10; ++i)
+        acIntegrate(dt);
 
-	// Benchmark
-	Timer t;
-	timer_reset(&t);
-	for (int i = 0; i < NSAMPLES; ++i)
-		acIntegrate(dt);
-	acSynchronize();
-	const double ms_elapsed = timer_diff_nsec(t) / 1e6;
-	printf("Average integration time: %.4g ms\n", ms_elapsed / NSAMPLES);
+    // Benchmark
+    Timer t;
+    timer_reset(&t);
+    for (int i = 0; i < NSAMPLES; ++i)
+        acIntegrate(dt);
+    acSynchronize();
+    const double ms_elapsed = timer_diff_nsec(t) / 1e6;
+    printf("Average integration time: %.4g ms\n", ms_elapsed / NSAMPLES);
 
-	// Destroy
-	acQuit();
-	acHostMeshDestroy(&model);
-	acHostMeshDestroy(&candidate);
+    // Destroy
+    acQuit();
+    acHostMeshDestroy(&model);
+    acHostMeshDestroy(&candidate);
 
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
