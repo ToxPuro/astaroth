304cfcab787f208037feda9b6e6084f50ad025f7
jpekkila <johannes.pekkila@protonmail.com>
2021-06-16 14:18:15 +0300

    Added division operator overload to math utils

--- src/common/math_utils.h
+++ src/common/math_utils.h
@@ -200,6 +200,12 @@ operator*(const AcReal3& b, const AcReal& a)
     return (AcReal3){a * b.x, a * b.y, a * b.z};
 }
 
+static HOST_DEVICE_INLINE AcReal3
+operator/(const AcReal3& a, const AcReal& b)
+{
+    return (AcReal3){a.x / b, a.y / b, a.z / b};
+}
+
 static HOST_DEVICE_INLINE AcReal
 dot(const AcReal3& a, const AcReal3& b)
 {
