dac3dfda81022a151f84fc098566d092684b49e9
jpekkila <johannes.pekkila@aalto.fi>
2022-02-03 16:44:36 +0200

    Added a sample for testing MPI-IO read/writes with Astaroth

---
+++ samples/mpi-io/main.c
@@ -0,0 +1,98 @@
+/*
+    Copyright (C) 2014-2022, Johannes Pekkila, Miikka Vaisala.
+
+    This file is part of Astaroth.
+
+    Astaroth is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Astaroth is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Astaroth.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/**
+    Running: mpirun -np <num processes> <executable>
+*/
+#include "astaroth.h"
+#include "astaroth_utils.h"
+#include "errchk.h"
+
+#if !AC_MPI_ENABLED
+int
+main(void)
+{
+    printf("The library was built without MPI support, cannot run mpitest. Rebuild Astaroth with "
+           "cmake -DMPI_ENABLED=ON .. to enable.\n");
+    return EXIT_FAILURE;
+}
+#else
+
+#include <mpi.h>
+
+int
+main(int argc, char** argv)
+{
+    MPI_Init(NULL, NULL);
+    int pid, nprocs;
+    MPI_Comm_rank(MPI_COMM_WORLD, &pid);
+    MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+
+    AcMeshInfo info;
+    acLoadConfig(AC_DEFAULT_CONFIG, &info);
+    // Set mesh dimensions
+    if (argc != 4) {
+        fprintf(stderr, "Usage: ./mpi-io <nx> <ny> <nz>\n");
+        return EXIT_FAILURE;
+    }
+    else {
+        info.int_params[AC_nx] = atoi(argv[1]);
+        info.int_params[AC_ny] = atoi(argv[2]);
+        info.int_params[AC_nz] = atoi(argv[3]);
+        acHostUpdateBuiltinParams(&info);
+    }
+
+    // Alloc
+    AcMesh model, candidate;
+    acHostMeshCreate(info, &model);
+    acHostMeshCreate(info, &candidate);
+
+    // Init
+    acHostMeshRandomize(&model);
+    acHostMeshRandomize(&candidate);
+    acHostMeshApplyPeriodicBounds(&model);
+
+    acGridInit(info);
+    acGridLoadMesh(STREAM_DEFAULT, model);
+    acGridPeriodicBoundconds(STREAM_DEFAULT);
+    acGridStoreMesh(STREAM_DEFAULT, &candidate);
+
+    if (!pid) {
+        const AcResult res = acVerifyMesh("CPU-GPU Load/store", model, candidate);
+        ERRCHK_ALWAYS(res == AC_SUCCESS);
+    }
+
+    // Read-write
+    acGridAccessMeshOnDisk((VertexBufferHandle)0, "test.out", ACCESS_WRITE);
+    acHostMeshRandomize(&candidate);
+    acGridLoadMesh(STREAM_DEFAULT, candidate);
+    acGridAccessMeshOnDisk((VertexBufferHandle)0, "test.out", ACCESS_READ);
+    acGridPeriodicBoundconds(STREAM_DEFAULT);
+    acGridStoreMesh(STREAM_DEFAULT, &candidate);
+
+    if (!pid) {
+        const AcResult res = acVerifyMesh("MPI-IO disk read/write", model, candidate);
+        ERRCHK_ALWAYS(res == AC_SUCCESS);
+    }
+
+    acGridQuit();
+
+    MPI_Finalize();
+    return EXIT_SUCCESS;
+}
+#endif
