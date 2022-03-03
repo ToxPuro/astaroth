e539a53468642e47ef06864150e09fb81dfb32cd
jpekkila <johannes.pekkila@aalto.fi>
2022-03-03 11:17:08 +0200

    Added cast to AC_THERMAL_CONDUCTIVITY in modelsolver.c

--- src/utils/modelsolver.c
+++ src/utils/modelsolver.c
@@ -764,7 +764,8 @@ heat_conduction(const ScalarData ss, const ScalarData lnrho)
     const Vector third_term = getReal(AC_gamma) * (inv_cp_sound * gradient(ss) + gradient(lnrho)) +
                               grad_ln_chi;
 
-    const Scalar chi = AC_THERMAL_CONDUCTIVITY / (exp(value(lnrho)) * getReal(AC_cp_sound));
+    const Scalar chi = (AcReal)(AC_THERMAL_CONDUCTIVITY) /
+                       (exp(value(lnrho)) * getReal(AC_cp_sound));
     return getReal(AC_cp_sound) * chi * (first_term + dot(second_term, third_term));
 }
 
