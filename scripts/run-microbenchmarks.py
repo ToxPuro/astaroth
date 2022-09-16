ac10da1afa40dbc3f9faeddb0613fb358e47282b
jpekkila <johannes.pekkila@aalto.fi>
2022-09-16 14:30:30 +0300

    bwtest-benchmark now produces sufficiently detailed csv files for plots for the paper

--- scripts/run-microbenchmarks.py
+++ scripts/run-microbenchmarks.py
@@ -2,16 +2,44 @@
 import os
 
 
-build_dir='/m/home/home6/61/pekkilj1/unix/repositories/astaroth/build'
-cmake='/m/home/home6/61/pekkilj1/unix/repositories/cmake/build/bin/cmake'
+#build_dir='/m/home/home6/61/pekkilj1/unix/repositories/astaroth/build'
+build_dir='/home/pekkilj1/astaroth/build'
+#cmake='/m/home/home6/61/pekkilj1/unix/repositories/cmake/build/bin/cmake'
+cmake='/home/pekkilj1/cmake/build/bin/cmake'
 
 cwd = os.getcwd()
 if cwd != build_dir:
     print(f"Invalid dir {cwd}. Should be {build_dir}")
     exit(-1)
 
-os.system("rm bwtest-benchmark.csv")
 
+# Build and remove previous results
+os.system(f'{cmake} .. && make -j')
+os.system('echo "problemsize,workingsetsize,milliseconds,bandwidth" > bwtest-benchmark.csv')
+
+
+max_problem_size = 1 * 1024**3    # 1 GiB
+max_working_set_size = 8200 #512 * 1024 # 512 KiB
+
+problem_size = 8
+working_set_size = 8
+
+# Variable problem size
+while problem_size <= max_problem_size:
+    os.system(f'./bwtest-benchmark {problem_size} {working_set_size}')
+    problem_size *= 2
+os.system('mv bwtest-benchmark.csv problem-size.csv')
+
+# Variable working set size
+problem_size = 256 * 1024**2       # 256 MiB
+while working_set_size <= max_working_set_size:
+    os.system(f'./bwtest-benchmark {problem_size} {working_set_size}')
+    working_set_size *= 2
+os.system('mv bwtest-benchmark.csv working-set-size.csv')
+
+
+
+'''
 num_fields = 8
 num_stencils = 10
 num_points_per_stencil = 55
@@ -26,3 +54,4 @@ while r <= working_set_size:
         r = 1
     else:
         r = 2 * r
+'''
