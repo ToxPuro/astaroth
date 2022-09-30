bb170d917279ff8c80f608ac67a4eafc438b3aa6
Matthias Rheinhardt <matthias.rheinhardt@aalto.fi>
2022-09-30 12:14:14 +0000

    heat_conduction_kramers.h correctesd

--- acc-runtime/samples/mhd/heat_conduction_kramers.h
+++ acc-runtime/samples/mhd/heat_conduction_kramers.h
@@ -1,19 +1,17 @@
 heat_conduction_kramers() {
-      hcond0_kramers = 0.
-      lnTT0 = 0.
-      AC_nkramers = 0.
-      AC_cv = 0.
-      temperature = 0.
-      
+
+// Calculates diffusive term in entropy equation for Kramers conductivity.
+
       cv1 = 1./AC_cv
       rho1 = exp(-value(VTXBUF_LNRHO))
-      lnTT = lnTT0+cv1*value(VTXBUF_ENTROPY)+(AC_gamma-1)*(value(VTXBUF_LNRHO)-AC_lnrho0)
+      lnTT = AC_lnTT0+cv1*value(VTXBUF_ENTROPY)+(AC_gamma-1)*(value(VTXBUF_LNRHO)-AC_lnrho0)
       glnrho = gradient(VTXBUF_LNRHO)
+
       glnTT  = cv1*gradient(VTXBUF_ENTROPY) + (AC_gamma-1)*glnrho
       del2lnTT = cv1*laplace(VTXBUF_ENTROPY) + (AC_gamma-1)*laplace(VTXBUF_LNRHO)
 
-      Krho1 = hcond0_kramers*pow(rho1,(2.*AC_nkramers+1.))*pow(temperature,(6.5*AC_nkramers))   // = K/rho
-      //Krho1 = hcond0_kramers*exp(-p%lnrho*(2.*nkramers+1.)+p%lnTT*(6.5*nkramers))   ! = K/rho
+      Krho1 = AC_hcond0_kramers * pow(rho1,(2.*AC_nkramers+1.)) * pow(exp(lnTT),(6.5*AC_nkramers))   // = K/rho
+
       g2=dot(-2.*AC_nkramers*glnrho+(6.5*AC_nkramers+1)*glnTT,glnTT)
       return Krho1*(del2lnTT)+g2
 }
