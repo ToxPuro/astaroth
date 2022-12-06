e50c27443c4b1328d8636fd698c58317dcfd4849
jpekkila <johannes.pekkila@aalto.fi>
2022-12-06 19:22:59 +0200

    Added hero run scripts and set the default conf to 4k grid

---
+++ herorun-scripts/hero-run-4.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --account=project_462000120
+#SBATCH --gres=gpu:4
+#SBATCH --partition=pilot
+#SBATCH --ntasks=4
+#SBATCH --nodes=1
+#SBATCH --time=00:14:59
+
+module purge
+
+        module load CrayEnv
+        module load PrgEnv-cray
+        module load craype-accel-amd-gfx90a
+        module load rocm
+        module load buildtools
+        module load cray-python
+        
+srun ./ac_run_mpi --config /users/pekkila/astaroth/config/astaroth-hero-run.conf 
---
+++ herorun-scripts/hero-run-4096.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --account=project_462000120
+#SBATCH --gres=gpu:8
+#SBATCH --partition=pilot
+#SBATCH --ntasks=4096
+#SBATCH --nodes=512
+#SBATCH --time=00:59:59
+
+module purge
+
+        module load CrayEnv
+        module load PrgEnv-cray
+        module load craype-accel-amd-gfx90a
+        module load rocm
+        module load buildtools
+        module load cray-python
+
+srun ./ac_run_mpi --config /users/pekkila/astaroth/config/astaroth-hero-run.conf
