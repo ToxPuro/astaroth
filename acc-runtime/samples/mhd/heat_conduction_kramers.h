b78764e3c680a309d8dbbdbff0108f8ac9199b9f
Matthias Rheinhardt <matthias.rheinhardt@aalto.fi>
2022-12-13 17:07:57 +0000

    new cooling term, step function

--- acc-runtime/samples/mhd/heat_conduction_kramers.h
+++ acc-runtime/samples/mhd/heat_conduction_kramers.h
@@ -13,5 +13,12 @@ heat_conduction_kramers() {
       Krho1 = AC_hcond0_kramers * pow(rho1,(2.*AC_n_kramers+1.)) * pow(exp(lnTT),(6.5*AC_n_kramers))   // = K/rho
 
       g2=dot(-2.*AC_n_kramers*glnrho+(6.5*AC_n_kramers+1)*glnTT,glnTT)
-      return Krho1*(del2lnTT)+g2
+      diffusion = Krho1*(del2lnTT)+g2
+
+// Cooling in a surface layer; step profile
+      prof = step(z,AC_step_pos,AC_step_width)
+      return diffusion - AC_cool*prof*(AC_gamma*(AC_cp_sound-AC_cv_sound)*exp(lnTT)/AC_cs2cool-1.)
+}
+step(x,x0,width){   //AcReal x, AcReal x0, AcReal width){
+      return 0.5*(1.+tanh((x-x0)/width))
 }
