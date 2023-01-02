dee26ebf72366e00cb7c0f6527e993e05cb8c3d9
jpekkila <johannes.pekkila@aalto.fi>
2023-01-02 12:14:43 +0200

    Updated astaroth/pilot dir with sample scripts

--- pilot/pilot-4096.sh
+++ pilot/pilot-4096.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=project_462000120
 #SBATCH --gres=gpu:8
-#SBATCH --partition=pilot
+#SBATCH --partition=dev-g
 #SBATCH --ntasks=4096
 #SBATCH --nodes=512
 #SBATCH --time=00:59:00
@@ -14,5 +14,7 @@ module load craype-accel-amd-gfx90a
 module load rocm
 module load buildtools
 module load cray-python
+
+export MPICH_GPU_SUPPORT_ENABLED=1
 
 srun ./ac_run_mpi --config ./astaroth.conf --from-pc-varfile=/flash/project_462000120/striped_dir/var.dat
--- pilot/pilot-8.sh
+++ pilot/pilot-8.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=project_462000120
 #SBATCH --gres=gpu:8
-#SBATCH --partition=pilot
+#SBATCH --partition=dev-g
 #SBATCH --ntasks=8
 #SBATCH --nodes=1
 #SBATCH --time=00:10:00
@@ -14,5 +14,7 @@ module load craype-accel-amd-gfx90a
 module load rocm
 module load buildtools
 module load cray-python
+
+export MPICH_GPU_SUPPORT_ENABLED=1
 
 srun ./ac_run_mpi --config ./astaroth.conf --from-pc-varfile=/scratch/project_462000120/jpekkila/mahti-512-varfile/var.dat
