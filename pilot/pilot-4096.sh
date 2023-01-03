7166c37e1acfde4f88582a728c7f0682c7feecb2
jpekkila <johannes.pekkila@aalto.fi>
2023-01-03 14:16:23 +0200

    Updated the pilot sample dir with the corrected slurm options

--- pilot/pilot-4096.sh
+++ pilot/pilot-4096.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #SBATCH --account=project_462000120
-#SBATCH --gres=gpu:8
 #SBATCH --partition=dev-g
-#SBATCH --ntasks=8
 #SBATCH --nodes=512
+#SBATCH --ntasks-per-node=8
+#SBATCH --gpus-per-node=8
+#SBATCH --exclusive
 #SBATCH --time=00:59:00
 
 module purge
@@ -17,5 +18,6 @@ module load cray-python
 
 export MPICH_GPU_SUPPORT_ENABLED=1
 
-srun ./ac_run_mpi --config ./astaroth.conf --from-pc-varfile=/flash/project_462000120/pilot_experiments/data/var.4096.dat # From varfile
-# srun ./ac_run_mpi --config ./astaroth.conf --from-snapshot # From snapshot
+srun --cpu-bind=map_cpu:48,56,16,24,1,8,32,40 ./ac_run_mpi --config ./astaroth.conf --from-pc-varfile=/flash/project_462000120/pilot_experiments/data/var.4096.dat # From varfile
+# srun --cpu-bind=map_cpu:48,56,16,24,1,8,32,40 ./ac_run_mpi --config ./astaroth.conf --from-snapshot # From snapshot
+# srun --cpu-bind=map_cpu:48,56,16,24,1,8,32,40 ./mpitest 64 64 64 # Autotest
--- pilot/pilot-8.sh
+++ pilot/pilot-8.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #SBATCH --account=project_462000120
-#SBATCH --gres=gpu:8
 #SBATCH --partition=dev-g
-#SBATCH --ntasks-per-node=8
 #SBATCH --nodes=1
+#SBATCH --ntasks-per-node=8
+#SBATCH --gpus-per-node=8
+#SBATCH --exclusive
 #SBATCH --time=00:10:00
 
 module purge
@@ -17,5 +18,6 @@ module load cray-python
 
 export MPICH_GPU_SUPPORT_ENABLED=1
 
-srun ./ac_run_mpi --config ./astaroth.conf --from-pc-varfile=/scratch/project_462000120/jpekkila/mahti-512-varfile/var.dat # From varfile
-# srun ./ac_run_mpi --config ./astaroth.conf --from-snapshot # From snapshot
+srun --cpu-bind=map_cpu:48,56,16,24,1,8,32,40 ./ac_run_mpi --config ./astaroth.conf --from-pc-varfile=/scratch/project_462000120/jpekkila/mahti-512-varfile/var.dat # From varfile
+# srun --cpu-bind=map_cpu:48,56,16,24,1,8,32,40 ./ac_run_mpi --config ./astaroth.conf --from-snapshot # From snapshot
+# srun --cpu-bind=map_cpu:48,56,16,24,1,8,32,40 ./mpitest 64 64 64 # Autotest
