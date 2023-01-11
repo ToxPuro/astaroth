f042e2e6c20f2da3366521b561f6d81557148577
jpekkila <johannes.pekkila@aalto.fi>
2023-01-11 15:24:23 +0200

    Autoformat benchmark-device

--- samples/benchmark-device/main.c
+++ samples/benchmark-device/main.c
@@ -14,19 +14,22 @@ static const bool verify = false;
 
 // TODO use common sort with benchmark, benchmark-device, benchmark-node, mpi-io
 void
-validate(const size_t count, const double* arr) {
+validate(const size_t count, const double* arr)
+{
     for (size_t i = 1; i < count; ++i)
-        ERRCHK_ALWAYS(arr[i] >= arr[i-1]);
+        ERRCHK_ALWAYS(arr[i] >= arr[i - 1]);
 }
 
 // TODO use common sort with benchmark, benchmark-device, benchmark-node, mpi-io
-void sort(const size_t count, double* arr) {
+void
+sort(const size_t count, double* arr)
+{
     for (size_t j = 0; j < count; ++j) {
-        for (size_t i = j+1; i < count; ++i) {
+        for (size_t i = j + 1; i < count; ++i) {
             if (arr[i] < arr[j]) {
                 const double tmp = arr[j];
-                arr[j] = arr[i];
-                arr[i] = tmp;
+                arr[j]           = arr[i];
+                arr[i]           = tmp;
             }
         }
     }
@@ -130,7 +133,7 @@ main(int argc, char** argv)
     }
 
     if (verify) {
-    acDeviceStoreMesh(device, STREAM_DEFAULT, &candidate);
+        acDeviceStoreMesh(device, STREAM_DEFAULT, &candidate);
 
         for (size_t j = 0; j < nsteps; ++j) {
             acHostIntegrateStep(model, dt);
@@ -152,7 +155,7 @@ main(int argc, char** argv)
 
     // Benchmark
     Timer t;
-    //timer_reset(&t);
+    // timer_reset(&t);
     double results[NSAMPLES] = {0};
 #pragma unroll
     for (int j = 0; j < NSAMPLES; ++j) {
@@ -164,7 +167,7 @@ main(int argc, char** argv)
         results[j] = timer_diff_nsec(t) / 1e6;
 
         // Full integration step
-        //for (int i = 0; i < 3; ++i) {
+        // for (int i = 0; i < 3; ++i) {
         //    acDeviceIntegrateSubstep(device, STREAM_DEFAULT, i, n_min, n_max, dt);
         //    acDeviceSwapBuffers(device);
         //    acDevicePeriodicBoundconds(device, STREAM_DEFAULT, m_min, m_max);
@@ -172,29 +175,33 @@ main(int argc, char** argv)
     }
     sort(NSAMPLES, results);
     validate(NSAMPLES, results);
-    const double min = results[0];
-    const double median = NSAMPLES % 2 ? results[NSAMPLES/2] : 0.5 * (results[NSAMPLES/2 - 1] + results[NSAMPLES/2]);
+    const double min            = results[0];
+    const double median         = NSAMPLES % 2 ? results[NSAMPLES / 2]
+                                               : 0.5 * (results[NSAMPLES / 2 - 1] + results[NSAMPLES / 2]);
     const double percentile90th = results[(size_t)ceil(0.9 * NSAMPLES)];
-    const double max = results[NSAMPLES-1];
-    
+    const double max            = results[NSAMPLES - 1];
+
     printf("Integration times:\n");
     printf("\tmin: %g\n", min);
     printf("\tmedian: %g\n", median);
-    printf("\t90th percentile: %g\n", percentile90th); // Conservative, takes the first precentile >= 90%
+    printf("\t90th percentile: %g\n",
+           percentile90th); // Conservative, takes the first precentile >= 90%
     printf("\tmax: %g\n", max);
 
-    //for (size_t i = 0; i < NSAMPLES; ++i)
-    //    printf("%g\n", results[i]);
-    //acDeviceSynchronizeStream(device, STREAM_DEFAULT);
-    //const double ms_elapsed   = timer_diff_nsec(t) / 1e6;
-    //const double milliseconds = ms_elapsed / NSAMPLES;
-    //printf("Average integration time: %.4g ms\n", milliseconds);
+    // for (size_t i = 0; i < NSAMPLES; ++i)
+    //     printf("%g\n", results[i]);
+    // acDeviceSynchronizeStream(device, STREAM_DEFAULT);
+    // const double ms_elapsed   = timer_diff_nsec(t) / 1e6;
+    // const double milliseconds = ms_elapsed / NSAMPLES;
+    // printf("Average integration time: %.4g ms\n", milliseconds);
 
     // Write to file
     const char* benchmark_dir = "device-benchmark.csv";
     FILE* fp                  = fopen(benchmark_dir, "a");
     ERRCHK_ALWAYS(fp);
-    // 'implementation, maxthreadsperblock,millisecondsmin,millisecondsmedian,milliseconds90thpercentile,millisecondsmax, nx, ny, nz, devices'
+    // 'implementation,
+    // maxthreadsperblock,millisecondsmin,millisecondsmedian,milliseconds90thpercentile,millisecondsmax,
+    // nx, ny, nz, devices'
     const int num_devices = 1;
     fprintf(fp, "%d,%d,%g,%d,%d,%d,%d\n", IMPLEMENTATION, MAX_THREADS_PER_BLOCK, percentile90th,
             info.int_params[AC_nx], info.int_params[AC_ny], info.int_params[AC_nz], num_devices);
