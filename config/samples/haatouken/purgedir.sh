8109a37d892bb040fc079d9f1c0663f174b80c72
Miikka Vaisala <mvaisala@asiaa.sinica.edu.tw>
2023-05-02 16:18:06 +0800

    No need to compile everything just the standalone_mpi

---
+++ config/samples/haatouken/purgedir.sh
@@ -0,0 +1,22 @@
+ #/bin/bash 
+ 
+ rm -rf acc-runtime
+ rm -rf ac-interpreter
+ rm -rf ac_run_mpi
+ rm -rf autotune.csv
+ rm -rf CMakeCache.txt
+ rm -rf CMakeFiles
+ rm -rf cmake_install.cmake
+ rm -rf COMMIT_CODE.log
+ rm -rf compile_commands.json
+ rm -rf Makefile
+ rm -rf mesh_info.list
+ rm -rf output-slices
+ rm -rf output-snapshots
+ rm -rf purgedir.sh
+ rm -rf purge.sh
+ rm -rf README.md
+ rm -rf samples
+ rm -rf snapshots_info.csv
+ rm -rf src
+ rm -rf timeseries.ts
