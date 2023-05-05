9b2dd901e621d911ba626882284005ac0a7bd70e
Miikka Vaisala <mvaisala@asiaa.sinica.edu.tw>
2023-05-05 16:45:02 +0800

    Adjustted test run parameters

--- config/samples/haatouken/run_on_tiara.sh
+++ config/samples/haatouken/run_on_tiara.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-mpirun -np 4 ./ac_run_mpi --config astaroth.conf --run-init-kernel 
+#mpirun -np 4 ./ac_run_mpi --config astaroth.conf --run-this-init-kernel Haatouken
+
+( mpirun -np 4 ./ac_run_mpi --config astaroth.conf --run-this-init-kernel Haatouken >& output.log & )
 
