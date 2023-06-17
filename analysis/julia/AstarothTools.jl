69cc49c59bf6bcd51779f2cf91fedde4678ba98d
Miikka Vaisala <mvaisala@asiaa.sinica.edu.tw>
2023-06-17 18:23:08 +0800

    Astaroth postprocess io tools update branch.
    Target is to read and process process split snapshot files in python and examine the use of Julia in post processing.

---
+++ analysis/julia/AstarothTools.jl
@@ -0,0 +1,9 @@
+
+module AstarothTools
+
+function ReadACData()
+    println("Reading snapshot data data...")
+    return 0 
+end
+
+end 
---
+++ analysis/julia/plotting_sample.jl
@@ -0,0 +1,5 @@
+import .AstarothTools
+
+AstarothTools.ReadACData()
+
+
