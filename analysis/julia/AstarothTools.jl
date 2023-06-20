08a33c5f2fa9a98a55704606dbc566f1c2f7a259
Miikka Vaisala <mvaisala@asiaa.sinica.edu.tw>
2023-06-20 20:24:50 +0800

    Improving parsing and file reading

--- analysis/julia/AstarothTools.jl
+++ analysis/julia/AstarothTools.jl
@@ -28,30 +28,30 @@ function ReadACData()
     dirpath = "/tiara/ara/data/mvaisala/202304_haatouken/astaroth/config/samples/haatouken/output-snapshots/"
     directory = readdir(dirpath)
 
-    mesh_info = Dict("variable name" => "variable_value")
-
+    # Describe for of the the lists 
     mesh_info_int  = Dict("variable name" => 1)
+    mesh_info_int3  = Dict("variable name" => (1,1,1))
     mesh_info_real = Dict("variable name" => 1.0)
 
+    # Read lines in the file 
     open(dirpath*"../mesh_info.list") do fobj
         for line in eachline(fobj)
-            #println(line)
+            println(line)
             line_content = split(line)
             #println(line_content)
             type = line_content[1]
             varname = line_content[2]
             varvalue = line_content[3]
-            println(type, " ", varname, " ", varvalue)
+            #println(type, " ", varname, " ", varvalue)
             if type == "real"
-                println(parse(Float64, varvalue))
                 varnumber = parse(Float64, varvalue)
                 mesh_info_real[varname] = varnumber
-            elseif type == "int"
+            elseif type == "int" 
                 varnumber = parse(Int, varvalue)
                 mesh_info_int[varname] = varnumber
-            elseif type == "int3" #TODO FIGURE THIS OUT 
-                varnumber = parse(Float64, varvalue)
-                mesh_info_int[varname] = varnumber
+            elseif type == "int3"
+                varnumber3 = (parse(Int, line_content[3]), parse(Int, line_content[4]), parse(Int, line_content[5]))
+                mesh_info_int3[varname] = varnumber3 
             elseif type == "size_t"
                 varnumber = parse(Float64, varvalue)
                 mesh_info_int[varname] = varnumber
@@ -60,30 +60,51 @@ function ReadACData()
             end
         end
     end
+    delete!(mesh_info_int, "variable name")
+    delete!(mesh_info_int3, "variable name")
+    delete!(mesh_info_real, "variable name")
 
-    println(mesh_info)
     println(mesh_info_int)
+    println(mesh_info_int3)
     println(mesh_info_real)
 
     println(directory)
-    println(directory[1])
 
-    binfile = dirpath * directory[1]
-    println(binfile)
-    xdim = 128 
-    ydim = 128
+    xdim = 256
+    ydim = 256
     zdim = 256
-    filesize = xdim*ydim*zdim
-    binary_data = Array{Float64}(undef, filesize, 1);
-    #binary_data = read(binfile, Float64)
-    read!(binfile, binary_data)
 
-    println(size(binary_data))
-    println(filesize)
-    #println(binary_data)
-    println(typeof(binary_data))
-    binary_data = reshape(binary_data, (xdim, ydim, zdim))
-    println(size(binary_data))
+    arraydims = (xdim, ydim, zdim)
+
+    println(arraydims)
+    whole_array = zeros(arraydims)
+
+    for my_dir in directory
+
+        binfile = dirpath * my_dir
+        println(binfile)
+        xdim_loc = 128 
+        ydim_loc = 128
+        zdim_loc = 256
+        filesize = xdim_loc*ydim_loc*zdim_loc
+        binary_data = Array{Float64}(undef, filesize, 1);
+        read!(binfile, binary_data)
+
+        file_info = split(my_dir, ".")
+        file_info = split(file_info[1], "-")
+        println(file_info)
+        iix   = file_info[3]
+        iiy   = file_info[4]
+        iiz   = file_info[5]
+        nstep = file_info[6] 
+
+        println(size(binary_data))
+        println(filesize)
+        println(typeof(binary_data))
+        binary_data = reshape(binary_data, (xdim_loc, ydim_loc, zdim_loc))
+        println(size(binary_data))
+        whole_array[1:128, 1:128, 1:256] = binary_data
+    end 
 
     return 0 
 end
