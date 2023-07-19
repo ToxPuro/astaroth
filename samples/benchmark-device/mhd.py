aca7f07f1a803266f18cd63865bea3972c0204cd
jpekkila <johannes.pekkila@protonmail.com>
2023-07-19 17:15:42 +0300

    Fix np.fp64 -> np.float64

--- samples/benchmark-device/mhd.py
+++ samples/benchmark-device/mhd.py
@@ -437,7 +437,7 @@ class Output:
                'jobid': args.jobid,
                'seed': seed,
                'iteration': iteration,
-               'double_precision': int(args.dtype == np.fp64)}
+               'double_precision': int(args.dtype == np.float64)}
         self.df.loc[len(self.df.index)] = row
 
     def __del__(self):
