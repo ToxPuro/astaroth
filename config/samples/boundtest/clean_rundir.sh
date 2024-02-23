6e8042025b00292d2e2abf323fddeb05b64a24f2
Miikka Vaisala <mvaisala@asiaa.sinica.edu.tw>
2024-02-23 15:26:05 +0800

    Better clean script.

--- config/samples/boundtest/clean_rundir.sh
+++ config/samples/boundtest/clean_rundir.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 shopt -s extglob
-rm -vr !("astaroth.conf"|"purgedir.sh"|"dslsource"|"clean_rundir.sh"|"my_cmake.sh"|"README.md"|"a2_timeseries.ts"|"run_on_tiara.sh"|"add_to_pythonpath.sh"|"check_run_properties.py")
+rm -vr !("astaroth.conf"|"twcc_batch_16.sh"|"twcc_batch_32.sh"|"twcc_batch_8.sh"|"purgedir.sh"|"dslsource"|"clean_rundir.sh"|"my_cmake.sh"|"README.md"|"a2_timeseries.ts"|"run_on_tiara.sh"|"add_to_pythonpath.sh"|"check_run_properties.py")
 
