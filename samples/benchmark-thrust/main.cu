6135f0d5e0110915b14b7064379f43abc5fd9b41
jpekkila <johannes.pekkila@aalto.fi>
2024-03-26 17:47:39 +0200

    Code for profiling benchmark-thrust/main.cu

--- samples/benchmark-thrust/main.cu
+++ samples/benchmark-thrust/main.cu
@@ -8,6 +8,8 @@
 #include <thrust/functional.h>
 #include <thrust/transform_reduce.h>
 
+#include "astaroth_utils.h"
+
 template <typename T> struct square {
     __host__ __device__ T operator()(const T& x) const { return x * x; }
 };
@@ -36,6 +38,8 @@ benchmark_thrust(const size_t mx, const size_t my, const size_t mz)
 int
 main(int argc, char** argv)
 {
+    cudaProfilerStop();
+
     fprintf(stderr, "Usage: ./benchmark-thrust <nx> <ny> <nz>\n");
     const size_t nx     = (argc > 1) ? (size_t)atol(argv[1]) : 32;
     const size_t ny     = (argc > 2) ? (size_t)atol(argv[2]) : 32;
@@ -56,15 +60,36 @@ main(int argc, char** argv)
 
     benchmark_thrust(mx, my, mz);
 
-    VertexBufferArray vba    = acVBACreate(mx, my, mz);
+    // Device
+    cudaSetDevice(0);
+
+    // Mesh info
+    AcMeshInfo info;
+    acLoadConfig(AC_DEFAULT_CONFIG, &info);
+    acSetMeshDims(nx, ny, nz, &info);
+    acLoadMeshInfo(info, 0);
+
+    // Random
+    const size_t seed  = 12345;
+    const size_t pid   = 0;
+    const size_t count = acVertexBufferCompdomainSize(info);
+    acRandInitAlt(seed, count, pid);
+
+    AcMeshDims dims       = acGetMeshDims(info);
+    VertexBufferArray vba = acVBACreate(mx, my, mz);
+    acVBAReset(0, &vba);
+    acLaunchKernel(randomize, 0, dims.n0, dims.n1, vba);
+    acVBASwapBuffers(&vba);
+
     ProfileBufferArray pba   = acPBACreate(mz);
-    AcBufferArray scratchpad = acBufferArrayCreate(12, mx * my);
+    AcBufferArray scratchpad = acBufferArrayCreate(14, mx * my);
     // acMapCross(vba, 0, (int3){radius, radius, 0}, (int3){radius + nx, radius + ny, 1},
     // scratchpad);
     acMapCrossReduce(vba, 0, scratchpad, pba);
 
     Timer t;
 
+    cudaProfilerStart();
     cudaDeviceSynchronize();
     timer_reset(&t);
     // for (size_t k = 0; k < mz; ++k)
@@ -75,8 +100,11 @@ main(int argc, char** argv)
 
     cudaDeviceSynchronize();
     timer_diff_print(t);
+    cudaProfilerStop();
+    cudaDeviceSynchronize();
 
     ERRCHK_CUDA_KERNEL_ALWAYS();
+    acRandQuit();
     acBufferArrayDestroy(&scratchpad);
     acPBADestroy(&pba);
     acVBADestroy(&vba);
