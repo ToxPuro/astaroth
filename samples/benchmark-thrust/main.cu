2ca03d6b1a742ce06bf4f9939cb5b3027711f50b
jpekkila <johannes.pekkila@protonmail.com>
2024-03-26 11:38:47 +0200

    Added a sample for benchmarking thrust

---
+++ samples/benchmark-thrust/main.cu
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "acc_runtime.h"
+#include "timer_hires.h"
+
+#include <thrust/device_vector.h>
+#include <thrust/functional.h>
+#include <thrust/transform_reduce.h>
+
+template <typename T> struct square {
+    __host__ __device__ T operator()(const T& x) const { return x * x; }
+};
+
+static void
+benchmark_thrust(const size_t mx, const size_t my, const size_t mz)
+{
+    std::vector<thrust::device_vector<double>> inputs(mz);
+    for (size_t k = 0; k < mz; ++k)
+        inputs[k] = thrust::device_vector<double>(mx * my, 1);
+
+    thrust::device_vector<double> results(mz);
+
+    for (size_t k = 0; k < mz; ++k)
+        results[k] = thrust::reduce(inputs[k].begin(), inputs[k].end());
+
+    Timer t;
+    cudaDeviceSynchronize();
+    timer_reset(&t);
+    for (size_t k = 0; k < mz; ++k)
+        results[k] = thrust::reduce(inputs[k].begin(), inputs[k].end());
+    cudaDeviceSynchronize();
+    timer_diff_print(t);
+}
+
+int
+main(int argc, char** argv)
+{
+    fprintf(stderr, "Usage: ./benchmark-thrust <nx> <ny> <nz>\n");
+    const size_t nx     = (argc > 1) ? (size_t)atol(argv[1]) : 32;
+    const size_t ny     = (argc > 2) ? (size_t)atol(argv[2]) : 32;
+    const size_t nz     = (argc > 3) ? (size_t)atol(argv[3]) : 32;
+    const size_t radius = STENCIL_ORDER / 2;
+    const size_t mx     = nx + 2 * radius;
+    const size_t my     = ny + 2 * radius;
+    const size_t mz     = nz + 2 * radius;
+
+    printf("Input parameters:\n");
+    printf("\tnx: %zu\n", nx);
+    printf("\tny: %zu\n", ny);
+    printf("\tnz: %zu\n", nz);
+    printf("\tradius: %zu\n", radius);
+    printf("\tmx: %zu\n", mx);
+    printf("\tmy: %zu\n", my);
+    printf("\tmz: %zu\n", mz);
+
+    benchmark_thrust(mx, my, mz);
+
+    VertexBufferArray vba = acVBACreate(mx, my, mz);
+    acMapInit(vba);
+    acMapCross(vba, 0, (int3){radius, radius, 0}, (int3){radius + nx, radius + ny, 1});
+
+    Timer t;
+
+    cudaDeviceSynchronize();
+    timer_reset(&t);
+    for (size_t k = 0; k < mz; ++k)
+        acMapCross(vba, 0, (int3){radius, radius, k}, (int3){radius + nx, radius + ny, k + 1});
+    cudaDeviceSynchronize();
+    timer_diff_print(t);
+
+    ERRCHK_CUDA_KERNEL_ALWAYS();
+    acMapQuit();
+    acVBADestroy(&vba);
+    return EXIT_SUCCESS;
+}
