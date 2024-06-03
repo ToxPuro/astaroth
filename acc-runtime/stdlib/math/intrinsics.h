ffc998e6f3ae611bd25c08408d4db25f98d6644f
ToxPuro <touko.puro@aalto.fi>
2024-06-03 19:46:08 +0300

    push stdlib/math

---
+++ acc-runtime/stdlib/math/intrinsics.h
@@ -0,0 +1,17 @@
+intrinsic sin
+intrinsic cos 
+intrinsic tan 
+
+intrinsic sinh 
+intrinsic cosh 
+intrinsic tanh
+
+intrinsic exp
+intrinsic sqrt 
+intrinsic fabs 
+intrinsic pow 
+
+intrinsic log
+intrinsic abs
+intrinsic atan2
+intrinsic isnan
