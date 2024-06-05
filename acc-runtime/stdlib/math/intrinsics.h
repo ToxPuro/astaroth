d1a806685a582888dff0d45cfb281b15eb7f943c
fredagent <fred.gent.ncl@gmail.com>
2024-06-05 20:32:39 +0300

    Smooth and max min

--- acc-runtime/stdlib/math/intrinsics.h
+++ acc-runtime/stdlib/math/intrinsics.h
@@ -1,18 +1,21 @@
-//NOTE: max and min are defined macros elsewhere in Astaroth and thus they do not appear here but are still usable
 intrinsic sin
 intrinsic cos 
 intrinsic tan 
+intrinsic atan2
 
 intrinsic sinh 
 intrinsic cosh 
 intrinsic tanh
 
 intrinsic exp
-intrinsic sqrt 
-intrinsic fabs 
-intrinsic pow 
-
 intrinsic log
+
+intrinsic sqrt 
+intrinsic pow 
+intrinsic fabs 
 intrinsic abs
-intrinsic atan2
+
 intrinsic isnan
+
+intrinsic min
+intrinsic max
