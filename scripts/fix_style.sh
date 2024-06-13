f208e695634e1e0653c8d65c2bc543ae8b1b9047
jpekkila <johannes.pekkila@protonmail.com>
2024-06-13 15:34:46 +0300

    Cherry-picked .gitignore and fix_style.sh from develop

--- scripts/fix_style.sh
+++ scripts/fix_style.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 if [[ $1 == "DO" && $2 == "IT!" ]]; then
-    find -name \*.h -o -name \*.cc -o -name \*.c | xargs clang-format -i -style=file
+    find ./include ./src ./acc-runtime -name "*.h" -o -name "*.cc" -o -name "*.c" -o -name "*.cu" -o -name "*.cuh" | xargs clang-format -i -style=file
     echo "It is done."
 else
-    find -name \*.h -o -name \*.cc -o -name \*.c
+    find ./include ./src ./acc-runtime -name "*.h" -o -name "*.cc" -o -name "*.c" -o -name "*.cu" -o -name "*.cuh"
     echo "I'm going to try to fix the style of these files."
     echo "If you're absolutely sure, give \"DO IT!\" as a parameter."
 fi
