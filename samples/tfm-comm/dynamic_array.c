e7d565e97fbca5719bec162145e80dc5acb02bf4
jpekkila <johannes.pekkila@protonmail.com>
2024-09-19 08:28:04 +0300

    Renamed dynamic_array datatype to something that can be easily searched and replaced later if/when changed into a template

--- samples/tfm-comm/dynamic_array.c
+++ samples/tfm-comm/dynamic_array.c
@@ -10,18 +10,18 @@ array_create(const size_t capacity)
     DynamicArray arr = (DynamicArray){
         .len      = 0,
         .capacity = capacity,
-        .data     = (datatype*)malloc(sizeof(arr.data[0]) * capacity),
+        .data     = (DATYPE*)malloc(sizeof(arr.data[0]) * capacity),
     };
     ERRCHK(arr.data);
     return arr;
 }
 
 void
-array_append(const datatype element, DynamicArray* array)
+array_append(const DATYPE element, DynamicArray* array)
 {
     if (array->len == array->capacity) {
         array->capacity += 128;
-        array->data = (datatype*)realloc(array->data, sizeof(array->data[0]) * array->capacity);
+        array->data = (DATYPE*)realloc(array->data, sizeof(array->data[0]) * array->capacity);
         WARNING("Array too small, reallocated");
         ERRCHK(array->data);
     }
@@ -30,7 +30,7 @@ array_append(const datatype element, DynamicArray* array)
 }
 
 void
-array_append_multiple(const size_t count, const datatype* elements, DynamicArray* array)
+array_append_multiple(const size_t count, const DATYPE* elements, DynamicArray* array)
 {
     for (size_t i = 0; i < count; ++i)
         array_append(elements[i], array);
--- samples/tfm-comm/dynamic_array.h
+++ samples/tfm-comm/dynamic_array.h
@@ -1,18 +1,28 @@
 #pragma once
 #include <stddef.h>
 
-typedef size_t datatype;
+typedef size_t DATYPE;
 
 typedef struct {
     size_t len;
     size_t capacity;
-    datatype* data;
+    DATYPE* data;
 } DynamicArray;
 
 DynamicArray array_create(const size_t capacity);
 
-void array_append(const datatype element, DynamicArray* array);
+void array_append(const DATYPE element, DynamicArray* array);
 
-void array_append_multiple(const size_t count, const datatype* elements, DynamicArray* array);
+void array_append_multiple(const size_t count, const DATYPE* elements, DynamicArray* array);
 
 void array_destroy(DynamicArray* array);
+
+// typedef struct {
+//     size_t ndims;
+//     size_t* shape;
+// } Shape;
+
+// typedef struct {
+//     DynamicArray array;
+//     Shape shape;
+// } NdArray;
