eaed158a647c3d78b433dc5f9647fe111d35d06a
ToxPuro <touko.puro@aalto.fi>
2024-09-26 17:52:10 +0300

    commit builtin intrinsics

---
+++ acc-runtime/built-in/intrinsics.h
@@ -0,0 +1,24 @@
+real intrinsic sin(real) 
+real intrinsic cos(real) 
+real intrinsic tan(real) 
+real intrinsic atan2(real)
+
+real intrinsic sinh(real) 
+real intrinsic cosh(real) 
+real intrinsic tanh(real)
+
+real intrinsic exp(real)
+real intrinsic log(real)
+
+real intrinsic sqrt(real) 
+real intrinsic pow(real) 
+real intrinsic fabs(real)
+
+
+intrinsic min
+intrinsic max
+
+intrinsic ceil
+real intrinsic random_uniform
+real intrinsic atomicAdd
+
