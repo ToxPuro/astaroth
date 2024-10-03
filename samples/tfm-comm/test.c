3bf64660522da921e55c0757399af940093977eb
jpekkila <johannes.pekkila@protonmail.com>
2024-10-03 12:24:35 +0300

    Segment and comm tests to test.c

--- samples/tfm-comm/test.c
+++ samples/tfm-comm/test.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "comm.h"
 #include "dynarr.h"
 #include "math_utils.h"
 #include "mpi_utils.h"
 #include "pack.h"
 #include "partition.h"
 #include "print.h"
+#include "segment.h"
 #include "type_conversion.h"
 
 int
 main(void)
 {
+    test_comm();
     test_dynarr();
     test_math_utils();
     test_mpi_utils();
     test_pack();
     test_partition();
+    test_segment();
     return EXIT_SUCCESS;
 }
