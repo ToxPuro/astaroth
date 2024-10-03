f391edd53cdd5d3340deac13b058586c0b0d4d78
jpekkila <johannes.pekkila@protonmail.com>
2024-10-03 12:24:20 +0300

    Comments to mpi_utils.h

--- samples/tfm-comm/mpi_utils.h
+++ samples/tfm-comm/mpi_utils.h
@@ -7,6 +7,7 @@ void to_mpi_format(const size_t ndims, const size_t* dims, int* mpi_dims);
 /** Translates an array in MPI (rows first) format to Astaroth format (columns first) */
 void to_astaroth_format(const size_t ndims, const int* mpi_dims, size_t* dims);
 
+/** Returns the next positive integer that can be used as a tag */
 int get_tag(void);
 
 void test_mpi_utils(void);
