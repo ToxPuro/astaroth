edcff8f5aaced2c1de96019558b7d410a7d9f52f
jpekkila <johannes.pekkila@protonmail.com>
2024-10-10 14:36:14 +0300

    tfm-comm: packet in a separate file

---
+++ samples/tfm-comm/packet.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <stddef.h>
+
+#include <mpi.h>
+
+#include "buf.h"
+#include "segment.h"
+
+typedef struct {
+    Segment segment; // Shape of the data block the packet represents
+    Buffer buffer;   // Buffer holding the data
+    MPI_Request req; // MPI request for handling synchronization
+} Packet;
+
+Packet packet_create(const size_t ndims, const size_t* dims, const size_t* offset,
+                     const size_t nbuffers);
+
+void packet_wait(Packet* packet);
+
+void packet_destroy(Packet* packet);
