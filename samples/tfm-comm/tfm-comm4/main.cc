d04456c05c2f95c884f57a753a76d9ab929e31d0
jpekkila <johannes.pekkila@protonmail.com>
2024-10-18 16:59:27 +0300

    cpp segment to tfm-comm4

--- samples/tfm-comm/tfm-comm4/main.cc
+++ samples/tfm-comm/tfm-comm4/main.cc
@@ -2,6 +2,10 @@
 
 #include "static_array.h"
 
+#include "segment.h"
+
+#include "print.h"
+
 int
 main(void)
 {
@@ -10,6 +14,12 @@ main(void)
     Ntuple<uint64_t> global_nn = {4, 4};
     Ntuple<uint64_t> local_nn(global_nn.count);
     Ntuple<uint64_t> global_nn_offset(global_nn.count);
+
+    Shape dims    = {1, 2, 3};
+    Shape subdims = {1, 2, 3};
+    Index offset  = {4, 5, 6};
+    Segment segment(dims, subdims, offset);
+    PRINTD(segment);
 
     acCommSetup(global_nn.count, global_nn.data, local_nn.data, global_nn_offset.data);
     acCommPrint();
