819c83ddda7a7a66d762866a59ac9622d8ad8910
jpekkila <johannes.pekkila@protonmail.com>
2024-10-24 14:14:40 +0300

    Switched from typedef to using syntax for consistency due to more flexibility with templated types

--- samples/tfm-comm/tfm-comm6/shape.h
+++ samples/tfm-comm/tfm-comm6/shape.h
@@ -3,17 +3,8 @@
 #include "static_array.h"
 
 constexpr size_t MAX_NDIMS = 4;
-typedef StaticArray<uint64_t, MAX_NDIMS> Index;
-typedef StaticArray<uint64_t, MAX_NDIMS> Shape;
-typedef StaticArray<int, MAX_NDIMS> MPIIndex;
-typedef StaticArray<int, MAX_NDIMS> MPIShape;
 
-// struct Shape : public StaticArray<uint64_t, MAX_NDIMS> {
-
-//     // Initialize Shape to 1 by default
-//     __host__ __device__ Shape() : StaticArray<uint64_t, MAX_NDIMS>(1) {}
-
-//     // Inherit the rest of the functions from StaticArray
-//     using StaticArray<uint64_t, MAX_NDIMS>::StaticArray;
-// };
-// typedef StaticArray<uint64_t, MAX_NDIMS> Index;
+using Index    = StaticArray<uint64_t, MAX_NDIMS>;
+using Shape    = StaticArray<uint64_t, MAX_NDIMS>;
+using MPIIndex = StaticArray<int, MAX_NDIMS>;
+using MPIShape = StaticArray<int, MAX_NDIMS>;
