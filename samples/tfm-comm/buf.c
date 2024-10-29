86c4a9545c0af2a9a9fddd7f85352f5c3f6c20c4
jpekkila <johannes.pekkila@protonmail.com>
2024-10-29 09:37:53 +0200

    committing some superficial changes to avoid the need for stashing

--- samples/tfm-comm/buf.c
+++ samples/tfm-comm/buf.c
@@ -1,19 +1,23 @@
 #include "buf.h"
 
-#include "nalloc.h"
+#include "errchk.h"
+// #include "nalloc.h"
 
 Buffer
 buffer_create(const size_t count)
 {
-    Buffer buffer;
-    buffer.count = count;
-    ncalloc(count, buffer.data);
+    Buffer buffer = {
+        .count = count,
+        .data  = calloc(count, sizeof(buffer.data[0])),
+    };
+    ERRCHK(buffer.data);
     return buffer;
 }
 
 void
 buffer_destroy(Buffer* buffer)
 {
-    ndealloc(buffer->data);
+    free(buffer->data);
+    buffer->data  = NULL;
     buffer->count = 0;
 }
--- samples/tfm-comm/stash2/gen-type-conversions.py
+++ samples/tfm-comm/stash2/gen-type-conversions.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 # %%
 
-types = ["size_t", "int64_t", "int"]
+types = ["size_t", "int64_t", "int", "double"]
 
 for a in types:
-    print(f"#define as_{a}_array(count, a, b) _Generic((a), ", end="")
+    print(f"#define as_{a}(count, a, b) _Generic((a), ", end="")
     for b in types:
         if a == b:
             continue
-        print(f"{b}*: {b}_as_{a}_array, ", end="")
-        print(f"const {b}*: {b}_as_{a}_array", end="")
+        print(f"{b}*: {b}_as_{a}, ", end="")
+        print(f"const {b}*: {b}_as_{a}", end="")
         if b != types[-1]:
             print(", ", end="")
     print(")(count, a, b)")
@@ -19,7 +19,7 @@ for a in types:
     for b in types:
         if a == b:
             continue
-        print(f"void {b}_as_{a}_array(const size_t count, const {b}* a, {a}* b);")
+        print(f"void {b}_as_{a}(const size_t count, const {b}* a, {a}* b);")
 
 print("---")
 
@@ -28,5 +28,5 @@ for a in types:
         if a == b:
             continue
         print(
-            f"void {b}_as_{a}_array(const size_t count, const {b}* a, {a}* b) {{ for (size_t i=0; i<count;++i) b[i] = as_{a}(a[i]); }}"
+            f"void {b}_as_{a}(const size_t count, const {b}* a, {a}* b) {{ for (size_t i=0; i<count;++i) b[i] = as_{a}(a[i]); }}"
         )
