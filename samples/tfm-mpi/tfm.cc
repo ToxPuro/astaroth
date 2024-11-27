558d2520a8d8b432d80a37f1215b661535fec8f7
jpekkila <johannes.pekkila@aalto.fi>
2024-11-27 08:44:44 +0200

    Basic allocations to tfm-mpi, though kernel launch fails for some reason

--- samples/tfm-mpi/tfm.cc
+++ samples/tfm-mpi/tfm.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include <functional>
 
 #include "acc-runtime/api/acc_runtime.h"
 
@@ -16,6 +17,9 @@
 #include <mpi.h>
 #include "acc-comm/mpi_utils.h"
 
+// #include "astaroth.h"
+#include "stencil_loader.h"
+
 #define ERRCHK_ACM(errcode)                                                                        \
     do {                                                                                           \
         const ACM_Errorcode _tmp_acm_api_errcode_ = (errcode);                                     \
@@ -31,7 +35,7 @@
     do {                                                                                           \
         const AcResult _tmp_ac_api_errcode_ = (errcode);                                     \
         if (_tmp_ac_api_errcode_ != AC_SUCCESS) {                                      \
-            errchk_print_error(__func__, __FILE__, __LINE__, #errcode);                            \
+            errchk_print_error(__func__, __FILE__, __LINE__, #errcode, "Astaroth error");                            \
             errchk_print_stacktrace();                                                             \
             MPI_Abort(MPI_COMM_WORLD, -1);                                                         \
         }                                                                                          \
@@ -51,6 +55,61 @@ static int get_local_box_size(const size_t ndims, const uint64_t* global_nn, con
     }
     
     return 0;
+}
+
+// static VertexBufferArray* vba_alloc(const Shape& mm)
+// {
+//     auto* vba = new VertexBufferArray;
+//     *vba = acVBACreate(mm[0], mm[1], mm[2]);
+//     return vba;
+// }
+
+// static void vba_dealloc(VertexBufferArray* vba)
+// {
+//     acVBADestroy(vba);
+//     delete vba;
+// }
+
+using vba_ptr_t = std::unique_ptr<VertexBufferArray, std::function<void(VertexBufferArray*)>>;
+
+static auto
+make_vba(const Shape& mm)
+{
+    return vba_ptr_t{
+        [&mm](){
+            auto* vba = new VertexBufferArray;
+            *vba = acVBACreate(mm[0], mm[1], mm[2]);
+            return vba;
+        }(), 
+        [](VertexBufferArray* vba){
+            acVBADestroy(vba); 
+            delete vba;
+        }
+    };
+}
+
+// using device_ptr_t = std::unique_ptr<Device, std::function<void(Device*)>>;
+
+// static auto
+// make_device(const int id, const AcMeshInfo& local_info)
+// {
+//     return device_ptr_t{
+//         [id, &local_info](){
+//             auto* device = new Device;
+//             ERRCHK_AC(acDeviceCreate(id, local_info, device));
+//             return device;
+//         }(), 
+//         [](Device* device){
+//             acDeviceDestroy(*device); 
+//             delete device;
+//         }
+//     };
+// }
+
+static int3 make_int3(const Shape& shape)
+{
+    ERRCHK(shape.size() == 3);
+    return {as<int>(shape[0]), as<int>(shape[1]), as<int>(shape[2])};
 }
 
 static AcMeshInfo get_local_mesh_info(const MPI_Comm cart_comm, const AcMeshInfo info)
@@ -117,20 +176,20 @@ int main(int argc, char* argv[])
         ERRCHK(acPrintArguments(args) == 0);
 
         // Load configuration
-        AcMeshInfo info{};
+        AcMeshInfo raw_info{};
         if (args.config_path) {
-            ERRCHK(acParseINI(args.config_path, &info) == 0);
+            ERRCHK(acParseINI(args.config_path, &raw_info) == 0);
         } else {
             const std::string config_path{AC_DEFAULT_TFM_CONFIG};
             PRINT_LOG("No config path supplied, using %s", config_path.c_str());
-            ERRCHK(acParseINI(config_path.c_str(), &info) == 0);
+            ERRCHK(acParseINI(config_path.c_str(), &raw_info) == 0);
         }
-        ERRCHK(acPrintMeshInfo(info) == 0);
+        // ERRCHK(acPrintMeshInfo(raw_info) == 0);
 
         const uint64_t global_nn_c[NDIMS]{
-            as<uint64_t>(info.int_params[AC_global_nx]), 
-            as<uint64_t>(info.int_params[AC_global_ny]), 
-            as<uint64_t>(info.int_params[AC_global_nz])
+            as<uint64_t>(raw_info.int_params[AC_global_nx]), 
+            as<uint64_t>(raw_info.int_params[AC_global_ny]), 
+            as<uint64_t>(raw_info.int_params[AC_global_nz])
         };
         PRINT_DEBUG_ARRAY(NDIMS, global_nn_c);
 
@@ -139,8 +198,8 @@ int main(int argc, char* argv[])
         ERRCHK_ACM(ACM_MPI_Cart_comm_create(MPI_COMM_WORLD, NDIMS, global_nn_c, &cart_comm));
 
         // Fill the local mesh configuration (overwrite the earlier info
-        info = get_local_mesh_info(cart_comm, info);
-        ERRCHK(acPrintMeshInfo(info) == 0);
+        const AcMeshInfo local_info = get_local_mesh_info(cart_comm, raw_info);
+        ERRCHK(acPrintMeshInfo(local_info) == 0);
 
         // Select device
         int original_rank, nprocs;
@@ -152,29 +211,48 @@ int main(int argc, char* argv[])
         ERRCHK_CUDA_API(cudaSetDevice(original_rank % device_count));
         ERRCHK_CUDA_API(cudaDeviceSynchronize());
     
-        // Allocate buffers
+        // Setup mesh dimensions
         const Shape global_nn {
-            as<uint64_t>(info.int_params[AC_global_nx]), 
-            as<uint64_t>(info.int_params[AC_global_ny]), 
-            as<uint64_t>(info.int_params[AC_global_nz])
+            as<uint64_t>(local_info.int_params[AC_global_nx]), 
+            as<uint64_t>(local_info.int_params[AC_global_ny]), 
+            as<uint64_t>(local_info.int_params[AC_global_nz])
         };
         const Shape local_nn {
-            as<uint64_t>(info.int_params[AC_nx]), 
-            as<uint64_t>(info.int_params[AC_ny]), 
-            as<uint64_t>(info.int_params[AC_nz])
+            as<uint64_t>(local_info.int_params[AC_nx]), 
+            as<uint64_t>(local_info.int_params[AC_ny]), 
+            as<uint64_t>(local_info.int_params[AC_nz])
         };
         const Index rr {
             as<uint64_t>((STENCIL_WIDTH - 1)/2), 
             as<uint64_t>((STENCIL_HEIGHT - 1)/2), 
             as<uint64_t>((STENCIL_DEPTH - 1)/2)
         };
-        const Shape local_mm{as<uint64_t>(2) * rr + local_nn};
+        const Shape local_mm {
+            as<uint64_t>(local_info.int_params[AC_mx]), 
+            as<uint64_t>(local_info.int_params[AC_my]), 
+            as<uint64_t>(local_info.int_params[AC_mz])
+        };
 
 
-        // std::unique_ptr<VertexBufferArray, std::decltype(&acVBADestroy)> vba{acVBACreate(1,1,1), &acVBADestroy};
-        // auto vba = std::unique_ptr<VertexBufferArray, std::decltype(&acVBADestroy)>(acVBACreate(local_mm[0], local_mm[1], local_mm[2]), &acVBADestroy);
-        // VertexBufferArray vba = acVBACreate(local_mm[0], local_mm[1], local_mm[2]);
-        // ERRCHK_AC(acVBADestroy(&vba));
+        // Setup device memory
+        const cudaStream_t stream = 0;
+        AcReal stencils[NUM_STENCILS][STENCIL_DEPTH][STENCIL_HEIGHT][STENCIL_WIDTH];
+        get_stencil_coeffs(local_info, stencils);
+        
+        auto vba_ptr = make_vba(local_mm);
+        ERRCHK_AC(acLoadMeshInfo(local_info, stream));
+        ERRCHK_AC(acLoadRealUniform(stream, AC_dt, 1e-3));
+        ERRCHK_AC(acLoadIntUniform(stream, AC_step_number, 0));
+        ERRCHK_AC(acLoadStencils(stream, stencils));
+        ERRCHK_CUDA_API(cudaDeviceSynchronize());
+
+        ERRCHK_AC(acVBAReset(stream, vba_ptr.get()));
+        ERRCHK_CUDA_API(cudaDeviceSynchronize());
+
+        // ERRCHK_AC(acLaunchKernel(singlepass_solve, stream, make_int3(rr), make_int3(rr + Shape(rr.size(), 1)), *(vba_ptr.get())));
+        // ERRCHK_AC(acLaunchKernel(singlepass_solve, stream, make_int3(rr), make_int3(rr + local_nn), *vba_ptr));
+        ERRCHK_CUDA_API(cudaStreamSynchronize(stream));
+        ERRCHK_CUDA_API(cudaDeviceSynchronize());
 
 
         ERRCHK_ACM(ACM_MPI_Cart_comm_destroy(&cart_comm));
