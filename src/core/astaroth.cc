ToxPuro/astaroth

commit 48cd963df12f5121e8e4185f0aa88750167a78b2

Now also setting the multi-GPU built-in parameters with acHostUpdateBuiltinParams

src/core/astaroth.cc
@@ -277,6 +277,14 @@ acHostUpdateBuiltinParams(AcMeshInfo* config)
     config->int_params[AC_nxy]  = config->int_params[AC_nx] * config->int_params[AC_ny];
     config->int_params[AC_nxyz] = config->int_params[AC_nxy] * config->int_params[AC_nz];
 
+    /* Multi-GPU params */
+    config->int3_params[AC_multigpu_offset] = (int3){0, 0, 0};
+    config->int3_params[AC_global_grid_n]   = (int3){
+        config->int_params[AC_nx],
+        config->int_params[AC_ny],
+        config->int_params[AC_nz],
+    };
+
     return AC_SUCCESS;
 }
 

commit b84a11332a87ab34fb0b74e64db8888e896c741a

An update to acHostUpdateBuiltinParams in 48cd963df12f5121e8e4185f0aa88750167a78b2 broke multi-GPU because a call to the function after setting the decomposition would overwrite multi-GPU offset and global_grid_n

src/core/astaroth.cc
@@ -277,14 +277,6 @@ acHostUpdateBuiltinParams(AcMeshInfo* config)
     config->int_params[AC_nxy]  = config->int_params[AC_nx] * config->int_params[AC_ny];
     config->int_params[AC_nxyz] = config->int_params[AC_nxy] * config->int_params[AC_nz];
 
-    /* Multi-GPU params */
-    config->int3_params[AC_multigpu_offset] = (int3){0, 0, 0};
-    config->int3_params[AC_global_grid_n]   = (int3){
-        config->int_params[AC_nx],
-        config->int_params[AC_ny],
-        config->int_params[AC_nz],
-    };
-
     return AC_SUCCESS;
 }
 