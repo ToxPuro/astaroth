d6dbf0674760c6ceb04954fcbd6bd2117556cc23
jpekkila <johannes.pekkila@protonmail.com>
2025-01-19 18:43:45 +0200

    Nested vectors now printable with print_debug.h

--- acc-comm/acm/detail/print_debug.h
+++ acc-comm/acm/detail/print_debug.h
@@ -5,25 +5,6 @@
 #include <memory>
 #include <vector>
 
-// template <typename T>
-// std::ostream&
-// operator<<(std::ostream& os, const std::vector<T>& vec)
-// {
-//     if (vec.size() < 8) {
-//         os << "{";
-//         for (size_t i{0}; i < vec.size(); ++i)
-//             os << vec[i] << (i + 1 < vec.size() ? ", " : "");
-//         os << "}";
-//     }
-//     else {
-//         os << "{\n";
-//         for (size_t i{0}; i < vec.size(); ++i)
-//             os << "\t" << i << ": " << vec[i] << (i + 1 < vec.size() ? ", " : "") << std::endl;
-//         os << "}";
-//     }
-//     return os;
-// }
-
 template <typename T>
 void
 print_debug(const std::string& label, const T& value)
@@ -41,19 +22,57 @@ print_debug_array(const std::string& label, const size_t count, const T* arr)
     std::cout << "}" << std::endl;
 }
 
-template <typename T>
-void
-print_debug_vector(const std::string& label, const std::vector<T>& vec)
+namespace print::debug {
+static inline void
+indent(const size_t depth)
 {
-    std::cout << label << ": {";
-    for (size_t i{0}; i < vec.size(); ++i)
-        std::cout << vec[i] << (i + 1 < vec.size() ? ", " : "");
+    for (size_t i = 0; i < depth; ++i)
+        std::cout << "    ";
+}
+
+template <typename T>
+static inline void
+print(const T& elem)
+{
+    std::cout << elem;
+}
+
+template <typename T>
+static inline void
+print(const std::vector<T>& vec, const size_t depth = 0)
+{
+    print::debug::indent(depth);
+    std::cout << "{ ";
+    for (const auto& elem : vec)
+        std::cout << elem << " ";
     std::cout << "}" << std::endl;
+}
+
+template <typename T>
+static inline void
+print(const std::vector<std::vector<T>>& vec, const size_t depth = 0)
+{
+    print::debug::indent(depth);
+    std::cout << "{" << std::endl;
+    for (const auto& elem : vec) {
+        print(elem, depth + 1);
+    }
+    print::debug::indent(depth);
+    std::cout << "}" << std::endl;
+}
+} // namespace print::debug
+
+template <typename T>
+static inline void
+print_debug(const std::string& label, const std::vector<T>& vec)
+{
+    std::cout << label << ":" << std::endl;
+    print::debug::print(vec);
 }
 
 #define PRINT_DEBUG(value) (print_debug(#value, (value)))
 #define PRINT_DEBUG_ARRAY(count, arr) (print_debug_array(#arr, (count), (arr)))
-#define PRINT_DEBUG_VECTOR(vec) (print_debug_vector(#vec, (vec)))
+#define PRINT_DEBUG_VECTOR(vec) (print_debug(#vec, (vec)))
 
 template <typename T>
 void
