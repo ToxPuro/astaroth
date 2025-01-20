01aa579e76ec1b646b3fe075d09590d38399a679
jpekkila <johannes.pekkila@protonmail.com>
2025-01-20 08:02:30 +0200

    Explicit cast and cleanup to acm mpi-test.cc

--- acc-comm/tests/mpi-test.cc
+++ acc-comm/tests/mpi-test.cc
@@ -37,7 +37,7 @@ test_reduce(const MPI_Comm& cart_comm, const Shape& global_nn)
         // coordinates on that axis
         for (size_t i{0}; i < count; ++i) {
             const size_t nprocs_on_axis{nprocs / decomp[axis]};
-            ERRCHK(tmp[i] == value * nprocs_on_axis);
+            ERRCHK(tmp[i] == value * as<int>(nprocs_on_axis));
         }
     }
 }
@@ -54,36 +54,6 @@ main()
         test_reduce<ac::mr::pinned_host_memory_resource>(cart_comm, global_nn);
         test_reduce<ac::mr::pinned_write_combined_host_memory_resource>(cart_comm, global_nn);
         test_reduce<ac::mr::device_memory_resource>(cart_comm, global_nn);
-
-        // Shape decomp{ac::mpi::get_decomposition(cart_comm)};
-        // Index coords{ac::mpi::get_coords(cart_comm)};
-        // const size_t nprocs{prod(decomp)};
-
-        // // Checks that the reduce sum is the sum of all processes along a specific axis
-        // for (size_t axis{0}; axis < global_nn.size(); ++axis) {
-        //     constexpr size_t count{10};
-        //     const int value{as<int>((coords[axis] + 1) * nprocs)};
-        //     ac::buffer<int, ac::mr::host_memory_resource> buf{count, value};
-
-        //     ac::mpi::reduce(cart_comm,
-        //                     ac::mpi::get_dtype<int>(),
-        //                     MPI_SUM,
-        //                     axis,
-        //                     buf.size(),
-        //                     buf.data());
-
-        //     PRINT_DEBUG(decomp);
-        //     PRINT_DEBUG(coords);
-        //     PRINT_DEBUG(nprocs);
-        //     PRINT_DEBUG_ARRAY(buf.size(), buf.data());
-
-        //     // E.g. 4 procs on axis, the value in the buffer of each proc corresponds to its
-        //     // coordinates on that axis
-        //     for (size_t i{0}; i < count; ++i) {
-        //         const size_t nprocs_on_axis{nprocs / decomp[axis]};
-        //         ERRCHK(buf[i] == value * nprocs_on_axis);
-        //     }
-        // }
 
         ac::mpi::cart_comm_destroy(cart_comm);
     }
