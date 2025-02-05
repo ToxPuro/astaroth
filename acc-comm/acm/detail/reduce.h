ed576d6f7c61c9626fe9cf9db580a54567dde063
jpekkila <johannes.pekkila@aalto.fi>
2025-02-05 12:20:47 +0200

    Working device-side reduce task and autotest

--- acc-comm/acm/detail/reduce.h
+++ acc-comm/acm/detail/reduce.h
@@ -1,13 +1,3 @@
 #pragma once
 
-enum class ReduceType { sum, max, min };
-
-template <typename T> class ReduceTask {
-  private:
-    // Local memory
-
-  public:
-    ReduceTask(const size_t count) {}
-};
-
 void test_reduce();
