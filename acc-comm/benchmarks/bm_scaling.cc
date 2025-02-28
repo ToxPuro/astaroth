b1ae5da9c57947acc36d585d165c08f457455d80
jpekkila <johannes.pekkila@protonmail.com>
2025-02-28 16:36:42 +0200

    Added host/device verification of packed halo exchange

--- acc-comm/benchmarks/bm_scaling.cc
+++ acc-comm/benchmarks/bm_scaling.cc
@@ -8,6 +8,8 @@
 #include "acm/detail/mpi_utils.h"
 #include "acm/detail/ndbuffer.h"
 
+#include <array>
+
 /** Verify halo exchange
  * Strategy:
  *     1) Set global mesh to iota
@@ -26,7 +28,8 @@ verify(const MPI_Comm& cart_comm, const ac::shape& global_nn, const ac::index& r
 
     const auto           local_mm{ac::mpi::get_local_mm(cart_comm, global_nn, rr)};
     const auto           local_nn{ac::mpi::get_local_nn(cart_comm, global_nn)};
-    ac::host_ndbuffer<T> lmesh{local_mm, -1};
+    ac::host_ndbuffer<T> host_lmesh{local_mm, -1};
+    auto                 lmesh{host_lmesh.to_device()};
 
     ac::mpi::scatter_advanced(cart_comm,
                               ac::mpi::get_dtype<T>(),
@@ -46,60 +49,74 @@ verify(const MPI_Comm& cart_comm, const ac::shape& global_nn, const ac::index& r
     // ac::comm::async_halo_exchange_task<T> he{local_mm, local_nn, rr, 1};
     // he.launch(cart_comm, {lmesh.get()});
     // he.wait({lmesh.get()});
+    host_lmesh = lmesh.to_host();
 
     const auto global_nn_offset{ac::mpi::get_global_nn_offset(cart_comm, global_nn)};
     for (uint64_t i{0}; i < prod(local_mm); ++i) {
         const auto lcoords{(global_nn + global_nn_offset + ac::to_spatial(i, local_mm) - rr) %
                            global_nn};
         const auto linear_idx{to_linear(lcoords, global_nn)};
-        ERRCHK(within_machine_epsilon(lmesh[i], static_cast<T>(linear_idx)));
+        ERRCHK(within_machine_epsilon(host_lmesh[i], static_cast<T>(linear_idx)));
     }
 
     return 0;
 }
 
-/*
 template <typename T>
 static int
 verify_packed(const MPI_Comm& cart_comm, const ac::shape& global_nn, const ac::index& rr)
 {
-    ac::host_ndbuffer<T> gmesh0{global_nn};
-    std::iota(gmesh0.begin(), gmesh0.end(), 0);
+    std::array<ac::host_ndbuffer<T>, 4> gbufs{
+        ac::host_ndbuffer<T>{global_nn},
+        ac::host_ndbuffer<T>{global_nn},
+        ac::host_ndbuffer<T>{global_nn},
+        ac::host_ndbuffer<T>{global_nn},
+    };
+    for (size_t i{0}; i < gbufs.size(); ++i)
+        std::iota(gbufs[i].begin(), gbufs[i].end(), i * prod(global_nn));
 
-    const auto           local_mm{ac::mpi::get_local_mm(cart_comm, global_nn, rr)};
-    const auto           local_nn{ac::mpi::get_local_nn(cart_comm, global_nn)};
-    ac::host_ndbuffer<T> lmesh{local_mm, -1};
+    const auto                          local_mm{ac::mpi::get_local_mm(cart_comm, global_nn, rr)};
+    const auto                          local_nn{ac::mpi::get_local_nn(cart_comm, global_nn)};
+    std::array<ac::host_ndbuffer<T>, 4> lbufs{
+        ac::device_ndbuffer<T>{local_mm, -1},
+        ac::device_ndbuffer<T>{local_mm, -1},
+        ac::device_ndbuffer<T>{local_mm, -1},
+        ac::device_ndbuffer<T>{local_mm, -1},
+    };
 
-    ac::mpi::scatter_advanced(cart_comm,
-                              ac::mpi::get_dtype<T>(),
-                              global_nn,
-                              ac::make_index(global_nn.size(), 0),
-                              gmesh0.data(),
-                              local_mm,
-                              local_nn,
-                              rr,
-                              lmesh.data());
+    for (size_t i{0}; i < gbufs.size(); ++i)
+        ac::mpi::scatter_advanced(cart_comm,
+                                  ac::mpi::get_dtype<T>(),
+                                  global_nn,
+                                  ac::make_index(global_nn.size(), 0),
+                                  gbufs[i].data(),
+                                  local_mm,
+                                  local_nn,
+                                  rr,
+                                  lbufs[i].data());
 
-    auto reqs{launch_halo_exchange(cart_comm, local_mm, local_nn, rr, lmesh.data(), lmesh.data())};
-    while (!reqs.empty()) {
-        ac::mpi::request_wait_and_destroy(&reqs.back());
-        reqs.pop_back();
-    }
-    // ac::comm::async_halo_exchange_task<T> he{local_mm, local_nn, rr, 1};
-    // he.launch(cart_comm, {lmesh.get()});
-    // he.wait({lmesh.get()});
+    std::vector<ac::mr::host_pointer<T>> lptrs;
+    for (auto& lbuf : lbufs)
+        lptrs.push_back(lbuf.get());
+
+    ac::comm::async_halo_exchange_task<T> he{local_mm, local_nn, rr, lbufs.size()};
+    he.launch(cart_comm, lptrs);
+    he.wait(lptrs);
 
     const auto global_nn_offset{ac::mpi::get_global_nn_offset(cart_comm, global_nn)};
-    for (uint64_t i{0}; i < prod(local_mm); ++i) {
-        const auto lcoords{(global_nn + global_nn_offset + ac::to_spatial(i, local_mm) - rr) %
-                           global_nn};
-        const auto linear_idx{to_linear(lcoords, global_nn)};
-        ERRCHK(within_machine_epsilon(lmesh[i], static_cast<T>(linear_idx)));
+    for (uint64_t j{0}; j < lbufs.size(); ++j) {
+        const auto lbuf{lbufs[j].to_host()};
+        for (uint64_t i{0}; i < prod(local_mm); ++i) {
+            const auto lcoords{(global_nn + global_nn_offset + ac::to_spatial(i, local_mm) - rr) %
+                               global_nn};
+            const auto linear_idx{to_linear(lcoords, global_nn)};
+            ERRCHK(
+                within_machine_epsilon(lbuf[i], static_cast<T>(linear_idx + j * prod(global_nn))));
+        }
     }
 
     return 0;
 }
-*/
 
 /** Benchmark async MPI halo exhange in a Cartesian grid*/
 static void
@@ -145,6 +162,7 @@ main()
                                                      ac::mpi::RankReorderMethod::hierarchical)};
 
         verify<int>(cart_comm, global_nn, rr);
+        verify_packed<int>(cart_comm, global_nn, rr);
         bm_halo_exchange(cart_comm, global_nn, rr);
         ac::mpi::cart_comm_destroy(&cart_comm);
     }
