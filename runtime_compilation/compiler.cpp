450b65c3692623b4159e2388417c436eed7f356a
ToxPuro <touko.puro@aalto.fi>
2025-03-13 14:50:11 +0200

    run cmake again now only when recompiling fully; because of this passing different cmake options to runtime compilation forces full rebuild; benefit is that when not running cmake make has less false positives about recompilation;

--- runtime_compilation/compiler.cpp
+++ runtime_compilation/compiler.cpp
@@ -87,6 +87,48 @@ file_exists(const char* filename)
   return (stat (filename, &buffer) == 0);
 }
 void
+check_for_cmake()
+{
+   char cmd[2*10000];
+   sprintf(cmd,"cmake --help > /dev/null");
+   const int retval = system(cmd);
+   if(retval)
+   {
+   	fprintf(stderr,"%s","Did not find cmake\n");
+   	fflush(stdout);
+   	fflush(stderr);
+   	exit(EXIT_FAILURE);
+   }
+}
+void
+run_cmake(const char* compilation_string, const char* log_dst)
+{
+  
+  char cmd[2*10000];
+  sprintf(cmd,"cd %s && cmake -DREAD_OVERRIDES=ON -DBUILD_SHARED_LIBS=ON -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DACC_COMPILER_PATH=%s %s %s &> %s",
+         runtime_astaroth_build_path, acc_compiler_path, compilation_string,astaroth_base_path,log_dst);
+  const int retval = system(cmd);
+  if(retval)
+  {
+  	fprintf(stderr,"%s %d\n","Fatal was not able to run cmake:",retval);
+  	fprintf(stderr,"Cmake command: %s\n",cmd);
+  	fflush(stdout);
+  	fflush(stderr);
+  	exit(EXIT_FAILURE);
+  }
+  sprintf(cmd,"echo %s > %s", compilation_string, previous_cmake_options_path);
+  const int echo_retval = system(cmd);
+  if(echo_retval)
+  {
+  	fprintf(stderr,"%s %d\n","Was not able to store cmake options:",retval);
+  	fprintf(stderr,"Storing command: %s\n",cmd);
+  	fflush(stdout);
+  	fflush(stderr);
+  	exit(EXIT_FAILURE);
+  }
+
+}
+void
 acCompile(const char* compilation_string, const char* target, AcMeshInfo mesh_info)
 {
 	check_that_built_ins_loaded(mesh_info.run_consts);
@@ -118,18 +160,23 @@ acCompile(const char* compilation_string, const char* target, AcMeshInfo mesh_in
 		else
 			sprintf(log_dst,"%s/%s",cwd,mesh_info.runtime_compilation_log_dst);
 
+		check_for_cmake();
 		sprintf(cmd,"diff tmp_astaroth_run_consts.h %s",AC_OVERRIDES_PATH);
 		const bool overrides_exists = file_exists(AC_OVERRIDES_PATH);
 		const bool loaded_different = 
 				        overrides_exists	
 					? system(cmd) : true;
+		const bool stored_cmake = file_exists(previous_cmake_options_path);
+		sprintf(cmd,"echo %s | diff - %s",compilation_string,previous_cmake_options_path);
+		const bool different_cmake_string =  stored_cmake ? system(cmd) : true;
+		const bool compile = !overrides_exists || loaded_different || different_cmake_string;
 		acLoadRunConstsBase(AC_OVERRIDES_PATH,mesh_info);
 		if(!overrides_exists)
-			fprintf(stderr,"Compiling Astaroth\n");
-		if(loaded_different)
+			fprintf(stderr,"Compiling Astaroth; logging to %s\n",log_dst);
+		if(compile)
 		{
-			if(loaded_different && overrides_exists)
-				fprintf(stderr,"Loaded different run_const values; recompiling\n");
+			if(loaded_different && overrides_exists)  fprintf(stderr,"Loaded different run_const values; recompiling; loggin to %s\n",log_dst);
+			else if(overrides_exists && different_cmake_string && stored_cmake) fprintf(stderr,"Gave different cmake options; recompiling; logging to %s\n",log_dst);
 			sprintf(cmd,"rm -rf %s",runtime_astaroth_build_path);
 			int retval = system(cmd);
 			if(retval)
@@ -149,6 +196,7 @@ acCompile(const char* compilation_string, const char* target, AcMeshInfo mesh_in
 				fprintf(stderr,"%s","Fatal error was not able to make build directory\n");
 				exit(EXIT_FAILURE);
 			}
+			run_cmake(compilation_string,log_dst);
 		}
 		sprintf(cmd,"cd %s",
 		       runtime_astaroth_build_path);
@@ -156,26 +204,6 @@ acCompile(const char* compilation_string, const char* target, AcMeshInfo mesh_in
 		if(retval)
 		{
 			fprintf(stderr,"%s","Fatal error was not able to go into build directory\n");
-			fflush(stdout);
-			fflush(stderr);
-			exit(EXIT_FAILURE);
-		}
-		sprintf(cmd,"cmake --help > /dev/null");
-		retval = system(cmd);
-		if(retval)
-		{
-			fprintf(stderr,"%s","Did not find cmake\n");
-			fflush(stdout);
-			fflush(stderr);
-			exit(EXIT_FAILURE);
-		}
-		sprintf(cmd,"cd %s && cmake -DREAD_OVERRIDES=ON -DBUILD_SHARED_LIBS=ON -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DACC_COMPILER_PATH=%s %s %s &> %s",
-		       runtime_astaroth_build_path, acc_compiler_path, compilation_string,astaroth_base_path,log_dst);
-		retval = system(cmd);
-		if(retval)
-		{
-			fprintf(stderr,"%s %d\n","Fatal was not able to run cmake:",retval);
-			fprintf(stderr,"Cmake command: %s\n",cmd);
 			fflush(stdout);
 			fflush(stderr);
 			exit(EXIT_FAILURE);
