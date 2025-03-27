3c170fc58b3e2ffa83644a506ba99207cf6f38d4
ToxPuro <touko.puro@aalto.fi>
2025-03-27 14:37:17 +0200

    be more principled in mpitest: actually AC_nlocal the actually subdomain size and not the grid size

--- src/utils/memory.cc
+++ src/utils/memory.cc
@@ -119,10 +119,10 @@ acHostMeshApplyPeriodicBounds(AcMesh* mesh)
                     j_src += ny_min;
                     k_src += nz_min;
 
-                    const size_t src_idx = acVertexBufferIdx(i_src, j_src, k_src, info);
-                    const size_t dst_idx = acVertexBufferIdx(i_dst, j_dst, k_dst, info);
-                    ERRCHK(src_idx < acVertexBufferSize(info));
-                    ERRCHK(dst_idx < acVertexBufferSize(info));
+                    const size_t src_idx = acGridVertexBufferIdx(i_src, j_src, k_src, info);
+                    const size_t dst_idx = acGridVertexBufferIdx(i_dst, j_dst, k_dst, info);
+                    ERRCHK(src_idx < acGridVertexBufferSize(info));
+                    ERRCHK(dst_idx < acGridVertexBufferSize(info));
                     mesh->vertex_buffer[w][dst_idx] = mesh->vertex_buffer[w][src_idx];
 #if AC_LAGRANGIAN_GRID
 		    {
