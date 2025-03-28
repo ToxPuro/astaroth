2af9b77bb2cbc67bf9ececf86399f3cbdbb161e8
jpekkila <johannes.pekkila@protonmail.com>
2025-03-28 10:22:04 +0200

    Added a revised halo exchange implementation that scales better to large number of nodes (comm duplication at initialization instead of launch)

---
+++ acc-comm/acm/detail/halo_exchange_custom.h
@@ -0,0 +1,229 @@
+#pragma once
+
+#include <mpi.h>
+#include <vector>
+
+#include "buffer.h"
+#include "pack.h"
+#include "partition.h"
+#include "pointer.h"
+#include "type_conversion.h"
+
+#include "errchk_mpi.h"
+#include "mpi_utils.h"
+
+namespace acm {
+
+template <typename T, typename Allocator> class packet {
+  private:
+    ac::shape m_local_mm;
+    ac::shape m_local_nn;
+    ac::index m_local_rr;
+
+    ac::segment m_recv_segment;
+    ac::segment m_send_segment;
+
+    ac::buffer<T, Allocator> m_recv_buffer;
+    ac::buffer<T, Allocator> m_send_buffer;
+
+    MPI_Comm m_comm{MPI_COMM_NULL};
+    int16_t  m_tag{0};
+
+    int m_recv_neighbor{MPI_PROC_NULL};
+    int m_send_neighbor{MPI_PROC_NULL};
+
+    MPI_Request m_recv_req{MPI_REQUEST_NULL};
+    MPI_Request m_send_req{MPI_REQUEST_NULL};
+
+  public:
+    packet(const MPI_Comm& parent_comm, const ac::shape& local_mm, const ac::shape& local_nn,
+           const ac::index& local_rr, const ac::segment& recv_segment,
+           const size_t n_max_aggregate_buffers)
+        : m_local_mm{local_mm},
+          m_local_nn{local_nn},
+          m_local_rr{local_rr},
+          m_recv_segment{recv_segment},
+          m_send_segment{recv_segment.dims,
+                         ((local_nn + recv_segment.offset - local_rr) % local_nn) + local_rr},
+          m_recv_buffer{n_max_aggregate_buffers * prod(recv_segment.dims)},
+          m_send_buffer{n_max_aggregate_buffers * prod(recv_segment.dims)}
+    {
+        ERRCHK_MPI_API(MPI_Comm_dup(parent_comm, &m_comm));
+
+        const ac::direction recv_direction{
+            ac::mpi::get_direction(m_recv_segment.offset, m_local_nn, m_local_rr)};
+        m_recv_neighbor = ac::mpi::get_neighbor(m_comm, recv_direction);
+        m_send_neighbor = ac::mpi::get_neighbor(m_comm, -recv_direction);
+    }
+
+    ~packet()
+    {
+        ERRCHK_MPI(m_send_req == MPI_REQUEST_NULL);
+        ERRCHK_MPI(m_recv_req == MPI_REQUEST_NULL);
+
+        ERRCHK_MPI_API(MPI_Comm_free(&m_comm));
+    }
+
+    packet(const packet&)            = delete; // Copy constructor
+    packet& operator=(const packet&) = delete; // Copy assignment operator
+    packet(packet&&)                 = delete; // Move constructor
+    packet& operator=(packet&&)      = delete; // Move assignment operator
+
+    void launch(const std::vector<ac::mr::pointer<T, Allocator>>& inputs)
+    {
+        ERRCHK_MPI(m_send_req == MPI_REQUEST_NULL);
+        ERRCHK_MPI(m_recv_req == MPI_REQUEST_NULL);
+
+        // Calculate the number of elements to send
+        const size_t count{inputs.size() * prod(m_recv_segment.dims)};
+        ERRCHK_MPI(count <= m_send_buffer.size());
+        ERRCHK_MPI(count <= m_recv_buffer.size());
+
+        // Post recv
+        ERRCHK_MPI_API(MPI_Irecv(m_recv_buffer.data(),
+                                 as<int>(count),
+                                 ac::mpi::get_dtype<T>(),
+                                 m_recv_neighbor,
+                                 m_tag,
+                                 m_comm,
+                                 &m_recv_req));
+
+        // Pack and post send
+        pack(m_local_mm, m_send_segment.dims, m_send_segment.offset, inputs, m_send_buffer.get());
+
+        ERRCHK_MPI_API(MPI_Isend(m_send_buffer.data(),
+                                 as<int>(count),
+                                 ac::mpi::get_dtype<T>(),
+                                 m_send_neighbor,
+                                 m_tag,
+                                 m_comm,
+                                 &m_send_req));
+
+        ac::mpi::increment_tag(m_tag);
+    }
+
+    /** Completes the task */
+    void wait(std::vector<ac::mr::pointer<T, Allocator>> outputs)
+    {
+        ERRCHK_MPI(m_send_req != MPI_REQUEST_NULL);
+        ERRCHK_MPI(m_recv_req != MPI_REQUEST_NULL);
+
+        ERRCHK_MPI_API(MPI_Wait(&m_recv_req, MPI_STATUS_IGNORE));
+        ERRCHK_MPI_API(MPI_Wait(&m_send_req, MPI_STATUS_IGNORE));
+        unpack(m_recv_buffer.get(),
+               m_local_mm,
+               m_recv_segment.dims,
+               m_recv_segment.offset,
+               outputs);
+
+        ERRCHK_MPI(m_send_req == MPI_REQUEST_NULL);
+        ERRCHK_MPI(m_recv_req == MPI_REQUEST_NULL);
+    }
+
+    /** Returns true if the task is ready to be waited on */
+    bool ready() const
+    {
+        ERRCHK_MPI(m_send_req != MPI_REQUEST_NULL);
+        ERRCHK_MPI(m_recv_req != MPI_REQUEST_NULL);
+
+        int send_flag, recv_flag;
+        ERRCHK_MPI_API(MPI_Request_get_status(m_send_req, &send_flag, MPI_STATUS_IGNORE));
+        ERRCHK_MPI_API(MPI_Request_get_status(m_recv_req, &recv_flag, MPI_STATUS_IGNORE));
+        return send_flag && recv_flag;
+    };
+
+    /** Returns true if there is no task in flight */
+    bool complete() const
+    {
+        return (m_send_req == MPI_REQUEST_NULL) && (m_recv_req == MPI_REQUEST_NULL);
+    };
+};
+
+template <typename T, typename Allocator> class halo_exchange {
+  private:
+    std::vector<std::unique_ptr<packet<T, Allocator>>> m_packets;
+
+  public:
+    halo_exchange(const MPI_Comm& parent_comm, const ac::shape& global_nn, const ac::index& rr,
+                  const size_t n_max_aggregate_buffers)
+    {
+        // Get local mesh dimensions
+        const auto local_mm{ac::mpi::get_local_mm(parent_comm, global_nn, rr)};
+        const auto local_nn{ac::mpi::get_local_nn(parent_comm, global_nn)};
+
+        // Partition the domain
+        auto segments{partition(local_mm, local_nn, rr)};
+
+        // Prune the segment containing the computational domain
+        for (size_t i{0}; i < segments.size(); ++i) {
+            if (within_box(segments[i].offset, local_nn, rr)) {
+                segments.erase(segments.begin() + as<long>(i));
+                --i;
+            }
+        }
+
+        // Sort the packets from largest to smallest
+        std::sort(segments.begin(), segments.end(), [](const auto& a, const auto& b) {
+            return ac::prod(a.dims) > ac::prod(b.dims);
+        });
+
+        for (const auto& segment : segments)
+            m_packets.push_back(std::make_unique<packet<T, Allocator>>(parent_comm,
+                                                                       local_mm,
+                                                                       local_nn,
+                                                                       rr,
+                                                                       segment,
+                                                                       n_max_aggregate_buffers));
+    }
+
+    void launch(const std::vector<ac::mr::pointer<T, Allocator>>& inputs)
+    {
+        ERRCHK_MPI(complete());
+
+        for (auto& packet : m_packets)
+            packet->launch(inputs);
+    }
+
+    /** Waits one message and returns */
+    void wait_one(std::vector<ac::mr::pointer<T, Allocator>> outputs)
+    {
+        ERRCHK_MPI(!complete());
+
+        for (auto& packet : m_packets)
+            if (!packet->complete() && packet->ready())
+                packet->wait(outputs);
+    }
+
+    /** Waits all message and returns */
+    void wait(std::vector<ac::mr::pointer<T, Allocator>> outputs)
+    {
+        ERRCHK_MPI(!complete());
+
+        while (!complete())
+            wait_one(outputs);
+    }
+
+    /** Returns true if there is at least one message that can be waited on */
+    bool ready() const
+    {
+        ERRCHK_MPI(!complete());
+
+        for (const auto& packet : m_packets)
+            if (!packet->complete() && packet->ready())
+                return true;
+
+        return false;
+    }
+
+    /** Returns true if all messages have been completed */
+    bool complete() const
+    {
+        for (const auto& packet : m_packets)
+            if (!packet->complete())
+                return false;
+
+        return true;
+    }
+};
+
+} // namespace acm
--- acc-comm/tests/test_halo_exchange.cc
+++ acc-comm/tests/test_halo_exchange.cc
@@ -1,6 +1,8 @@
 #include <cstdlib>
 
+#include "acm/detail/allocator.h"
 #include "acm/detail/halo_exchange.h"
+#include "acm/detail/halo_exchange_custom.h"
 #include "acm/detail/halo_exchange_mpi.h"
 #include "acm/detail/halo_exchange_packed.h"
 #include "acm/detail/mpi_utils.h"
@@ -175,12 +177,70 @@ verify_revised_mpi_halo_exchange(const MPI_Comm& cart_comm, const ac::shape& glo
     return 0;
 }
 
+template <typename T>
+static int
+verify_custom_revised_packed_halo_exchange(const MPI_Comm& cart_comm, const ac::shape& global_nn,
+                                           const ac::index& rr)
+{
+    std::array<ac::host_ndbuffer<T>, 4> gbufs{
+        ac::host_ndbuffer<T>{global_nn},
+        ac::host_ndbuffer<T>{global_nn},
+        ac::host_ndbuffer<T>{global_nn},
+        ac::host_ndbuffer<T>{global_nn},
+    };
+    for (size_t i{0}; i < gbufs.size(); ++i)
+        std::iota(gbufs[i].begin(), gbufs[i].end(), i * prod(global_nn));
+
+    const auto                            local_mm{ac::mpi::get_local_mm(cart_comm, global_nn, rr)};
+    const auto                            local_nn{ac::mpi::get_local_nn(cart_comm, global_nn)};
+    std::array<ac::device_ndbuffer<T>, 4> lbufs{
+        ac::host_ndbuffer<T>{local_mm, -1}.to_device(),
+        ac::host_ndbuffer<T>{local_mm, -1}.to_device(),
+        ac::host_ndbuffer<T>{local_mm, -1}.to_device(),
+        ac::host_ndbuffer<T>{local_mm, -1}.to_device(),
+    };
+
+    for (size_t i{0}; i < gbufs.size(); ++i)
+        ac::mpi::scatter_advanced(cart_comm,
+                                  ac::mpi::get_dtype<T>(),
+                                  global_nn,
+                                  ac::make_index(global_nn.size(), 0),
+                                  gbufs[i].data(),
+                                  local_mm,
+                                  local_nn,
+                                  rr,
+                                  lbufs[i].data());
+
+    std::vector<ac::mr::device_pointer<T>> lptrs;
+    for (auto& lbuf : lbufs)
+        lptrs.push_back(lbuf.get());
+
+    acm::halo_exchange<T, ac::mr::device_allocator> he{cart_comm, global_nn, rr, lbufs.size()};
+    he.launch(lptrs);
+    he.wait(lptrs);
+
+    const auto global_nn_offset{ac::mpi::get_global_nn_offset(cart_comm, global_nn)};
+    for (uint64_t j{0}; j < lbufs.size(); ++j) {
+        const auto lbuf{lbufs[j].to_host()};
+        for (uint64_t i{0}; i < prod(local_mm); ++i) {
+            const auto lcoords{(global_nn + global_nn_offset + ac::to_spatial(i, local_mm) - rr) %
+                               global_nn};
+            const auto linear_idx{to_linear(lcoords, global_nn)};
+            ERRCHK(
+                within_machine_epsilon(lbuf[i], static_cast<T>(linear_idx + j * prod(global_nn))));
+        }
+    }
+
+    return 0;
+}
+
 static void
 verify(const MPI_Comm& cart_comm, const ac::shape& global_nn, const ac::index& rr)
 {
     ERRCHK_MPI(verify_mpi_halo_exchange<int>(cart_comm, global_nn, rr) == 0);
-    ERRCHK_MPI(verify_custom_packed_halo_exchange<int>(cart_comm, global_nn, rr) == 0);
     ERRCHK_MPI(verify_revised_mpi_halo_exchange<int>(cart_comm, global_nn, rr) == 0);
+    ERRCHK_MPI(verify_custom_packed_halo_exchange<int>(cart_comm, global_nn, rr) == 0);
+    ERRCHK_MPI(verify_custom_revised_packed_halo_exchange<int>(cart_comm, global_nn, rr) == 0);
 }
 
 int
