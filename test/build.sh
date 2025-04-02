09d0b2ffb2bd24ad46f2744a9371bdd791348f79
ToxPuro <touko.puro@aalto.fi>
2025-04-02 14:52:58 +0300

    simple parallelization of the build script in test

--- test/build.sh
+++ test/build.sh
@@ -2,25 +2,26 @@
 
 TEST_DIR="$AC_HOME/test"
 overall_success=true  # Flag to track overall success
-LOG="$PWD/compilation_log.txt"
-rm -f $LOG
-touch $LOG
 
-# Iterate over all subdirectories in TEST_DIR
-for dir in "$TEST_DIR"/*/; do
-    cd "$dir" || continue
+build_project() {
+    local dir="$1"
+    local log="$dir/compilation_log.txt"
+    rm -f $log
+    touch $log
+    cd "$dir" || return 1
     test_name=$(basename "${dir}")
+    
     # Run test script if it exists and is executable
     if [ -x "build.sh" ]; then
         echo "Building $test_name"
-        ./build.sh &>> $LOG
-        exit_code=$?  # Capture exit code of build.sh
+        ./build.sh &>> $log
+        exit_code=$?
 
         if [ $exit_code -ne 0 ]; then
             echo "[ERROR] Build failed for $test_name with exit code $exit_code"
-	    cat $LOG
-	    echo "" > $LOG
-            overall_success=false  # Mark failure but continue with other directories
+            cat $log
+            echo "" > $log
+            overall_success=false
         else
             echo "[SUCCESS] Build succeeded for $test_name"
         fi
@@ -29,9 +30,15 @@ for dir in "$TEST_DIR"/*/; do
     fi
 
     cd - >/dev/null  # Return to previous directory silently
+}
+
+export -f build_project
+for dir in "$TEST_DIR"/*/; do
+    build_project "$dir" &
 done
 
-# Final build summary
+wait
+
 if [ "$overall_success" = true ]; then
     echo "[SUCCESS] All builds completed successfully!"
     exit 0  # Success
--- test/mpi-profile-reduce/build.sh
+++ test/mpi-profile-reduce/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #
-mkdir -p build && cd build && cmake -DMPI_ENABLED=ON -DOPTIMIZE_MEM_ACCESSES=ON -DBUILD_TESTS=ON -DDSL_MODULE_DIR=../DSL $AC_HOME && make mpi-profile-reduce -j
+target=$(basename "${PWD}")
+mkdir -p build && cd build && cmake -DMPI_ENABLED=ON -DOPTIMIZE_MEM_ACCESSES=ON -DBUILD_TESTS=ON -DDSL_MODULE_DIR=../DSL $AC_HOME && make $target -j
