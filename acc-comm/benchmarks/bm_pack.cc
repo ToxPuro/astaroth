fa96a582c1609f2c2e81d7862aa527191f5f50db
jpekkila <johannes.pekkila@aalto.fi>
2025-04-09 12:10:47 +0300

    Moved mpi-specific stuff out from bm_pack

--- acc-comm/benchmarks/bm_pack.cc
+++ acc-comm/benchmarks/bm_pack.cc
@@ -5,7 +5,6 @@
 #include "acm/detail/convert.h"
 #include "acm/detail/errchk_mpi.h"
 #include "acm/detail/math_utils.h"
-#include "acm/detail/mpi_utils.h"
 #include "acm/detail/ndbuffer.h"
 #include "acm/detail/pack.h"
 #include "acm/detail/partition.h"
@@ -16,118 +15,9 @@
 #include "acm/detail/errchk_cuda.h"
 #endif
 
+#include "mpi_utils_experimental.h"
 #include "bm.h"
 
-namespace ac::mpi {
-
-class comm {
-  private:
-    std::unique_ptr<MPI_Comm, std::function<void(MPI_Comm*)>> m_comm;
-
-  public:
-    comm()
-        : m_comm{new MPI_Comm{MPI_COMM_NULL}, [](MPI_Comm* ptr) {
-                     if (*ptr != MPI_COMM_NULL)
-                         ERRCHK_MPI_API(MPI_Comm_free(ptr));
-                     delete ptr;
-                 }}
-    {
-    }
-
-    explicit comm(const MPI_Comm& parent_comm, const bool take_ownership = false)
-        : comm{}
-    {
-        if (take_ownership)
-            *m_comm = parent_comm;
-        else
-            ERRCHK_MPI_API(MPI_Comm_dup(parent_comm, m_comm.get()));
-    }
-
-    const MPI_Comm& get() const { return *m_comm; }
-};
-
-class cart_comm {
-  private:
-    comm      m_comm;
-    ac::shape m_global_nn;
-
-  public:
-    cart_comm(const MPI_Comm& parent_comm, const ac::shape& global_nn)
-        : m_comm{ac::mpi::cart_comm_create(parent_comm, global_nn), true}, m_global_nn{global_nn}
-    {
-    }
-
-    const MPI_Comm& get() const { return m_comm.get(); }
-    ac::shape       global_nn() const { return m_global_nn; }
-};
-
-class datatype {
-  private:
-    std::unique_ptr<MPI_Datatype, std::function<void(MPI_Datatype*)>> m_datatype;
-
-  public:
-    datatype()
-        : m_datatype{new MPI_Datatype{MPI_DATATYPE_NULL}, [](MPI_Datatype* ptr) {
-                         ERRCHK_MPI(ptr != nullptr);
-                         if (*ptr != MPI_DATATYPE_NULL)
-                             ERRCHK_MPI_API(MPI_Type_free(ptr));
-                         delete ptr;
-                     }}
-    {
-    }
-
-    explicit datatype(const MPI_Datatype& parent_datatype, const bool take_ownership = false)
-        : datatype{}
-    {
-        if (take_ownership)
-            *m_datatype = parent_datatype;
-        else
-            ERRCHK_MPI_API(MPI_Type_dup(parent_datatype, m_datatype.get()));
-    }
-
-    const MPI_Datatype& get() const { return *m_datatype; }
-};
-
-template <typename T> class subarray {
-  private:
-    datatype m_datatype;
-
-  public:
-    subarray(const ac::shape& dims, const ac::shape& subdims, const ac::index& offset)
-        : m_datatype{ac::mpi::subarray_create(dims, subdims, offset, ac::mpi::get_dtype<T>()), true}
-    {
-    }
-
-    const MPI_Datatype& get() const { return m_datatype.get(); }
-};
-
-class request {
-  private:
-    std::unique_ptr<MPI_Request, std::function<void(MPI_Request*)>> m_req;
-
-  public:
-    /**
-     * Wrap an MPI_Request.
-     * Does not take ownership, starts only to track the resource and raises an error
-     * if request goes out of scope without being released/waited upon.
-     */
-    explicit request(const MPI_Request& req = MPI_REQUEST_NULL)
-        : m_req{new MPI_Request{req}, [](MPI_Request* ptr) {
-                    ERRCHK_MPI(*ptr == MPI_REQUEST_NULL);
-                    delete ptr;
-                }}
-    {
-    }
-
-    void wait() noexcept
-    {
-        ERRCHK_MPI(*m_req != MPI_REQUEST_NULL);
-        ERRCHK_MPI_API(MPI_Wait(m_req.get(), MPI_STATUS_IGNORE));
-        ERRCHK_MPI(*m_req == MPI_REQUEST_NULL);
-    }
-};
-
-} // namespace ac::mpi
 
 template <typename T, typename Allocator> class mpi_pack_strategy {
   private:
