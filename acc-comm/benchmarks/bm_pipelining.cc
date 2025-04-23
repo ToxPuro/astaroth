1c4aad5157e8fb5aacd86931ffb1892b5c2a41f5
jpekkila <johannes.pekkila@protonmail.com>
2025-04-23 11:22:41 +0300

    Added a sample for benchmarking pipelining

---
+++ acc-comm/benchmarks/bm_pipelining.cc
@@ -0,0 +1,156 @@
+#include <cstdlib>
+#include <iostream>
+#include <mpi.h>
+#include <sstream>
+
+#include "acm/detail/allocator.h"
+#include "acm/detail/buffer.h"
+#include "acm/detail/convert.h"
+#include "acm/detail/errchk_mpi.h"
+#include "acm/detail/halo_exchange_custom.h"
+#include "acm/detail/halo_exchange_mpi.h"
+#include "acm/detail/math_utils.h"
+#include "acm/detail/mpi_utils.h"
+#include "acm/detail/ndbuffer.h"
+#include "acm/detail/ntuple.h"
+#include "acm/detail/pack.h"
+#include "acm/detail/partition.h"
+#include "acm/detail/print_debug.h"
+
+#if defined(ACM_DEVICE_ENABLED)
+#include "acm/detail/cuda_utils.h"
+#include "acm/detail/errchk_cuda.h"
+#endif
+
+#include "acm/detail/experimental/mpi_utils_experimental.h"
+#include "bm.h"
+
+#include "acm/detail/halo_exchange_batched.h"
+#include "acm/detail/halo_exchange_custom.h"
+#include "acm/detail/halo_exchange_mpi.h"
+
+int
+main(int argc, char* argv[])
+{
+    ac::mpi::init_funneled();
+    try {
+        using T         = double;
+        using Allocator = ac::mr::device_allocator;
+
+        if (ac::mpi::get_rank(MPI_COMM_WORLD) == 0)
+            std::cerr << "Usage: ./bm_pipelining <nx> <ny> <nz> <radius> <npack> <nsamples> "
+                         "<jobid>"
+                      << std::endl;
+        const size_t nx{(argc > 1) ? std::stoull(argv[1]) : 32};
+        const size_t ny{(argc > 2) ? std::stoull(argv[2]) : 32};
+        const size_t nz{(argc > 3) ? std::stoull(argv[3]) : 32};
+        const size_t radius{(argc > 4) ? std::stoull(argv[4]) : 3};
+        const size_t npack{(argc > 5) ? std::stoull(argv[5]) : 1};
+        const size_t nsamples{(argc > 6) ? std::stoull(argv[6]) : 10};
+        const size_t jobid{(argc > 7) ? std::stoull(argv[7]) : 0};
+
+        const ac::shape global_nn{nx, ny, nz};
+        const ac::index rr{ac::make_index(global_nn.size(), radius)};
+
+        if (ac::mpi::get_rank(MPI_COMM_WORLD) == 0) {
+            PRINT_DEBUG(nx);
+            PRINT_DEBUG(ny);
+            PRINT_DEBUG(nz);
+            PRINT_DEBUG(radius);
+            PRINT_DEBUG(nsamples);
+            PRINT_DEBUG(jobid);
+        }
+
+        std::ostringstream oss;
+        oss << "bm-pipelining-" << jobid << "-" << ac::mpi::get_rank(MPI_COMM_WORLD) << ".csv";
+        const auto output_file{oss.str()};
+        FILE*      fp{fopen(output_file.c_str(), "w")};
+        ERRCHK(fp);
+        fprintf(fp, "impl,nx,ny,nz,radius,npack,sample,nsamples,jobid,ns\n");
+        ERRCHK(fclose(fp) == 0);
+
+        auto print = [&](const std::string&                                label,
+                         const std::vector<std::chrono::nanoseconds::rep>& results) {
+            FILE* fp{fopen(output_file.c_str(), "a")};
+            ERRCHK(fp);
+
+            for (size_t i{0}; i < results.size(); ++i) {
+                fprintf(fp, "%s", label.c_str());
+                fprintf(fp, ",%zu", nx);
+                fprintf(fp, ",%zu", ny);
+                fprintf(fp, ",%zu", nz);
+                fprintf(fp, ",%zu", radius);
+                fprintf(fp, ",%zu", npack);
+                fprintf(fp, ",%zu", i);
+                fprintf(fp, ",%zu", nsamples);
+                fprintf(fp, ",%zu", jobid);
+                fprintf(fp, ",%lld", as<long long>(results[i]));
+                fprintf(fp, "\n");
+            }
+            ERRCHK(fclose(fp) == 0);
+        };
+
+        ac::mpi::cart_comm comm{MPI_COMM_WORLD, global_nn};
+
+        const int device_id{ac::mpi::select_device_lumi()};
+        MPI_SYNCHRONOUS_BLOCK_START(MPI_COMM_WORLD);
+        PRINT_DEBUG(ac::mpi::get_rank(MPI_COMM_WORLD));
+        PRINT_DEBUG(ac::mpi::get_rank(comm.get()));
+        PRINT_DEBUG(device_id);
+        if (ac::mpi::get_rank(MPI_COMM_WORLD) == 0) {
+            PRINT_DEBUG(global_nn);
+            PRINT_DEBUG(ac::mpi::local_nn(comm));
+        }
+        PRINT_DEBUG(ac::mpi::get_global_nn_offset(comm.get(), global_nn));
+        PRINT_DEBUG(ac::mpi::get_coords(comm.get()));
+        MPI_SYNCHRONOUS_BLOCK_END(MPI_COMM_WORLD);
+
+        std::vector<ac::ndbuffer<T, Allocator>> inputs;
+        for (size_t i{0}; i < npack; ++i)
+            inputs.push_back(ac::ndbuffer<T, Allocator>{ac::mpi::local_mm(comm, rr)});
+
+        auto init = [&]() {
+            for (auto& input : inputs)
+                bm::randomize(input.get());
+        };
+
+        auto sync = []() {
+#if defined(ACM_DEVICE_ENABLED)
+            ERRCHK_CUDA_API(cudaDeviceSynchronize());
+#endif
+            ERRCHK_MPI_API(MPI_Barrier(MPI_COMM_WORLD));
+        };
+
+        std::string                          mpi_he_label{"mpi-he"};
+        ac::mpi::halo_exchange<T, Allocator> mpi_he{comm.get(), global_nn, rr};
+        auto                                 mpi_he_bench = [&inputs, &mpi_he]() {
+            for (auto& input : inputs)
+                mpi_he.launch(input.get(), input.get());
+            mpi_he.wait_all();
+        };
+
+        std::string                      acm_packed_he_label{"acm-packed-he"};
+        acm::halo_exchange<T, Allocator> acm_packed_he{comm.get(), global_nn, rr, npack};
+        auto                             acm_packed_he_bench = [&inputs, &acm_packed_he]() {
+            acm_packed_he.launch(ac::unwrap_get(inputs));
+            acm_packed_he.wait(ac::unwrap_get(inputs));
+        };
+
+        std::string                      acm_batched_he_label{"acm-batched-he"};
+        acm::halo_exchange<T, Allocator> acm_batched_he{comm.get(), global_nn, rr, npack};
+        auto                             acm_batched_he_bench = [&inputs, &acm_batched_he]() {
+            acm_batched_he.launch(ac::unwrap_get(inputs));
+            acm_batched_he.wait(ac::unwrap_get(inputs));
+        };
+
+        print(mpi_he_label, bm::benchmark(init, mpi_he_bench, sync));
+        print(acm_packed_he_label, bm::benchmark(init, acm_packed_he_bench, sync));
+        print(acm_batched_he_label, bm::benchmark(init, acm_batched_he_bench, sync));
+    }
+    catch (const std::exception& e) {
+        ac::mpi::abort();
+        return EXIT_FAILURE;
+    }
+    ac::mpi::finalize();
+    return EXIT_SUCCESS;
+}
