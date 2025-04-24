b149170ff777bc207e4387b280861fc08578ee7c
jpekkila <johannes.pekkila@aalto.fi>
2025-04-24 11:42:33 +0300

    Removed the old benchmarking function as deprecated

--- acc-comm/benchmarks/bm.cc
+++ acc-comm/benchmarks/bm.cc
@@ -3,38 +3,6 @@
 #include "acm/detail/math_utils.h"
 #include "acm/detail/timer.h"
 
-double
-benchmark(const std::string label, const std::function<void()>& init,
-          const std::function<void()>& bench, const std::function<void()>& sync)
-{
-    constexpr size_t nsamples{100};
-    ERRCHK(nsamples > 0);
-
-    // Warmup
-    init();
-    bench();
-
-    // Benchmark
-    std::vector<long> samples;
-    for (size_t i{0}; i < nsamples; ++i) {
-        init();
-        sync();
-        const auto start{std::chrono::system_clock::now()};
-        bench();
-        sync();
-        const auto elapsed{std::chrono::duration_cast<std::chrono::microseconds>(
-            std::chrono::system_clock::now() - start)};
-        samples.push_back(elapsed.count());
-    }
-
-    std::sort(samples.begin(), samples.end());
-    std::cout << label << ":" << std::endl;
-    std::cout << "\tMin: " << samples[0] << " us" << std::endl;
-    std::cout << "\tMedian: " << bm::median(samples) << " us" << std::endl;
-    std::cout << "\tMax: " << samples[samples.size() - 1] << " us" << std::endl;
-    return bm::median(samples);
-}
-
 namespace bm {
 
 std::vector<std::chrono::nanoseconds::rep>
