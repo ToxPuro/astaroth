a4e6efc4b36085bd3fd44670d01ce632e09282f5
jpekkila <johannes.pekkila@aalto.fi>
2025-04-29 15:30:46 +0300

    Added hindexed mpi halo exchange (does not work yet)

---
+++ acc-comm/acm/detail/halo_exchange_mpi_hindexed.h
@@ -0,0 +1,139 @@
+#pragma once
+
+#include <mpi.h>
+
+#include "acm/detail/buffer.h"
+#include "acm/detail/experimental/mpi_utils_experimental.h"
+
+namespace ac::mpi::hindexed {
+
+template <typename T, typename Allocator> class packet {
+  private:
+    ac::mpi::comm              m_comm;
+    ac::mpi::hindexed_block<T> m_send_block;
+    ac::mpi::hindexed_block<T> m_recv_block;
+
+    ac::mpi::request m_send_req;
+    ac::mpi::request m_recv_req;
+    int16_t          m_tag{0};
+
+    int m_send_neighbor{MPI_PROC_NULL};
+    int m_recv_neighbor{MPI_PROC_NULL};
+
+  public:
+    packet(const MPI_Comm& parent_comm, const ac::shape& global_nn, const ac::index& rr,
+           const ac::segment& segment, const std::vector<ac::mr::pointer<T, Allocator>>& inputs,
+           const std::vector<ac::mr::pointer<T, Allocator>>& outputs)
+        : m_comm{parent_comm},
+          m_send_block(ac::mpi::get_local_mm(m_comm.get(), global_nn, rr), segment.dims,
+                       segment.offset, ac::unwrap_data(inputs)),
+          m_recv_block(ac::mpi::get_local_mm(m_comm.get(), global_nn, rr), segment.dims,
+                       segment.offset, ac::unwrap_data(outputs))
+    {
+        ERRCHK_MPI(inputs.size() == outputs.size());
+
+        const auto          local_nn{ac::mpi::get_local_nn(m_comm.get(), global_nn)};
+        const ac::direction recv_direction{ac::mpi::get_direction(segment.offset, local_nn, rr)};
+        m_recv_neighbor = ac::mpi::get_neighbor(m_comm.get(), recv_direction);
+        m_send_neighbor = ac::mpi::get_neighbor(m_comm.get(), -recv_direction);
+    }
+
+    void launch()
+    {
+        ERRCHK_MPI_API(MPI_Irecv(MPI_BOTTOM,
+                                 1,
+                                 m_recv_block.get(),
+                                 m_recv_neighbor,
+                                 m_tag,
+                                 m_comm.get(),
+                                 m_recv_req.data()));
+        ERRCHK_MPI_API(MPI_Isend(MPI_BOTTOM,
+                                 1,
+                                 m_send_block.get(),
+                                 m_send_neighbor,
+                                 m_tag,
+                                 m_comm.get(),
+                                 m_send_req.data()));
+
+        ac::mpi::increment_tag(m_tag);
+    }
+
+    /** Completes the task */
+    void wait()
+    {
+        m_send_req.wait();
+        m_recv_req.wait();
+    }
+
+    /** Returns true if the task is ready to be waited on */
+    bool ready() const { return m_send_req.ready() && m_recv_req.ready(); };
+
+    /** Returns true if there is no task in flight */
+    bool complete() const { return m_send_req.complete() && m_recv_req.complete(); };
+};
+
+template <typename T, typename Allocator> class halo_exchange {
+  private:
+    std::vector<packet<T, Allocator>> m_packets;
+
+  public:
+    halo_exchange(const MPI_Comm& parent_comm, const ac::shape& global_nn, const ac::index& rr,
+                  const std::vector<ac::mr::pointer<T, Allocator>>& inputs,
+                  const std::vector<ac::mr::pointer<T, Allocator>>& outputs)
+    {
+        const auto mm{ac::mpi::get_local_mm(parent_comm, global_nn, rr)};
+        const auto nn{ac::mpi::get_local_nn(parent_comm, global_nn)};
+        const auto segments{prune(partition(mm, nn, rr), nn, rr)};
+        for (const auto& segment : segments)
+            m_packets.push_back({parent_comm, global_nn, rr, segment, inputs, outputs});
+    }
+
+    void launch()
+    {
+        for (auto& packet : m_packets)
+            packet.launch();
+    }
+
+    /** Waits one message and returns */
+    void wait_one()
+    {
+        ERRCHK_MPI(!complete());
+
+        for (auto& packet : m_packets)
+            if (!packet.complete() && packet.ready())
+                packet.wait();
+    }
+
+    /** Waits all message and returns */
+    void wait_all()
+    {
+        ERRCHK_MPI(!complete());
+
+        while (!complete())
+            wait_one();
+    }
+
+    /** Returns true if there is at least one message that can be waited on */
+    bool ready() const
+    {
+        ERRCHK_MPI(!complete());
+
+        for (const auto& packet : m_packets)
+            if (!packet.complete() && packet.ready())
+                return true;
+
+        return false;
+    }
+
+    /** Returns true if all messages have been completed */
+    bool complete() const
+    {
+        for (const auto& packet : m_packets)
+            if (!packet.complete())
+                return false;
+
+        return true;
+    }
+};
+
+} // namespace ac::mpi::hindexed
--- acc-comm/tests/test_halo_exchange.cc
+++ acc-comm/tests/test_halo_exchange.cc
@@ -5,6 +5,7 @@
 #include "acm/detail/halo_exchange_batched.h"
 #include "acm/detail/halo_exchange_custom.h"
 #include "acm/detail/halo_exchange_mpi.h"
+#include "acm/detail/halo_exchange_mpi_hindexed.h"
 #include "acm/detail/halo_exchange_packed.h"
 #include "acm/detail/mpi_utils.h"
 #include "acm/detail/ndbuffer.h"
@@ -358,6 +359,81 @@ verify_custom_revised_packed_halo_exchange_v2(const MPI_Comm& cart_comm, const a
     return 0;
 }
 
+template <typename T>
+static int
+verify_halo_exchange_mpi_hindexed(const MPI_Comm& cart_comm, const ac::shape& global_nn,
+                                  const ac::index& rr)
+{
+    std::array<ac::host_ndbuffer<T>, 4> gbufs{
+        ac::host_ndbuffer<T>{global_nn},
+        ac::host_ndbuffer<T>{global_nn},
+        ac::host_ndbuffer<T>{global_nn},
+        ac::host_ndbuffer<T>{global_nn},
+    };
+    for (size_t i{0}; i < gbufs.size(); ++i)
+        std::iota(gbufs[i].begin(), gbufs[i].end(), i * prod(global_nn));
+
+    const auto                            local_mm{ac::mpi::get_local_mm(cart_comm, global_nn, rr)};
+    const auto                            local_nn{ac::mpi::get_local_nn(cart_comm, global_nn)};
+    std::array<ac::device_ndbuffer<T>, 4> lbufs{
+        ac::host_ndbuffer<T>{local_mm, -1}.to_device(),
+        ac::host_ndbuffer<T>{local_mm, -1}.to_device(),
+        ac::host_ndbuffer<T>{local_mm, -1}.to_device(),
+        ac::host_ndbuffer<T>{local_mm, -1}.to_device(),
+    };
+
+    for (size_t i{0}; i < gbufs.size(); ++i)
+        ac::mpi::scatter_advanced(cart_comm,
+                                  ac::mpi::get_dtype<T>(),
+                                  global_nn,
+                                  ac::make_index(global_nn.size(), 0),
+                                  gbufs[i].data(),
+                                  local_mm,
+                                  local_nn,
+                                  rr,
+                                  lbufs[i].data());
+
+    std::vector<ac::mr::device_pointer<T>> lptrs;
+    for (auto& lbuf : lbufs)
+        lptrs.push_back(lbuf.get());
+
+    ac::mpi::hindexed::halo_exchange<T, ac::mr::device_allocator> he{cart_comm,
+                                                                     global_nn,
+                                                                     rr,
+                                                                     lptrs,
+                                                                     lptrs};
+    he.launch();
+    he.wait_all();
+    // acm::rev::halo_exchange<T, ac::mr::device_allocator> he{cart_comm, global_nn, rr,
+    // lbufs.size()}; he.launch(lptrs); he.wait(lptrs);
+
+    const auto global_nn_offset{ac::mpi::get_global_nn_offset(cart_comm, global_nn)};
+    for (uint64_t j{0}; j < lbufs.size(); ++j) {
+        const auto lbuf{lbufs[j].to_host()};
+        for (uint64_t i{0}; i < prod(local_mm); ++i) {
+            const auto lcoords{(global_nn + global_nn_offset + ac::to_spatial(i, local_mm) - rr) %
+                               global_nn};
+            const auto linear_idx{to_linear(lcoords, global_nn)};
+            ERRCHK(
+                within_machine_epsilon(lbuf[i], static_cast<T>(linear_idx + j * prod(global_nn))));
+        }
+    }
+
+    // Benchmark
+    // ERRCHK_MPI_API(MPI_Barrier(MPI_COMM_WORLD));
+    // ac::timer t;
+    // ERRCHK_MPI_API(MPI_Barrier(MPI_COMM_WORLD));
+    // for (size_t i{0}; i < NSAMPLES; ++i) {
+    //     he.launch(lptrs);
+    //     he.wait(lptrs);
+    // }
+    // ERRCHK_MPI_API(MPI_Barrier(MPI_COMM_WORLD));
+    // if (ac::mpi::get_rank(cart_comm) == 0)
+    //     t.print_lap("custom revised v2 NSAMPLES TOTAL");
+
+    return 0;
+}
+
 static void
 verify(const MPI_Comm& cart_comm, const ac::shape& global_nn, const ac::index& rr)
 {
@@ -366,6 +442,7 @@ verify(const MPI_Comm& cart_comm, const ac::shape& global_nn, const ac::index& r
     ERRCHK_MPI(verify_custom_packed_halo_exchange<int>(cart_comm, global_nn, rr) == 0);
     ERRCHK_MPI(verify_custom_revised_packed_halo_exchange<int>(cart_comm, global_nn, rr) == 0);
     ERRCHK_MPI(verify_custom_revised_packed_halo_exchange_v2<int>(cart_comm, global_nn, rr) == 0);
+    ERRCHK_MPI(verify_halo_exchange_mpi_hindexed<int>(cart_comm, global_nn, rr) == 0);
 }
 
 int
