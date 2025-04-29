aeea77ba3e416b80b92b4a528c816197cd3e6bab
ToxPuro <touko.puro@aalto.fi>
2025-04-29 04:41:54 +0300

    split grid into vars and funcs

--- acc-runtime/stdlib/grid/funcs.h
+++ acc-runtime/stdlib/grid/funcs.h
@@ -1,11 +1,3 @@
-// physical grid
-//TP: origin assumes [0,AC_len] box
-run_const real3 AC_origin =  0.5*AC_len
-run_const real3 AC_center
-
-gmem real AC_x[AC_mlocal.x]
-gmem real AC_y[AC_mlocal.y]
-gmem real AC_z[AC_mlocal.z]
 
 grid_position() {
 //MR: generalize, using x,y,z?
