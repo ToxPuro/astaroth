ad420086bfb3de3c8dd73431534de078616b046e
Matthias Rheinhardt <mreinhar@puhti-login14.bullx>
2025-05-07 18:23:54 +0300

    MR: new data types in training added to built-in types; some new utility functions in stdlib/utils/funcs.h

--- acc-runtime/stdlib/utils/funcs.h
+++ acc-runtime/stdlib/utils/funcs.h
@@ -5,3 +5,24 @@ array_val(field_array)
 		res[i] = value(Field(field_array[i]))
 	return res
 }
+
+not_implemented(message)
+{
+    print("NOT IMPLEMENTED: %s\n",message)
+}
+
+tini_sqrt_div(real numerator, real a, real b)
+{
+  return
+     (abs(a) <= AC_REAL_MIN*5.0 || abs(b) <= AC_REAL_MIN*5.0)
+     ? 0.0
+     : numerator/(sqrt(a*b))
+}
+
+tini_sqrt_div_separate(real numerator, real a, real b)
+{
+  return
+     (abs(a) <= AC_REAL_MIN*5.0|| abs(b) <= AC_REAL_MIN*5.0)
+     ? 0.0
+     : numerator/(sqrt(a)*sqrt(b))
+}
