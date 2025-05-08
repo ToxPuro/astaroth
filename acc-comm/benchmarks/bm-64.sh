4ed491297685979f3f88661c4b5bbe2d62e7fb7e
jpekkila <johannes.pekkila@aalto.fi>
2025-05-08 16:19:41 +0300

    Updated bm-64 script

--- acc-comm/benchmarks/bm-64.sh
+++ acc-comm/benchmarks/bm-64.sh
@@ -17,21 +17,28 @@ module load craype-accel-amd-gfx90a # Must be loaded after LUMI/24.03
 export MPICH_GPU_SUPPORT_ENABLED=1
 export SRUN="srun --cpu-bind="map_cpu:49,57,17,25,1,9,33,41""
 
+# Expect ~8mins for these tests
 $SRUN ./benchmarks/bm_rank_reordering 1024 1024 1024 3 100 $SLURM_JOB_ID
 $SRUN ./benchmarks/bm_rank_reordering 2048 1024 512 3 100 $SLURM_JOB_ID
-#$SRUN ./benchmarks/bm_rank_reordering 1024 1024 512 3 100 $SLURM_JOB_ID
+##$SRUN ./benchmarks/bm_rank_reordering 1024 1024 512 3 100 $SLURM_JOB_ID
 $SRUN ./benchmarks/bm_rank_reordering 512 1024 2048 3 100 $SLURM_JOB_ID
 $SRUN ./benchmarks/bm_rank_reordering 4096 2048 128 3 100 $SLURM_JOB_ID
 
-$SRUN ./bm_collective_comm 1024 1024 1024 3 1 100
-$SRUN ./bm_collective_comm 1024 1024 1024 3 2 100
-$SRUN ./bm_collective_comm 1024 1024 1024 3 4 100
-$SRUN ./bm_collective_comm 1024 1024 1024 3 8 100
-$SRUN ./bm_collective_comm 1024 1024 1024 3 16 100
+# Expect ~10min for these tests
+$SRUN ./bm_collective_comm 512 512 512 3 1 100
+$SRUN ./bm_collective_comm 512 512 512 3 2 100
+$SRUN ./bm_collective_comm 512 512 512 3 4 100
+$SRUN ./bm_collective_comm 512 512 512 3 8 100
+$SRUN ./bm_collective_comm 512 512 512 3 16 100 # Should be around 90 seconds
+# $SRUN ./bm_collective_comm 1024 1024 1024 3 1 100
+# $SRUN ./bm_collective_comm 1024 1024 1024 3 2 100
+# $SRUN ./bm_collective_comm 1024 1024 1024 3 4 100
+# $SRUN ./bm_collective_comm 1024 1024 1024 3 8 100
+# $SRUN ./bm_collective_comm 1024 1024 1024 3 16 100
 ## $SRUN ./bm_collective_comm 1024 1024 1024 3 32 100 # Do not use, too large
 
 # Strong scaling
 ./tfm-mpi --config /users/pekkila/astaroth/samples/tfm/mhd/mhd.ini --global-nn-override 256,256,256 --job-id $SLURM_JOB_ID
 
 # Weak scaling
-./tfm-mpi --config /users/pekkila/astaroth/samples/tfm/mhd/mhd.ini --global-nn-override 1024,1024,1024 --job-id $SLURM_JOB_ID
+./tfm-mpi --config /users/pekkila/astaroth/samples/tfm/mhd/mhd.ini --global-nn-override 1024,1024,1024 --job-id $SLURM_JOB_ID # More than 5 min, note: need also disable bfield
