51ca4497cb5e673cfd922bb82aafe6f7deabc916
jpekkila <johannes.pekkila@aalto.fi>
2025-05-16 08:17:01 +0300

    Improvements to bm_plot

--- acc-comm/benchmarks/bm_plot.py
+++ acc-comm/benchmarks/bm_plot.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # %%
-# Pack
+# Packages
+import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import os
@@ -9,12 +10,28 @@ import glob
 print(f'cwd: {os.getcwd()}')
 outdir = "/users/pekkila/astaroth/build"
 
+def select(df, column, value):
+    return df[df[column] == value].drop(column, axis=1)
+
+def scatter(data, label):
+    print(len(data))
+    plt.scatter(np.arange(len(data)), data, label=label, alpha=0.6, s=15.0)
+
 # %%
 # Packing
 files = glob.glob(f"{outdir}/bm-pack-*")
 df = pd.concat((pd.read_csv(file) for file in files), ignore_index=True)
 
+nwarmup_steps = 5
+df = df[df['sample'] >= nwarmup_steps]
+# Check the distribution
+for impl in df['impl'].unique():
+    plt.plot(df[(df['impl'] == impl) & (df['dim'] == 256)]['ns'].values[:], label=impl)
+plt.legend()
+plt.show()
+
 df = df.drop(['sample', 'nsamples', 'jobid'], axis=1)
+
 df = df.groupby(list(df.columns.difference(['ns']))).describe()
 df = df.xs(256, level='dim')
 df = df.xs(3, level='radius')
@@ -31,11 +48,36 @@ df
 files = glob.glob(f"{outdir}/bm-rank-reordering-*.csv")
 df = pd.concat((pd.read_csv(file) for file in files), ignore_index=True)
 
+# Drop warmup steps
+nwarmup_steps = 5
+df = df[df['sample'] >= nwarmup_steps]
+scatter(df[(df['impl'] == 'hierarchical') & (df['rank'] == 0)]['ns'].values, label='hierarchical')
+scatter(df[(df['impl'] == 'mpi-no') & (df['rank'] == 0)]['ns'].values, label='mpi-no')
+scatter(df[(df['impl'] == 'mpi-default') & (df['rank'] == 0)]['ns'].values, label='mpi-default')
+plt.legend()
+plt.show()
+
+# Filter outliers and report (IQR)
+# df = select(df, 'radius', 3)
+# df = select(df, 'rank', 0)
+# df = select(df, 'nprocs', 64)
+# df = select(df, 'nsamples', 100)
+# assert(len(df['jobid'].unique()) == 1)
+# df = select(df, 'jobid', df['jobid'].unique()[-1])
+# df = df.drop('sample', axis=1)
+# df['nxyz'] = df['nx'].astype(str) + ',' + df['ny'].astype(str) + ',' + df['nz'].astype(str)
+# df = df.drop(['nx', 'ny', 'nz'], axis=1)
+# group = df.groupby(list(df.columns.difference(['ns'])))
+# iqr = group.quartile(0.75) - group.quartile(0.25)
+# df = df[]
+
+# Produce the results
 df = df.drop(['sample', 'nsamples', 'jobid'], axis=1)
 df = df.groupby(list(df.columns.difference(['ns']))).describe()
 df = df.xs(0, level='rank')
 df = df.xs(64, level='nprocs')
 df = df.xs(3, level='radius')
+
 df = df['ns']['50%']
 
 df = df.unstack('impl')
@@ -49,6 +91,17 @@ df
 # Collective communication
 files = glob.glob(f"{outdir}/bm-collective-comm*.csv")
 df = pd.concat((pd.read_csv(file) for file in files), ignore_index=True)
+
+nwarmup_steps = 5
+df = df[(df['sample'] >= nwarmup_steps) & (df['rank'] == 0)]
+for value in [1,2,4,8,16]:
+    scatter(df[(df['impl'] == 'mpi-he') & (df['rank'] == 0) & (df['npack'] == value)]['ns'].values, label='mpi-he')
+    scatter(df[(df['impl'] == 'mpi-he-hindexed') & (df['rank'] == 0) & (df['npack'] == value)]['ns'].values, label='mpi-he-hindexed')
+    scatter(df[(df['impl'] == 'acm-packed-he') & (df['rank'] == 0) & (df['npack'] == value)]['ns'].values, label='acm-packed-he')
+    scatter(df[(df['impl'] == 'acm-batched-he') & (df['rank'] == 0) & (df['npack'] == value)]['ns'].values, label='acm-batched-he')
+    plt.yscale('log')
+    plt.legend()
+    plt.show()
 
 df = df.drop(['sample', 'nsamples', 'jobid'], axis=1)
 df = df.groupby(list(df.columns.difference(['ns']))).describe()
@@ -69,6 +122,15 @@ df
 files = glob.glob(f"{outdir}/bm-tfm-mpi*.csv")
 df = pd.concat((pd.read_csv(file) for file in files), ignore_index=True)
 
+nwarmup_steps = 5
+df = df[(df['sample'] >= nwarmup_steps) & (df['rank'] == 0)]
+plt.plot(df[(df['nprocs'] == 1) & (df['rank'] == 0) & (df['gnz'] == 128)]['ns'].values, label='1')
+plt.plot(df[(df['nprocs'] == 8) & (df['rank'] == 0) & (df['gnz'] == 128)]['ns'].values, label='8')
+plt.plot(df[(df['nprocs'] == 64) & (df['rank'] == 0) & (df['gnz'] == 128)]['ns'].values, label='64')
+plt.yscale('log')
+plt.legend()
+plt.show()
+
 df = df.drop(['sample', 'nsamples', 'jobid'], axis=1)
 df = df.groupby(list(df.columns.difference(['ns']))).describe()
 df = df.xs(0, level='rank')
@@ -88,6 +150,16 @@ df
 # Weak scaling
 files = glob.glob(f"{outdir}/bm-tfm-mpi*.csv")
 df = pd.concat((pd.read_csv(file) for file in files), ignore_index=True)
+
+nwarmup_steps = 15
+df = df[(df['sample'] >= nwarmup_steps) & (df['rank'] == 0)]
+plt.plot(df[(df['nprocs'] == 1) & (df['rank'] == 0) & (df['lnz'] == 128)]['ns'].values, label='1')
+plt.plot(df[(df['nprocs'] == 8) & (df['rank'] == 0) & (df['lnz'] == 128)]['ns'].values, label='8')
+plt.plot(df[(df['nprocs'] == 64) & (df['rank'] == 0) & (df['lnz'] == 128)]['ns'].values, label='64')
+plt.plot(df[(df['nprocs'] == 512) & (df['rank'] == 0) & (df['lnz'] == 128)]['ns'].values, label='512')
+plt.yscale('log')
+plt.legend()
+plt.show()
 
 df = df.drop(['sample', 'nsamples', 'jobid'], axis=1)
 df = df.groupby(list(df.columns.difference(['ns']))).describe()
@@ -117,3 +189,24 @@ plt.barh(df['label'], df['ns']/1e9)
 # %%
 ns = 2634484064
 ns / 1e6 / 100
+
+# %%
+import matplotlib.pyplot as plt
+import pandas as pd
+files = [f'{outdir}/bm-tfm-mpi-0-122191-0.csv', f'{outdir}/bm-tfm-mpi-0-31510-0.csv']
+df = pd.concat((pd.read_csv(file) for file in files), ignore_index=True)
+
+df = df.drop(['sample', 'nsamples', 'jobid'], axis=1)
+# df = df.groupby(list(df.columns.difference(['ns']))).describe()
+df = df[['nprocs', 'ns']]
+
+plt.plot(df[df['nprocs']==1]['ns'].values, label="1 device")
+plt.plot(df[df['nprocs']==512]['ns'].values, label="512 devices")
+ax = plt.gca()
+ax.set_yticklabels([])
+ax.set_xticklabels([])
+plt.legend()
+ax.set_ylabel("Running time")
+ax.set_xlabel("nth step")
+
+df
