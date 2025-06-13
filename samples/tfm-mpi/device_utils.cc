21500fff272159e8bc5b588d8c96dff986ebf8f7
jpekkila <johannes.pekkila@aalto.fi>
2025-06-13 16:10:33 +0300

    More functionality to device_utils

--- samples/tfm-mpi/device_utils.cc
+++ samples/tfm-mpi/device_utils.cc
@@ -6,6 +6,25 @@
 
 namespace ac {
 
+AcMeshInfo get_info(const Device& device)
+{
+    AcMeshInfo info{};
+    ERRCHK_AC(acDeviceGetLocalConfig(device, &info));
+    return info;
+}
+
+VertexBufferArray get_vba(const Device& device) {
+    VertexBufferArray vba{};
+    ERRCHK_AC(acDeviceGetVBA(device, &vba));
+    return vba;
+}
+
+// AcReal* get_input_field(const Device& device, const Field& handle) {
+//     auto vba{get_vba(device)};
+//     ERRCHK(handle < NUM_FIELDS);
+//     return vba.in[handle];
+// }
+
 void
 swap_buffers(const Device& device)
 {
@@ -40,6 +59,13 @@ get_ptrs(const Device& device, const std::vector<Profile>& profiles, const Buffe
     ERRCHK_AC(acDeviceGetVBA(device, &vba));
 
     return acr::get_ptrs(vba, profiles, type);
+}
+
+ac::device_view<AcReal> get_dfield(const Device& device, const Field& field, const BufferGroup& group)
+{
+    VertexBufferArray vba{};
+    ERRCHK_AC(acDeviceGetVBA(device, &vba));
+    return acr::make_ptr(vba, field, group);
 }
 
 } // namespace ac
--- samples/tfm-mpi/device_utils.h
+++ samples/tfm-mpi/device_utils.h
@@ -6,6 +6,10 @@
 #include "acr_utils.h"
 
 namespace ac {
+
+AcMeshInfo get_info(const Device& device);
+VertexBufferArray get_vba(const Device& device);
+
 void swap_buffers(const Device& device);
 void swap_buffers(const Device& device, const VertexBufferHandle& handle);
 void swap_buffers(const Device& device, const std::vector<VertexBufferHandle>& handles);
@@ -15,4 +19,21 @@ get_ptrs(const Device& device, const std::vector<Field>& fields, const BufferGro
 
 std::vector<ac::device_view<AcReal>>
 get_ptrs(const Device& device, const std::vector<Profile>& profiles, const BufferGroup& type);
+
+// Revised 2025-06
+ac::device_view<AcReal> get_dfield(const Device& device, const Field& field, const BufferGroup& group);
+
+template<typename T>
+auto pull_param(const Device& device, const T& param)
+{
+    return acr::get(get_info(device), param);
+}
+
+template<typename T, typename U>
+auto push_param(const Device& device, const T& param, const U& value) { 
+    auto info{get_info(device)}; 
+    acr::set(param, value, info); 
+    ERRCHK_AC(acDeviceLoadMeshInfo(device, info));
+}
+
 } // namespace ac
