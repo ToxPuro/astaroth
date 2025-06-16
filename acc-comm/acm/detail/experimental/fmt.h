13822e47a525f8616fbb8fb0059cbc178af8e237
jpekkila <johannes.pekkila@aalto.fi>
2025-06-16 10:32:32 +0300

    Workaround for reading from std::hexfloat on Mahti

--- acc-comm/acm/detail/experimental/fmt.h
+++ acc-comm/acm/detail/experimental/fmt.h
@@ -41,7 +41,16 @@ pull_token(std::istream& is, T& output)
     // Parse token
     std::istringstream iss{token};
     ERRCHK(iss);
-    ERRCHK(iss >> output);
+    //ERRCHK(iss >> output); // Reading from std::hexfloat bugged on Mahti
+    if constexpr (std::is_same_v<double, T>) {
+	    output = std::strtod(iss.str().c_str(), nullptr);
+	    ERRCHK(output != 0);
+    } else if constexpr(std::is_same_v<float, T>) {
+	    output = std::strtof(iss.str().c_str(), nullptr);
+	    ERRCHK(output != 0);
+    } else {
+	    ERRCHK(iss >> output);
+    }
     ERRCHK(!iss.fail());
 }
 
