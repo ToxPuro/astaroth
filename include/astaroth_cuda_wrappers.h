22a5c70038047c92ec69c64f3cd22f3a70503403
Touko Puro <puro.touko2@gmail.com>
2025-07-16 17:45:55 +0300

    Mask out cuda declarations that only visible in C++ sources

--- include/astaroth_cuda_wrappers.h
+++ include/astaroth_cuda_wrappers.h
@@ -12,16 +12,29 @@ cudaError_t
 acSetDevice(const int id);
 cudaError_t
 acGetDeviceCount(int* dst);
+//TP: cudaMemcpyKind,cudaDeviceProp,cudaDeviceAttr and cudaSharedMemConfig are only defined for C++ sources
 #ifdef __cplusplus
 cudaError_t
 acDeviceSetSharedMemConfig(const cudaSharedMemConfig config);
-#endif
-cudaError_t
-acStreamDestroy(cudaStream_t stream);
 cudaError_t
 acMemcpy(void* dst, const void* src, const size_t bytes, cudaMemcpyKind kind);
 cudaError_t
 acMemcpyAsync(void* dst, const void* src, const size_t bytes, cudaMemcpyKind kind, const cudaStream_t stream);
+cudaError_t
+acMemcpyToSymbol(const void* symbol, const void* src, size_t count, size_t offset, cudaMemcpyKind kind);
+cudaError_t
+acMemcpyToSymbolAsync(const void* symbol, const void* src, size_t count, size_t offset, cudaMemcpyKind kind, cudaStream_t stream);
+cudaError_t 
+acMemcpyFromSymbol( void* dst, const void* symbol, size_t count, size_t offset, cudaMemcpyKind kind);
+cudaError_t 
+acMemcpyFromSymbolAsync(void* dst, const void* symbol, size_t count, size_t offset, cudaMemcpyKind kind, cudaStream_t stream);
+cudaError_t 
+acGetDeviceProperties(cudaDeviceProp* prop, int  device);
+cudaError_t
+acDeviceGetAttribute(int* dst, cudaDeviceAttr attr, int device);
+#endif
+cudaError_t
+acStreamDestroy(cudaStream_t stream);
 cudaError_t
 acMemcpyPeerAsync(AcReal* dst, int dst_id, const AcReal* src, int src_id, const size_t bytes, const cudaStream_t stream);
 cudaError_t
@@ -56,24 +69,12 @@ cudaError_t
 acEventElapsedTime(float* time, cudaEvent_t start_event, cudaEvent_t end_event);
 cudaError_t
 acEventDestroy(cudaEvent_t event);
-cudaError_t 
-acGetDeviceProperties(cudaDeviceProp* prop, int  device);
 cudaError_t
 acOccupancyMaxActiveBlocksPerMultiprocessor(int* numBlocks, const void* func, int blockSize, size_t smemSize);
 cudaError_t
-acDeviceGetAttribute(int* dst, cudaDeviceAttr attr, int device);
-cudaError_t
 acLaunchCooperativeKernel(void* func,dim3 bpg,dim3 tpb,void** args,size_t smem,cudaStream_t stream);
 cudaError_t
-acMemcpyToSymbol(const void* symbol, const void* src, size_t count, size_t offset, cudaMemcpyKind kind);
-cudaError_t
-acMemcpyToSymbolAsync(const void* symbol, const void* src, size_t count, size_t offset, cudaMemcpyKind kind, cudaStream_t stream);
-cudaError_t
 acDeviceGetPCIBusId(char* pciBusId, int len, int device);
-cudaError_t 
-acMemcpyFromSymbol( void* dst, const void* symbol, size_t count, size_t offset, cudaMemcpyKind kind);
-cudaError_t 
-acMemcpyFromSymbolAsync(void* dst, const void* symbol, size_t count, size_t offset, cudaMemcpyKind kind, cudaStream_t stream);
 cudaError_t
 acPeekAtLastError();
 cudaError_t
