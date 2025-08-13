79411958b9d3756782e9bb5b00f0452b494e2d45
jpekkila <johannes.pekkila@aalto.fi>
2025-08-13 08:41:44 +0300

    Consistent device selection in bm_collective_comm and bm_rank_reordering

--- acc-comm/benchmarks/bm_rank_reordering.cc
+++ acc-comm/benchmarks/bm_rank_reordering.cc
@@ -326,11 +326,7 @@ main(int argc, char* argv[])
 #if defined(ACM_DEVICE_ENABLED)
         int device_count{0};
         ERRCHK_CUDA_API(cudaGetDeviceCount(&device_count));
-        int device_id{ac::mpi::get_rank(MPI_COMM_WORLD) % device_count};
-        if (device_count == 8) { // Do manual GPU mapping for LUMI
-            ac::ntuple<int> device_ids{6, 7, 0, 1, 2, 3, 4, 5};
-            device_id = device_ids[as<size_t>(device_id)];
-        }
+        const auto device_id{ac::mpi::select_device_lumi()};
         ERRCHK_CUDA_API(cudaSetDevice(device_id));
 #endif
 
