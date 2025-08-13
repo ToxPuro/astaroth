d82dc3dec65e99e199a297a3081d732e599a2b4c
jpekkila <johannes.pekkila@aalto.fi>
2025-08-13 08:55:18 +0300

    Autoformat bm_collective_comm

--- acc-comm/benchmarks/bm_collective_comm.cc
+++ acc-comm/benchmarks/bm_collective_comm.cc
@@ -115,10 +115,10 @@ main(int argc, char* argv[])
         const auto filename{filename_stream.str()};
 
         if (ac::mpi::get_rank(MPI_COMM_WORLD) == 0) {
-        std::ofstream file{filename};
-        file.exceptions(~std::ios::goodbit);
-        file << "impl,nx,ny,nz,radius,npack,sample,nsamples,rank,nprocs,jobid,ns" << std::endl;
-        file.close();
+            std::ofstream file{filename};
+            file.exceptions(~std::ios::goodbit);
+            file << "impl,nx,ny,nz,radius,npack,sample,nsamples,rank,nprocs,jobid,ns" << std::endl;
+            file.close();
         }
 
         auto print = [&](const std::string&                                      label,
