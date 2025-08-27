01eb480a3a17036830a22b28f48de24184b56b62
jpekkila <johannes.pekkila@aalto.fi>
2025-08-27 17:38:32 +0300

    Added LUMI-specific CPU-GPU mapping to tfm-mpi

--- samples/tfm-mpi/tfm.cc
+++ samples/tfm-mpi/tfm.cc
@@ -1202,16 +1202,16 @@ select_device()
 {
     int device_count{0};
     ERRCHK_CUDA_API(cudaGetDeviceCount(&device_count));
-    const int device_id{ac::mpi::get_rank(MPI_COMM_WORLD) % device_count};
+    int device_id{ac::mpi::get_rank(MPI_COMM_WORLD) % device_count};
 
     // Enable for proper hierarchical mapping
     // TODO note: hardcoded for LUMI
     // Could benchmark the topology here instead of hardcoding the fastest paths
-    // PRINT_LOG_WARNING("select_device() hardcoded to use LUMI topology");
-    // if (device_count == 8) { // Do manual GPU mapping for LUMI
-    //     ac::ntuple<int> device_ids{6, 7, 0, 1, 2, 3, 4, 5};
-    //     device_id = device_ids[as<size_t>(device_id)];
-    // }
+    PRINT_LOG_WARNING("select_device() hardcoded to use LUMI topology");
+    if (device_count == 8) { // Do manual GPU mapping for LUMI
+        ac::ntuple<int> device_ids{6, 7, 0, 1, 2, 3, 4, 5};
+        device_id = device_ids[as<size_t>(device_id)];
+    }
 
     ERRCHK_CUDA_API(cudaSetDevice(device_id)); // likely unnecessary
     ERRCHK_CUDA_API(cudaDeviceSynchronize());  // likely unnecessary
