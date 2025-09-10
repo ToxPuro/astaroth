4f58a77dde4444a0a2ffb6e8fe4c95d55c75b37e
purotouko <touko.puro@aalto.com>
2025-09-10 15:14:49 +0300

    extended versions of spherical harmonics

--- acc-runtime/stdlib/spherical_harmonics.h
+++ acc-runtime/stdlib/spherical_harmonics.h
@@ -20,6 +20,7 @@ negative_real_spherical_harmonic(int m, int l)
 {
 	return AC_PLM[vertexIdx.y][l][m]*AC_sin_m_phis[vertexIdx.z][m]
 }
+
 calculate_mlm(Field src)
 {
 	r = AC_r[vertexIdx.x]
@@ -58,9 +59,92 @@ calculate_mlm(Field src)
 	}
 }
 
+calculate_mlm_extended(Field src)
+{
+	r = AC_r_extended[vertexIdx.x]
+	w = get_integration_weight_extended()
+	for l in 0:AC_n_spherical_harmonics
+	{
+		r_factor = 1.0
+		for i in 0:l
+		{
+			r_factor *= r
+		}
+		for m in 0:l+1
+		{
+			rest     = src*r_factor*w
+			reduce_sum(positive_real_spherical_harmonic(m,l)*rest,AC_upper_positive_MLM[l + m*AC_n_spherical_harmonics])
+			if(m > 0)
+			{
+				reduce_sum(negative_real_spherical_harmonic(m,l)*rest,AC_upper_negative_MLM[l + m*AC_n_spherical_harmonics])
+			}
+		}
+		//Exterior potential expansion
+		r_factor = 1.0
+		for i in 0:l+1
+		{
+			r_factor /= r
+		}
+		for m in 0:l+1
+		{
+			rest     = src*r_factor*w
+			reduce_sum(positive_real_spherical_harmonic(m,l)*rest,AC_lower_positive_MLM[l + m*AC_n_spherical_harmonics])
+			if(m > 0)
+			{
+				reduce_sum(negative_real_spherical_harmonic(m,l)*rest,AC_lower_negative_MLM[l + m*AC_n_spherical_harmonics])
+			}
+		}
+	}
+}
+
 calculate_expansion_with_mlm(Field lower, Field upper)
 {
 	r = AC_r[vertexIdx.x]
+
+	//Interior potential expansion
+	res = 0.0
+	for l in 0:AC_n_spherical_harmonics
+	{
+		r_factor = 1.0/r
+		for i in 0:l
+		{
+			r_factor /= r
+		}
+		for m in 0:l+1
+		{
+			res -= r_factor*AC_upper_positive_MLM[l + m*AC_n_spherical_harmonics]*positive_real_spherical_harmonic(m,l)
+			if(m > 0)
+			{
+				res -= r_factor*AC_upper_negative_MLM[l + m*AC_n_spherical_harmonics]*negative_real_spherical_harmonic(m,l)
+			}
+		}
+	}
+	write(upper,res)
+
+	//Exterior potential expansion
+	res = 0.0
+	for l in 0:AC_n_spherical_harmonics
+	{
+		r_factor = 1.0
+		for i in 0:l
+		{
+			r_factor *= r
+		}
+		for m in 0:l+1
+		{
+			res -= r_factor*AC_lower_positive_MLM[l + m*AC_n_spherical_harmonics]*positive_real_spherical_harmonic(m,l)
+			if(m > 0)
+			{
+				res -= r_factor*AC_lower_negative_MLM[l + m*AC_n_spherical_harmonics]*negative_real_spherical_harmonic(m,l)
+			}
+		}
+	}
+	write(lower,res)
+}
+
+calculate_expansion_with_mlm_extended(Field lower, Field upper)
+{
+	r = AC_r_extended[vertexIdx.x]
 
 	//Interior potential expansion
 	res = 0.0
