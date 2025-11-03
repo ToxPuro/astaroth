28a2435f002bffa4700d2b10773d9200010c7dbd
ToxPuro <touko.puro@aalto.fi>
2025-11-03 18:28:30 +0200

    fix prolongation operator, altough not yet tested

--- acc-runtime/stdlib/math/interpolation.h
+++ acc-runtime/stdlib/math/interpolation.h
@@ -75,79 +75,78 @@ restrict_full_weighting(Field fine_residual, Field coarse_residual)
 /*
  * Meant to be launched on the fine grid
  */
-trilinear_prolongation(Field coarse_residual, Field fine_residual)
+trilinear_prolongation(Field coarse_residual)
 {
 	const bool I_even = (vertexIdx.x % 2 == 0)
 	const bool J_even = (vertexIdx.y % 2 == 0)
 	const bool K_even = (vertexIdx.z % 2 == 0)
 
-	const int I_coarse = vertexIdx.x / 2;
-	const int J_coarse = vertexIdx.y / 2;
-	const int K_coarse = vertexIdx.z / 2;
+	const int3 coarse_vertexIdx = vertexIdx / 2;
 
 	if(I_even && J_even && K_even)
 	{
-		fine_residual[vertexIdx.x][vertexIdx.y][vertexIdx.z] = coarse_residual[vertexIdx.x][vertexIdx.y][vertexIdx.z]
+		return coarse_residual[coarse_vertexIdx.x][coarse_vertexIdx.y][coarse_vertexIdx.z]
 	}
 	if(I_even && J_even && !K_even)
 	{
-		fine_residual[vertexIdx.x][vertexIdx.y][vertexIdx.z] = 0.5*(
-								coarse_residual[vertexIdx.x][vertexIdx.y][vertexIdx.z]
-								+coarse_residual[vertexIdx.x][vertexIdx.y][vertexIdx.z+1]
+		return 0.5*(
+								coarse_residual[coarse_vertexIdx.x][coarse_vertexIdx.y][coarse_vertexIdx.z]
+								+coarse_residual[coarse_vertexIdx.x][coarse_vertexIdx.y][coarse_vertexIdx.z+1]
 				)
 	}
 	if(I_even && !J_even && K_even)
 	{
-		fine_residual[vertexIdx.x][vertexIdx.y][vertexIdx.z] = 0.5*(
-								coarse_residual[vertexIdx.x][vertexIdx.y][vertexIdx.z]
-								+coarse_residual[vertexIdx.x][vertexIdx.y+1][vertexIdx.z]
+		return 0.5*(
+								coarse_residual[coarse_vertexIdx.x][coarse_vertexIdx.y][coarse_vertexIdx.z]
+								+coarse_residual[coarse_vertexIdx.x][coarse_vertexIdx.y+1][coarse_vertexIdx.z]
 				)
 	}
 	if(!I_even && J_even && K_even)
 	{
-		fine_residual[vertexIdx.x][vertexIdx.y][vertexIdx.z] = 0.5*(
-								coarse_residual[vertexIdx.x][vertexIdx.y][vertexIdx.z]
-								+coarse_residual[vertexIdx.x+1][vertexIdx.y][vertexIdx.z]
+		return 0.5*(
+								coarse_residual[coarse_vertexIdx.x][coarse_vertexIdx.y][coarse_vertexIdx.z]
+								+coarse_residual[coarse_vertexIdx.x+1][coarse_vertexIdx.y][coarse_vertexIdx.z]
 				)
 	}
 	if(I_even && !J_even && !K_even)
 	{
-		fine_residual[vertexIdx.x][vertexIdx.y][vertexIdx.z] = 0.25*(
-								coarse_residual[vertexIdx.x][vertexIdx.y][vertexIdx.z]
-								+coarse_residual[vertexIdx.x][vertexIdx.y+1][vertexIdx.z]
-								+coarse_residual[vertexIdx.x][vertexIdx.y][vertexIdx.z+1]
-								+coarse_residual[vertexIdx.x][vertexIdx.y+1][vertexIdx.z+1]
+		return 0.25*(
+								coarse_residual[coarse_vertexIdx.x][coarse_vertexIdx.y][coarse_vertexIdx.z]
+								+coarse_residual[coarse_vertexIdx.x][coarse_vertexIdx.y+1][coarse_vertexIdx.z]
+								+coarse_residual[coarse_vertexIdx.x][coarse_vertexIdx.y][coarse_vertexIdx.z+1]
+								+coarse_residual[coarse_vertexIdx.x][coarse_vertexIdx.y+1][coarse_vertexIdx.z+1]
 				)
 	}
 	if(!I_even && J_even && !K_even)
 	{
-		fine_residual[vertexIdx.x][vertexIdx.y][vertexIdx.z] = 0.25*(
-								coarse_residual[vertexIdx.x][vertexIdx.y][vertexIdx.z]
-								+coarse_residual[vertexIdx.x+1][vertexIdx.y][vertexIdx.z]
-								+coarse_residual[vertexIdx.x][vertexIdx.y][vertexIdx.z+1]
-								+coarse_residual[vertexIdx.x+1][vertexIdx.y][vertexIdx.z+1]
+		return 0.25*(
+								coarse_residual[coarse_vertexIdx.x][coarse_vertexIdx.y][coarse_vertexIdx.z]
+								+coarse_residual[coarse_vertexIdx.x+1][coarse_vertexIdx.y][coarse_vertexIdx.z]
+								+coarse_residual[coarse_vertexIdx.x][coarse_vertexIdx.y][coarse_vertexIdx.z+1]
+								+coarse_residual[coarse_vertexIdx.x+1][coarse_vertexIdx.y][coarse_vertexIdx.z+1]
 				)
 	}
 	if(!I_even && !J_even && K_even)
 	{
-		fine_residual[vertexIdx.x][vertexIdx.y][vertexIdx.z] = 0.25*(
-								coarse_residual[vertexIdx.x][vertexIdx.y][vertexIdx.z]
-								+coarse_residual[vertexIdx.x+1][vertexIdx.y][vertexIdx.z]
-								+coarse_residual[vertexIdx.x][vertexIdx.y+1][vertexIdx.z]
-								+coarse_residual[vertexIdx.x+1][vertexIdx.y+1][vertexIdx.z]
+		return 0.25*(
+								coarse_residual[coarse_vertexIdx.x][coarse_vertexIdx.y][coarse_vertexIdx.z]
+								+coarse_residual[coarse_vertexIdx.x+1][coarse_vertexIdx.y][coarse_vertexIdx.z]
+								+coarse_residual[coarse_vertexIdx.x][coarse_vertexIdx.y+1][coarse_vertexIdx.z]
+								+coarse_residual[coarse_vertexIdx.x+1][coarse_vertexIdx.y+1][coarse_vertexIdx.z]
 				)
 	}
 	if(!I_even && !J_even && !K_even)
 	{
-		fine_residual[vertexIdx.x][vertexIdx.y][vertexIdx.z] = 0.125*(
-								coarse_residual[vertexIdx.x][vertexIdx.y][vertexIdx.z]
-								+coarse_residual[vertexIdx.x+1][vertexIdx.y][vertexIdx.z]
-								+coarse_residual[vertexIdx.x][vertexIdx.y+1][vertexIdx.z]
-								+coarse_residual[vertexIdx.x][vertexIdx.y][vertexIdx.z+1]
-								+coarse_residual[vertexIdx.x+1][vertexIdx.y+1][vertexIdx.z]
-								+coarse_residual[vertexIdx.x+1][vertexIdx.y][vertexIdx.z+1]
-								+coarse_residual[vertexIdx.x][vertexIdx.y+1][vertexIdx.z+1]
-								+coarse_residual[vertexIdx.x+1][vertexIdx.y+1][vertexIdx.z+1]
+		return 0.125*(
+								coarse_residual[coarse_vertexIdx.x][coarse_vertexIdx.y][coarse_vertexIdx.z]
+								+coarse_residual[coarse_vertexIdx.x+1][coarse_vertexIdx.y][coarse_vertexIdx.z]
+								+coarse_residual[coarse_vertexIdx.x][coarse_vertexIdx.y+1][coarse_vertexIdx.z]
+								+coarse_residual[coarse_vertexIdx.x][coarse_vertexIdx.y][coarse_vertexIdx.z+1]
+								+coarse_residual[coarse_vertexIdx.x+1][coarse_vertexIdx.y+1][coarse_vertexIdx.z]
+								+coarse_residual[coarse_vertexIdx.x+1][coarse_vertexIdx.y][coarse_vertexIdx.z+1]
+								+coarse_residual[coarse_vertexIdx.x][coarse_vertexIdx.y+1][coarse_vertexIdx.z+1]
+								+coarse_residual[coarse_vertexIdx.x+1][coarse_vertexIdx.y+1][coarse_vertexIdx.z+1]
 				)
 	}
+	return 0.0
 }
