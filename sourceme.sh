6a25b20c654118e2911a0dfad078114e858beba9
ToxPuro <touko.puro@aalto.fi>
2025-12-17 13:16:27 +0200

    make sourceme.sh executable and fix the return code logics

--- sourceme.sh
+++ sourceme.sh
@@ -5,4 +5,5 @@ export PATH=${PATH}:$AC_HOME/scripts/
 export PATH=${PATH}:$AC_HOME/bin
 echo $AC_HOME
 echo $PATH
-fetched_submodule=$(git submodule update --init --remote > /dev/null 2>&1)
+#TP: we don't want to return a non-zero error code in case pulling submodules fails since it is not fatal
+git submodule update --init --remote > /dev/null 2>&1 || true
