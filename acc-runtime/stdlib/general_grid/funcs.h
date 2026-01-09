cc2ca2a47515100a61ccc3923227510ddf121f63
Touko Puro <ToxPuro>
2026-01-09 07:09:50 +0200

    skip usage of coordinate arrays when uniform

--- acc-runtime/stdlib/general_grid/funcs.h
+++ acc-runtime/stdlib/general_grid/funcs.h
@@ -92,7 +92,11 @@ grid_xyz() {
 
 
 grid_position(int3 local_point) {
-    global_point = local_point + AC_multigpu_offset
+	if(AC_all_are_equidistant)
+	{
+    		global_point = (local_point-AC_nmin) + AC_multigpu_offset
+    		return global_point*AC_ds + AC_first_gridpoint
+	}
 	if(AC_coordinate_system == AC_CARTESIAN_COORDINATES)
 	{
 		return 
