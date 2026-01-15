2ae10b953612c42f421a7077a4b03e705004cc47
Touko Puro <ToxPuro>
2026-01-15 11:59:20 +0200

    BICGSTAB seems to work: see bicgstab-test

--- acc-runtime/stdlib/bicgstab.h
+++ acc-runtime/stdlib/bicgstab.h
@@ -1,34 +1,35 @@
 global output real BICGSTAB_r0Tv
 global output real BICGSTAB_rho_prev
-global output real BICGSTAB_rho_current
+global output real BICGSTAB_rho_next
 global output real BICGSTAB_tTs
 global output real BICGSTAB_tTt
 Field BICGSTAB_S
 Field BICGSTAB_H
 Field BICGSTAB_R
+Field BICGSTAB_R0
 Field BICGSTAB_T
 Field BICGSTAB_P
 Field BICGSTAB_V
 
-bicgstab_compute_r0Tv(real v, real r0)
+bicgstab_compute_v_and_r0Tv(real v)
 {
 	write(BICGSTAB_V,v)
-	reduce_sum(r0*v,BICGSTAB_r0Tv)
+	reduce_sum(BICGSTAB_R0*v,BICGSTAB_r0Tv)
 }
 
-bicgstab_compute_h_and_s(Field x, real v)
+bicgstab_compute_h_and_s(Field x)
 {
 	alpha = BICGSTAB_rho_prev/BICGSTAB_r0Tv
-	write(BICGSTAB_S,BICGSTAB_R - alpha*v)
-	write(BICGTAB_H,x + alpha*BICGSTAB_P)
+	write(BICGSTAB_S,BICGSTAB_R - alpha*BICGSTAB_V)
+	write(BICGSTAB_H,x + alpha*BICGSTAB_P)
 }
 
 bicgstab_compute_t(real As)
 {
 	t = As
 	write(BICGSTAB_T,As)
-	reduce_sum(BICGSTAB_tTt,t*t)
-	reduce_sum(BICGSTAB_tTs,t*s)
+	reduce_sum(t*t,BICGSTAB_tTt)
+	reduce_sum(t*BICGSTAB_S,BICGSTAB_tTs)
 }
 
 bicgstab_compute_next_solution(Field x)
@@ -37,14 +38,14 @@ bicgstab_compute_next_solution(Field x)
 	write(x,BICGSTAB_H + omega*BICGSTAB_S)
 	write(BICGSTAB_R,BICGSTAB_S - omega*BICGSTAB_T)
 }
-bicgstab_compute_rho_next(real r0) 
+bicgstab_compute_rho_next() 
 {
-	reduce_sum(r0*BICGSTAB_R,BICGSTAB_rho_next)
+	reduce_sum(BICGSTAB_R0*BICGSTAB_R,BICGSTAB_rho_next)
 }
 
-bicgstab_compute_rho_prev(real r0) 
+bicgstab_compute_rho_prev() 
 {
-	reduce_sum(r0*BICGSTAB_R,BICGSTAB_rho_prev)
+	reduce_sum(BICGSTAB_R0*BICGSTAB_R,BICGSTAB_rho_prev)
 }
 
 bicgstab_update_p() 
@@ -52,6 +53,6 @@ bicgstab_update_p()
 	alpha = BICGSTAB_rho_prev/BICGSTAB_r0Tv
 	omega = BICGSTAB_tTs/BICGSTAB_tTt
 	beta = (BICGSTAB_rho_next/BICGSTAB_rho_prev)*(alpha/omega)
-	write(BICGSTAP_P,BICGSTAB_R + beta*(BICGSTAB_P - omega*BICGSTAB_V)
+	write(BICGSTAB_P,BICGSTAB_R + beta*(BICGSTAB_P - omega*BICGSTAB_V))
 }
 
