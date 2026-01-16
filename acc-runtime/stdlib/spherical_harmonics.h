bd0746f3ff02c2d8c59fb8c8eb7c81c8b5439418
Touko Puro <ToxPuro>
2026-01-16 10:55:26 +0200

    bcs ought to take G

--- acc-runtime/stdlib/spherical_harmonics.h
+++ acc-runtime/stdlib/spherical_harmonics.h
@@ -188,7 +188,7 @@ calculate_expansion_with_mlm_extended(Field lower, Field upper)
 	write(lower,res)
 }
 
-multipole_expansion_bc_inner(AcBoundary boundary_region, Field f)
+multipole_expansion_bc_inner(AcBoundary boundary_region, Field f, real G)
 {
     const int3 normal = get_normal(boundary_region)
     const int3 boundary = get_boundary(normal)
@@ -220,12 +220,49 @@ multipole_expansion_bc_inner(AcBoundary boundary_region, Field f)
             }
         }
     }
+    res *= G
     f[ghost.x][ghost.y][ghost.z] = res
-
     }
 }
 
-multipole_expansion_bc_outer(AcBoundary boundary_region, Field f)
+multipole_expansion_bc_inner_extended(AcBoundary boundary_region, Field f, real G)
+{
+    const int3 normal = get_normal(boundary_region)
+    const int3 boundary = get_boundary(normal)
+    int3 domain = boundary
+    int3 ghost  = boundary
+    for i in 0:NGHOST
+    {
+            domain = domain - normal
+            ghost  = ghost  + normal
+    r = grid_position_extended(ghost).x
+
+    
+    res = 0.0
+    for l in 0:AC_n_spherical_harmonics
+    {
+        r_factor = 1.0
+        for i in 0:l
+        {
+            r_factor *= r
+        }
+        for m in 0:l+1
+        {
+            res -= r_factor*AC_lower_positive_MLM[l + m*AC_n_spherical_harmonics]*positive_real_spherical_harmonic(m,l) *  sqrt(4*AC_REAL_PI/(2*l+1))
+
+            if(m > 0)
+            {
+                res -= r_factor*AC_lower_negative_MLM[l + m*AC_n_spherical_harmonics]*negative_real_spherical_harmonic(m,l) * sqrt(4*AC_REAL_PI/(2*l+1))
+
+            }
+        }
+    }
+    res *= G
+    f[ghost.x][ghost.y][ghost.z] = res
+    }
+}
+
+multipole_expansion_bc_outer(AcBoundary boundary_region, Field f, real G)
 {
     const int3 normal = get_normal(boundary_region)
     const int3 boundary = get_boundary(normal)
@@ -255,7 +292,42 @@ multipole_expansion_bc_outer(AcBoundary boundary_region, Field f)
                 }
             }
         }
+    res *= G
+    f[ghost.x][ghost.y][ghost.z] = res
+    }
+}
 
+multipole_expansion_bc_outer_extended(AcBoundary boundary_region, Field f, real G)
+{
+    const int3 normal = get_normal(boundary_region)
+    const int3 boundary = get_boundary(normal)
+    int3 domain = boundary
+    int3 ghost  = boundary
+    for i in 0:NGHOST
+    {
+        domain = domain - normal
+        ghost  = ghost  + normal
+        r = grid_position_extended(ghost).x
+        res = 0.0
+        for l in 0:AC_n_spherical_harmonics
+        {
+            r_factor = 1.0/r
+            for i in 0:l
+            {
+                r_factor /= r
+            }
+            for m in 0:l+1
+            {
+                res -= r_factor*AC_upper_positive_MLM[l + m*AC_n_spherical_harmonics]*positive_real_spherical_harmonic(m,l) * sqrt(4*AC_REAL_PI/(2*l+1))
+ 
+                if(m > 0)
+                {
+                    res -= r_factor*AC_upper_negative_MLM[l + m*AC_n_spherical_harmonics]*negative_real_spherical_harmonic(m,l) * sqrt(4*AC_REAL_PI/(2*l+1))
+
+                }
+            }
+        }
+    res *= G
     f[ghost.x][ghost.y][ghost.z] = res
     }
 }
