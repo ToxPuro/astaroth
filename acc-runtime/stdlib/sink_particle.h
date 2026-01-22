00aab5e17ac248ab8f4eae31451da273a84bd9b0
Touko Puro <ToxPuro>
2026-01-22 08:38:30 +0200

    bug fixes

--- acc-runtime/stdlib/sink_particle.h
+++ acc-runtime/stdlib/sink_particle.h
@@ -4,7 +4,7 @@ real AC_central_sink_particle_mass = 0.0
 calculate_sink_particle_rhs(real radial_velocity, real density)
 {
 	r = AC_r[vertexIdx.x]
-	momentum_flux = density*radial_velocity*r*r*sin(AC_sin_theta[vertexIdx.y])
+	momentum_flux = density*radial_velocity*r*r*AC_sin_theta[vertexIdx.y]
 	//Only flux at the inner radius goes to the central sink particle
 	flux_going_to_sink_particle  = (radial_velocity < 0.0 && globalVertexIdx.x == NGHOST)*momentum_flux
 	reduce_sum(flux_going_to_sink_particle,AC_central_sink_particle_rhs)
@@ -12,7 +12,7 @@ calculate_sink_particle_rhs(real radial_velocity, real density)
 
 force_from_sink_particle(real G)
 {
-	r = AC_r[vertexIdx.x]*AC_r[vertexIdx.x]
+	r = AC_r[vertexIdx.x]
 	return -real3(
 			(G*AC_central_sink_particle_mass)/(r*r),
 			0.0,
