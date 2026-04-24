15e14d981ed2be41926e5ca45e4ac7894a3134cb
Ondřej Míchal <harrymichal@seznam.cz>
2026-04-24 16:31:31 +0200

    include: Cleanup astaroth.h to be easier to follow
    This moves many of the different helpers to astaroth_helpers.h, matches
    astaroth_helpers.h to what has been done in astaroth.h and tries to
    arange the sources in a more semantic way. First system includes (even
    the conditional ones), then the project includes, then type declarations
    and lastly functions, ideally in module and alphabetic order.
    
    Also adds a missing #pragma once to astaroth_helpers.h to prevent
    including it multiple times.
    
    Continuation from a2d7bf5a8c4e3a712377e26fc7c59d8f72471d4f.

--- include/astaroth.h
+++ include/astaroth.h
@@ -18,307 +18,28 @@
 */
 #pragma once
 
+#include <string.h>
+
+#ifdef __cplusplus
+#include <type_traits>
+#endif
+
 #include "astaroth_base.h"
+#include "astaroth_helpers.h"
+#include "astaroth_runtime_compilation.h"
+
+#if AC_RUNTIME_COMPILATION
+#include "astaroth_lib.h"
+#endif
+
+#include "device_set_input_decls.h"
+#include "device_get_output_decls.h"
+#include "device_get_input_decls.h"
+#include "get_vtxbufs_declares.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-int3 acConstructInt3Param(const AcIntParam a, const AcIntParam b, const AcIntParam c,
-                          const AcMeshInfo info);
-
-static inline AcReal3
-acConstructReal3Param(const AcRealParam a, const AcRealParam b, const AcRealParam c,
-                     const AcMeshInfo info)
-{
-    return (AcReal3){
-        info.real_params[a],
-        info.real_params[b],
-        info.real_params[c],
-    };
-}
-
-/*
- * =============================================================================
- * Helper functions
- * =============================================================================
- */
-
-FUNC_DEFINE(Volume, acGetLocalNN, (const AcMeshInfo info));
-FUNC_DEFINE(Volume, acGetLocalMM, (const AcMeshInfo info));
-FUNC_DEFINE(Volume, acGetGridNN, (const AcMeshInfo info));
-FUNC_DEFINE(Volume, acGetGridMM, (const AcMeshInfo info));
-FUNC_DEFINE(Volume, acGetMinNN, (const AcMeshInfo info));
-FUNC_DEFINE(Volume, acGetMaxNN, (const AcMeshInfo info));
-FUNC_DEFINE(Volume, acGetGridMaxNN, (const AcMeshInfo info));
-FUNC_DEFINE(AcReal3, acGetLengths, (const AcMeshInfo info));
-
-
-static inline size_t
-acVertexBufferSize(const AcMeshInfo info)
-{
-    const Volume mm = acGetLocalMM(info);
-    return mm.x*mm.y*mm.z;
-}
-
-static inline size_t
-acVertexBufferVariableSize(const AcMeshInfo info, const VertexBufferHandle vtxbuf)
-{
-    const Volume mm = 
-	    (Volume)
-	    {
-	    	(size_t)info.int3_params[vtxbuf_dims[vtxbuf]].x,
-	    	(size_t)info.int3_params[vtxbuf_dims[vtxbuf]].y,
-	    	(size_t)info.int3_params[vtxbuf_dims[vtxbuf]].z
-	    };
-    return mm.x*mm.y*mm.z;
-}
-
-static inline size_t
-acGridVertexBufferSize(const AcMeshInfo info)
-{
-    const Volume mm = acGetGridMM(info);
-    return mm.x*mm.y*mm.z;
-}
-
-static inline Volume 
-acVertexBufferDims(const AcMeshInfo info)
-{
-    return acGetLocalMM(info);
-}
-
-static inline Volume 
-acVertexBufferDimsVariable(const AcMeshInfo info, const VertexBufferHandle vtxbuf)
-{
-	return 
-		(Volume)
-		{
-			(size_t)info.int3_params[vtxbuf_dims[vtxbuf]].x,
-			(size_t)info.int3_params[vtxbuf_dims[vtxbuf]].x,
-			(size_t)info.int3_params[vtxbuf_dims[vtxbuf]].z
-		};
-}
-
-static inline size_t
-acVertexBufferSizeBytes(const AcMeshInfo info)
-{
-    return sizeof(AcReal) * acVertexBufferSize(info);
-}
-
-static inline size_t
-acVertexBufferSizeBytesVariable(const AcMeshInfo info, const VertexBufferHandle vtxbuf)
-{
-    return sizeof(AcReal) * acVertexBufferVariableSize(info,vtxbuf);
-}
-
-
-static inline size_t
-acVertexBufferCompdomainSize(const AcMeshInfo info)
-{
-    const Volume nn = acGetLocalNN(info);
-    return nn.x*nn.y*nn.z;
-}
-
-static inline size_t
-acVertexBufferCompdomainSizeVariable(const AcMeshInfo info, const VertexBufferHandle vtxbuf)
-{
-    const Volume nmin = acGetMinNN(info);
-    const Volume nn = 
-	    (Volume)
-	    {
-		    (size_t)info.int3_params[vtxbuf_dims[vtxbuf]].x -2*nmin.x,
-		    (size_t)info.int3_params[vtxbuf_dims[vtxbuf]].y -2*nmin.y,
-		    (size_t)info.int3_params[vtxbuf_dims[vtxbuf]].z -2*nmin.z
-	    };
-    return nn.x*nn.y*nn.z;
-}
-
-static inline size_t
-acVertexBufferCompdomainSizeBytes(const AcMeshInfo info)
-{
-    return sizeof(AcReal) * acVertexBufferCompdomainSize(info);
-}
-
-static inline size_t
-acVertexBufferCompdomainSizeBytesVariable(const AcMeshInfo info, const VertexBufferHandle vtxbuf)
-{
-    return sizeof(AcReal) * acVertexBufferCompdomainSizeVariable(info,vtxbuf);
-}
-
-static inline AcMeshDims
-acGetMeshDims(const AcMeshInfo info)
-{
-   const Volume n0 = acGetMinNN(info);
-   const Volume n1 = acGetMaxNN(info);
-   const Volume m0 = (Volume){0, 0, 0};
-   const Volume m1 = acGetLocalMM(info);
-   const Volume nn = acGetLocalNN(info);
-   const Volume reduction_tile = (Volume)
-   {
-	   as_size_t(info.int3_params[AC_reduction_tile_dimensions].x),
-	   as_size_t(info.int3_params[AC_reduction_tile_dimensions].y),
-	   as_size_t(info.int3_params[AC_reduction_tile_dimensions].z)
-   };
-
-   return (AcMeshDims){
-       .n0 = n0,
-       .n1 = n1,
-       .m0 = m0,
-       .m1 = m1,
-       .nn = nn,
-       .reduction_tile = reduction_tile,
-   };
-}
-
-static inline AcMeshDims
-acGetGridMeshDims(const AcMeshInfo info)
-{
-   const Volume n0 = acGetMinNN(info);
-   const Volume n1 = acGetGridMaxNN(info);
-   const Volume m0 = (Volume){0, 0, 0};
-   const Volume m1 = acGetGridMM(info);
-   const Volume nn = acGetGridNN(info);
-   const Volume reduction_tile = (Volume)
-   {
-	   as_size_t(info.int3_params[AC_reduction_tile_dimensions].x),
-	   as_size_t(info.int3_params[AC_reduction_tile_dimensions].y),
-	   as_size_t(info.int3_params[AC_reduction_tile_dimensions].z)
-   };
-
-   return (AcMeshDims){
-       .n0 = n0,
-       .n1 = n1,
-       .m0 = m0,
-       .m1 = m1,
-       .nn = nn,
-       .reduction_tile = reduction_tile,
-   };
-}
-
-FUNC_DEFINE(size_t, acGetKernelId,(const AcKernel kernel));
-
-
-FUNC_DEFINE(AcResult, acAnalysisGetKernelInfo,(const AcMeshInfo info, KernelAnalysisInfo* dst));
-FUNC_DEFINE(KernelAnalysisInfo, acAnalysisGetKernelInfoSingleWithInputParams,(const AcMeshInfo info, const AcKernel kernel, const acKernelInputParams input_params));
-FUNC_DEFINE(KernelAnalysisInfo, acAnalysisGetKernelInfoSingle,(const AcMeshInfo info, const AcKernel kernel));
-FUNC_DEFINE(AcResult, acAnalysisCheckForDSLErrors,(const AcMeshInfo info));
-	
-
-
-
-FUNC_DEFINE(size_t, acGetKernelIdByName,(const char* name));
-
-
-FUNC_DEFINE(AcMeshInfo, acGridDecomposeMeshInfo,(const AcMeshInfo global_config));
-
-#if AC_RUNTIME_COMPILATION == 0
-FUNC_DEFINE(VertexBufferArray, acGridGetVBA,(void));
-#endif
-
-FUNC_DEFINE(AcMeshInfo, acGridGetLocalMeshInfo,(void));
-
-#ifdef __cplusplus
-//TP: this is done for perf optim since if acVertexBufferIdx is called often
-//Making it an external function call is quite expensive
-static inline size_t
-acGridVertexBufferIdx(const int i, const int j, const int k, const AcMeshInfo info)
-{
-    const int3 mm = info[AC_mgrid];
-    return AC_INDEX_ORDER(i,j,k,mm.x,mm.y,mm.z);
-}
-static inline size_t
-acVertexBufferIdx(const int i, const int j, const int k, const AcMeshInfo info)
-{
-    const int3 mm = info[AC_mlocal];
-    return AC_INDEX_ORDER(i,j,k,mm.x,mm.y,mm.z);
-}
-#else
-static inline size_t
-acVertexBufferIdx(const int i, const int j, const int k, const AcMeshInfo info)
-{
-    const Volume mm = acGetLocalMM(info);
-    return AC_INDEX_ORDER(i,j,k,mm.x,mm.y,mm.z);
-}
-static inline size_t
-acGridVertexBufferIdx(const int i, const int j, const int k, const AcMeshInfo info)
-{
-    const Volume mm = acGetGridMM(info);
-    return AC_INDEX_ORDER(i,j,k,mm.x,mm.y,mm.z);
-}
-#endif
-
-static inline size_t
-acVertexBufferIdxVariable(const int i, const int j, const int k, const AcMeshInfo info, const VertexBufferHandle vtxbuf)
-{
-    const int3 mm = info.int3_params[vtxbuf_dims[vtxbuf]];
-    return AC_INDEX_ORDER(i,j,k,mm.x,mm.y,mm.z);
-}
-
-static inline int3
-acVertexBufferSpatialIdx(const size_t i, const AcMeshInfo info)
-{
-    const Volume mm = acGetLocalMM(info);
-
-    return (int3){
-        (int)i % (int)mm.x,
-        ((int)i % (int)(mm.x * mm.y)) / (int)mm.x,
-        (int)i / (int)(mm.x * mm.y),
-    };
-}
-
-
-
-/** Prints a list of initial condition condition types */
-void acQueryInitcondtypes(void);
-
-/** Prints a list of int parameters */
-void acQueryIntparams(void);
-
-/** Prints a list of int3 parameters */
-void acQueryInt3params(void);
-
-/** Prints a list of real parameters */
-void acQueryRealparams(void);
-
-/** Prints a list of real3 parameters */
-void acQueryReal3params(void);
-
-/** Prints a list of Scalar array handles */
-/*
-static inline void
-acQueryScalarrays(void)
-{
-    for (int i = 0; i < NUM_REAL_ARRS_1D; ++i)
-        printf("%s (%d)\n", realarr1D_names[i], i);
-}
-*/
-
-/** Prints a list of vertex buffer handles */
-static inline void
-acQueryVtxbufs(void)
-{
-    for (int i = 0; i < NUM_ALL_FIELDS; ++i)
-        printf("%s (%d)\n", vtxbuf_names[i], i);
-}
-
-/** Prints a list of kernels */
-void acQueryKernels(void);
-
-static inline void
-acPrintIntParam(const AcIntParam a, const AcMeshInfo info)
-{
-    printf("%s: %d\n", intparam_names[a], info.int_params[a]);
-}
-
-void acPrintIntParams(const AcIntParam a, const AcIntParam b, const AcIntParam c,
-                      const AcMeshInfo info);
-
-static inline void
-acPrintInt3Param(const AcInt3Param a, const AcMeshInfo info)
-{
-    const int3 vec = info.int3_params[a];
-    printf("{%s: (%d, %d, %d)}\n", int3param_names[a], vec.x, vec.y, vec.z);
-}
 
 /*
  * =============================================================================
@@ -380,6 +101,7 @@ FUNC_DEFINE(Node, acGetNode,(void));
  * =============================================================================
  */
 #include "astaroth_legacy.h"
+
 /*
  * =============================================================================
  * Helper functions
@@ -391,7 +113,32 @@ acHostUpdateParams(AcMeshInfo* config);
 AcResult 
 acHostUpdateCompParams(AcMeshInfo* config);
 
+/** Inits the profile to cosine wave */
+AcResult acHostInitProfileToCosineWave(const AcReal spacing, const AcReal initial_pos,
+                                       const AcReal amplitude, const AcReal wavenumber,
+                                       const size_t mz, AcReal* profile);
 
+/** Inits the profile to sine wave */
+AcResult acHostInitProfileToSineWave(const AcReal spacing, const AcReal initial_pos,
+                                     const AcReal amplitude, const AcReal wavenumber,
+                                     const size_t mz, AcReal* profile);
+
+/** Initialize a profile to a constant value */
+AcResult acHostInitProfileToValue(const long double value, const size_t profile_count,
+                                  AcReal* profile);
+
+/** Writes the host profile to a file */
+AcResult acHostWriteProfileToFile(const char* filepath, const AcReal* profile,
+                                  const size_t profile_count);
+
+/** Sets the dimensions of the computational grid to (nx, ny, nz) and recalculates the built-in
+ * parameters derived from them (mx, my, mz, nx_min, and others) */
+AcResult acSetGridMeshDims(const size_t nx, const size_t ny, const size_t nz, AcMeshInfo* info);
+
+/** Sets the dimensions of the computational subdomain to (nx, ny, nz) and recalculates the built-in
+ * parameters derived from them (mx, my, mz, nx_min, and others) */
+
+AcResult acSetLocalMeshDims(const size_t nx, const size_t ny, const size_t nz, AcMeshInfo* info);
 
 OVERLOADED_FUNC_DEFINE(AcReal*, acHostCreateVertexBuffer,(const AcMeshInfo info));
 FUNC_DEFINE(AcReal*, acHostCreateVertexBufferVariable,(const AcMeshInfo info, const VertexBufferHandle vtxbuf));
@@ -416,24 +163,6 @@ FUNC_DEFINE(AcResult, acHostGridMeshRandomize,(AcMesh* mesh));
 
 /** Destroys a mesh stored in host memory */
 FUNC_DEFINE(AcResult, acHostMeshDestroy,(AcMesh* mesh));
-
-FUNC_DEFINE(void, acStoreConfig,(const AcMeshInfo info, const char* filename));
-
-/** Prints all parameters inside AcMeshInfo */
-static inline void
-acPrintMeshInfo(const AcMeshInfo config)
-{
-    acStoreConfig(config,NULL);
-}
-
-/** Sets the dimensions of the computational grid to (nx, ny, nz) and recalculates the built-in
- * parameters derived from them (mx, my, mz, nx_min, and others) */
-AcResult acSetGridMeshDims(const size_t nx, const size_t ny, const size_t nz, AcMeshInfo* info);
-
-/** Sets the dimensions of the computational subdomain to (nx, ny, nz) and recalculates the built-in
- * parameters derived from them (mx, my, mz, nx_min, and others) */
-
-AcResult acSetLocalMeshDims(const size_t nx, const size_t ny, const size_t nz, AcMeshInfo* info);
 
 /*
  * =============================================================================
@@ -461,17 +190,10 @@ acGetPid3D(const uint64_t pid, const int3 decomp, const AcMeshInfo info);
 int
 acGetPid(const int3 pid, const int3 decomp, const AcMeshInfo info);
 
-
-#include "device_set_input_decls.h"
-#include "device_get_output_decls.h"
-#include "device_get_input_decls.h"
-#include "get_vtxbufs_declares.h"
-
 #if AC_RUNTIME_COMPILATION
-#include "astaroth_lib.h"
 
 #define LOAD_DSYM(FUNC_NAME,STREAM) *(void**)(&FUNC_NAME) = dlsym(handle,#FUNC_NAME); \
-			     if(!FUNC_NAME && STREAM) fprintf(STREAM,"Astaroth warning: was not able to load %s\n",#FUNC_NAME);
+  if(!FUNC_NAME && STREAM) fprintf(STREAM,"Astaroth warning: was not able to load %s\n",#FUNC_NAME);
 
   static AcResult __attribute__((unused)) acLoadLibrary(FILE* stream, const AcMeshInfo info)
   {
@@ -729,28 +451,58 @@ acGetPid(const int3 pid, const int3 decomp, const AcMeshInfo info);
   static AcResult __attribute__((unused)) acCloseLibrary() {return AC_FAILURE;}
 #endif
 
-/** Inits the profile to cosine wave */
-AcResult acHostInitProfileToCosineWave(const AcReal spacing, const AcReal initial_pos,
-                                       const AcReal amplitude, const AcReal wavenumber,
-                                       const size_t mz, AcReal* profile);
-
-/** Inits the profile to sine wave */
-AcResult acHostInitProfileToSineWave(const AcReal spacing, const AcReal initial_pos,
-                                     const AcReal amplitude, const AcReal wavenumber,
-                                     const size_t mz, AcReal* profile);
-
-/** Initialize a profile to a constant value */
-AcResult acHostInitProfileToValue(const long double value, const size_t profile_count,
-                                  AcReal* profile);
-
-/** Writes the host profile to a file */
-AcResult acHostWriteProfileToFile(const char* filepath, const AcReal* profile,
-                                  const size_t profile_count);
-
 #ifdef __cplusplus
 } // extern "C"
 #endif
 
+static AcCompInfo UNUSED
+acInitCompInfo()
+{
+      AcCompInfo res;
+      //TP: initially nothing is loaded and if they are not loaded their values 
+      //might as well be zero since then a default value is used for them
+      memset(&res,0,sizeof(res));
+      return res;
+}
+
+static AcMeshInfo UNUSED
+acInitInfo()
+{
+      AcMeshInfo res;
+      //TP: this is useful for the following reasons:
+      //All enums are initialized by default to the first enum value
+      //All array ptrs are initialized to nulls
+      //All booleans are initialized to false
+      //All booleans about whether values are loaded are false
+      memset(&res,0,sizeof(res));
+      // memset reads the second parameter as a byte even though it says int in
+      // the function declaration
+      //TP: for backwards compatibility set original datatypes to all ones as before
+      memset(&res.int_params,     (uint8_t)0xFF, sizeof(res.int_params));
+      memset(&res.real_params,    (uint8_t)0xFF, sizeof(res.real_params));
+      memset(&res.int3_params,    (uint8_t)0xFF, sizeof(res.int3_params));
+      memset(&res.real3_params,   (uint8_t)0xFF, sizeof(res.real3_params));
+      memset(&res.complex_params, (uint8_t)0xFF, sizeof(res.complex_params));
+
+#if AC_MPI_ENABLED
+      res.comm = (AcCommunicator*)malloc(sizeof(AcCommunicator));
+      res.comm->handle = MPI_COMM_NULL;
+#endif
+      res.run_consts = acInitCompInfo();
+      return res;
+}
+
+static AcMesh UNUSED
+acInitMesh()
+{
+      AcMesh res;
+      for(size_t j = 0; j < NUM_VTXBUF_HANDLES; ++j)
+      {
+	      res.vertex_buffer[j] = NULL;
+      }
+      res.info = acInitInfo();
+      return res;
+}
 
 #ifdef __cplusplus
 
@@ -1341,13 +1093,6 @@ acGridBuildTaskGraph(const std::vector<AcTaskDefinition> ops, const Volume start
     return BASE_FUNC_NAME(acGridBuildTaskGraphWithBounds)(ops.data(), ops.size(),start,end,globally_imposed_bcs);
 }
 #endif
-#endif
-
-#include "astaroth_runtime_compilation.h"
-
-#ifdef __cplusplus
-#include <type_traits>
-
 
 template <typename P, typename V>
 void
@@ -1387,48 +1132,3 @@ acUpdateDecompositionParams(AcMeshInfo* dst)
 }
 
 #endif
-#include <string.h>
-
-  static AcCompInfo UNUSED acInitCompInfo()
-  {
-	  AcCompInfo res;
-	  //TP: initially nothing is loaded and if they are not loaded their values 
-	  //might as well be zero since then a default value is used for them
-	  memset(&res,0,sizeof(res));
-	  return res;
-  }
-  static AcMeshInfo UNUSED acInitInfo()
-  {
-	  AcMeshInfo res;
-	  //TP: this is useful for the following reasons:
-	  //All enums are initialized by default to the first enum value
-	  //All array ptrs are initialized to nulls
-	  //All booleans are initialized to false
-	  //All booleans about whether values are loaded are false
-	  memset(&res,0,sizeof(res));
-    	  // memset reads the second parameter as a byte even though it says int in
-          // the function declaration
-	  //TP: for backwards compatibility set original datatypes to all ones as before
-    	  memset(&res.int_params,     (uint8_t)0xFF, sizeof(res.int_params));
-    	  memset(&res.real_params,    (uint8_t)0xFF, sizeof(res.real_params));
-    	  memset(&res.int3_params,    (uint8_t)0xFF, sizeof(res.int3_params));
-    	  memset(&res.real3_params,   (uint8_t)0xFF, sizeof(res.real3_params));
-    	  memset(&res.complex_params, (uint8_t)0xFF, sizeof(res.complex_params));
-
-#if AC_MPI_ENABLED
-	  res.comm = (AcCommunicator*)malloc(sizeof(AcCommunicator));
-	  res.comm->handle = MPI_COMM_NULL;
-#endif
-	  res.run_consts = acInitCompInfo();
-	  return res;
-  }
-  static AcMesh UNUSED acInitMesh()
-  {
-	  AcMesh res;
-	  for(size_t j = 0; j < NUM_VTXBUF_HANDLES; ++j)
-	  {
-		  res.vertex_buffer[j] = NULL;
-	  }
-	  res.info = acInitInfo();
-	  return res;
-  }
