463c18683075657643c1df544d4c5a22f3aee87a
Ondřej Míchal <harrymichal@seznam.cz>
2026-05-28 14:43:05 +0300

    [WIP] Add initial proof-of-concept for PyAstaroth
    This is a minimal example of creating Python bindings for Astaroth using
    nanobind in a way that integrates with the exisitng CMake build system
    and works with the proposed minimal example of a Python program using
    PyAstaroth.

---
+++ python/litgen_generate_bindings.py
@@ -0,0 +1,418 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2016 The Pybind Development Team, All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its contributors
+#    may be used to endorse or promote products derived from this software
+#    without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# You are under no obligation whatsoever to provide any bug fixes, patches, or
+# upgrades to the features, functionality or performance of the source code
+# ("Enhancements") to anyone; however, if you choose to make your Enhancements
+# available either publicly, or directly to the author of this software, without
+# imposing a separate written license agreement for such Enhancements, then you
+# hereby grant the following license: a non-exclusive, royalty-free perpetual
+# license to install, use, modify, prepare derivative works, incorporate into
+# other computer software, distribute, and sublicense such enhancements or
+# derivative works thereof, in binary and source code form.
+
+import argparse
+import re
+import tempfile
+from pathlib import Path
+
+import litgen
+from codemanip import code_utils
+from codemanip.amalgamated_header import (
+    write_amalgamate_header_file,
+    AmalgamationOptions,
+)
+from codemanip.code_replacements import RegexReplacementList
+
+
+ASTAROTH_PUBLIC_HEADER: str = "astaroth.h"
+
+
+def get_litgen_options() -> litgen.LitgenOptions:
+    def get_function_names_replacements_regex() -> RegexReplacementList:
+        regex: RegexReplacementList = RegexReplacementList()
+        # All ac-prefixed functions are in an PyAstaroth Python module, so the
+        # the prefix is not needed.
+        regex.add_replacement(
+            "^ac_",
+            "",
+        )
+        # The FFT-related functions are all included in an fft Python module,
+        # so the prefix is not needed.
+        regex.add_replacement(
+            "^fft_",
+            "",
+        )
+
+        return regex
+
+    def get_type_replacements_regex() -> RegexReplacementList:
+        regex: RegexReplacementList = RegexReplacementList()
+        # regex.add_replacement(
+        #     "AcReal",
+        #     "float",
+        # )
+
+        return regex
+
+    def code_preprocess_function(code: str) -> str:
+        regex: RegexReplacementList = RegexReplacementList()
+
+        regex.add_replacement(
+            r"[/]{2}\s{2,}[\w\s\.]+\s{2,}[/]{2}",
+            r"///",
+        )
+
+        regex.add_replacement(
+            r"[/]{3,}",
+            r"",
+        )
+
+        regex.add_replacement(
+            r"\n{2,}",
+            r"\n\n",
+        )
+
+        regex.add_replacement(
+            r"\{\n{2,}",
+            r"{\n",
+        )
+
+        # regex.add_replacement(r"[^ a-z]AC_BEGIN_C_DECLARATIONS[^;]", r"\nAC_BEGIN_C_DECLARATIONS;\n")
+        #
+        # regex.add_replacement(r"[^ a-z]AC_END_C_DECLARATIONS[^;]", r"\nAC_END_C_DECLARATIONS;\n")
+
+        regex.add_replacement(r"[^ a-z]AC_BEGIN_C_DECLARATIONS[^;]", r"")
+
+        regex.add_replacement(r"[^ a-z]AC_END_C_DECLARATIONS[^;]", r"")
+
+        regex.add_replacement(
+            r"#define UNUSED",
+            r"",
+        )
+
+        regex.add_replacement(
+            r"UNUSED",
+            r"",
+        )
+
+        regex.add_replacement(
+            r"__attribute__\(\(unused\)\)",
+            r"",
+        )
+
+        regex.add_replacement(
+            r"\[\[maybe_unused\]\]",
+            r"",
+        )
+
+        # "typedef enum { ... } <type>;" into "enum <type> { ... };"
+        regex.add_replacement(
+            r"typedef\s+enum[\s\w]*\{((?s:.*?))\}\s*(\w+)\s*;",
+            r"enum \2 {\1};",
+        )
+
+        # "typedef struct { ... } <type>;" into "struct <type> { ... };"
+        regex.add_replacement(
+            r"typedef\s+struct\s*\w*\s*{((?s:.*?))}\s*(\w+)\s*;",
+            r"struct \2 {\1};",
+        )
+
+        # "typedef union { ... } <type>;" into "union <type> { ... };"
+        regex.add_replacement(
+            r"typedef\s+union\s*\w*\s*{((?s:.*?))}\s*(\w+)\s*;",
+            r"union \2 {\1};",
+        )
+
+        # "typedef <type> <alias>;" into "using <alias> = <type>;"
+        regex.add_replacement(
+            r"typedef\s([\w\s*]+)\s\b(\w+);",
+            r"using \2 = \1;",
+        )
+
+        # "... UNUSED|HOST_INLINE|HOST_DEVICE_INLINE ..." into "... ..."
+        regex.add_replacement(
+            r"UNUSED|HOST_INLINE|HOST_DEVICE_INLINE",
+            r"",
+        )
+
+        # "OVERLOADED_FUNC_DEFINE(<return_type>, <func_name>, <func_params>)" into "<return_type> <func_name> <func_params>"
+        regex.add_replacement(
+            r"OVERLOADED_FUNC_DEFINE\s*\(([\w\s\*]+)\s*,\s*([\w]+)\s*,\s*\(([\w\s\*,\[\]\(\)]*)\)\)",
+            r"\n", # FIXME: Ignored for now
+        )
+
+        # "FUNC_DEFINE(<return_type>, <func_name>, <func_params>)" into "<return_type> <func_name> <func_params>"
+        regex.add_replacement(
+            r"FUNC_DEFINE\s*\(([\w\s\*]+)\s*,\s*([\w]+)\s*,\s*\(([\w\s\*,\[\]\(\)]*)\)\)",
+            r"\1 \2(\3)",
+        )
+
+        # FIXME: Litgen does not properly pick up on the 'const' qualifier
+        # after the parameter list which affects the 'this' keyword in a member
+        # function.
+        # "<return_type> <func_signature> const <func_body>" into "<return_type> <func_signature> <func_body>"
+        regex.add_replacement(
+            r"\) const {",
+            r"\) {",
+        )
+
+        # for line in regex.apply(code).encode("UTF-8").splitlines():
+        #     print(line)
+
+        return regex.apply(code)
+
+    def fn_exclude_by_name_function(code) -> bool:
+        blacklist = [
+            # FIXME: Struggles with templates.
+            r"^AS_SIZE_T$",
+            r"^ceil",
+            r"^acDevice",
+            r"^acConstruct.+Param$",
+            # FIXME: Cannot use double (or more) pointers in parameters.
+            # r"^acMalloc|acLaunchCooperativeKernel",
+            # FIXME: Cannot return double (or more) pointers from funcftions.
+            # r"^ac_allocate_scratchpad_real|ac_allocate_scratchpad_int|ac_allocate_scratchpad_float$",
+            # FIXME: litgen does not properly overload the fun c
+            r"^acMemcpy",
+            # FIXME: Incorrectly uses BoxedInt in place of a AcReal array
+            r"^acKernelFlush",
+            # FIXME: litgen does not handle multi-dimensional arrays as func parameters.
+            "acLoadStencil",
+            "acStoreStencil",
+        ]
+
+        for pattern in blacklist:
+            if re.match(pattern, code):
+                return True
+
+        return False
+
+    def fn_exclude_by_param_type_function(code) -> bool:
+        blacklist = [
+            # FIXME: Litgen does not directly support unions, so some more
+            # scaffolding will be needed (see https://github.com/pthom/litgen/issues/9).
+            r"acKernelInputParams",
+            # FIXME: Depends on acKernelInputParams which needs to be first
+            # added support for.
+            r"DeviceVertexBufferArray",
+            # FIXME: Depends on DeviceVertexBufferArray which needs to be first
+            # added support for.
+            r"VertexBufferArray",
+        ]
+
+        for pattern in blacklist:
+            if re.match(pattern, code):
+                return True
+
+        return False
+
+    def class_exclude_by_name_function(code: str) -> bool:
+        blacklist = [
+            "DeviceConfiguration",
+            "AcReduceBuffer",
+            # FIXME: Getting error from nanobind -> error: invalid use of incomplete type ”struct LoadKernelParamsFunc”
+            "AcTaskDefinition",
+            # FIXME: Struct/classes with const members do not have a default constructor.
+            "AcReduction",
+            "ParamLoadingInfo",
+            # FIXME: Cannot use double (or more) pointers in parameters.
+            "ScalarReduceBuffer",
+            # FIXME: Depends on acKernelInputParams which needs to be first
+            # added support for.
+            "DeviceVertexBufferArray",
+            # FIXME: Depends on DeviceVertexBufferArray which needs to be first
+            # added support for.
+            "VertexBufferArray",
+        ]
+
+        print("MAYBE", code)
+        for item in blacklist:
+            if item in code:
+                # print("EXCLUDED", code)
+                return True
+
+        return False
+
+    def header_filter_acceptable_function(code: str) -> bool:
+        _default = r"__cplusplus|_h_$|_h$|_H$|_H_$|hpp$|HPP$|hxx$|HXX$"
+        if re.match(_default, code):
+            return True
+
+        custom = [
+            r"^AC_BEGIN_C_DECLARATIONS$",
+            r"^AC_END_C_DECLARATIONS$",
+            r"^AC_DOUBLE_PRECISION$",
+            r"^AC_MPI_ENABLED$",
+            r"^AC_RUNTIME_COMPILATION$",
+            # r"^AC_RUNTIME_SOURCE$",
+        ]
+        for pattern in custom:
+            if re.match(pattern, code):
+                return True
+
+        return False
+
+    options: litgen.LitgenOptions = litgen.LitgenOptions()
+
+    options.bind_library = litgen.BindLibraryType.nanobind
+    options.namespaces_root = ["ac"]
+    # options.python_run_black_formatter = True
+
+    # Names translation from C++ to Python
+    options.python_convert_to_snake_case: bool = True
+    options.function_names_replacements.merge_replacements(
+        get_function_names_replacements_regex()
+    )
+    options.type_replacements.merge_replacements(get_type_replacements_regex())
+
+    # Class, struct, and member adaptations
+    options.class_exclude_by_name__regex = class_exclude_by_name_function
+
+    # Function and method adaptations
+    options.fn_exclude_by_name__regex = fn_exclude_by_name_function
+    options.fn_exclude_by_param_type__regex = fn_exclude_by_param_type_function
+
+    # Templated functions options
+    options.fn_template_options.add_specialization(
+        "^TO_VOLUME$", ["dim3", "size3_t"], add_suffix_to_function_name=False
+    )
+    options.fn_template_options.add_specialization(
+        "^max|min$", ["Volume"], add_suffix_to_function_name=False
+    )
+    options.fn_template_options.add_specialization(
+        "^as_int|as_int64_t|AS_SIZE_T$",
+        ["size_t", "int"],
+        add_suffix_to_function_name=False,
+    )
+
+    # Make "immutable python types" modifiable, when passed by pointer or reference
+    # options.fn_params_replace_modifiable_immutable_by_boxed__regex = r".*"
+    options.fn_params_output_modifiable_immutable_to_return__regex = r".*"
+
+    # Adapt class members
+    options.member_numeric_c_array_types = code_utils.join_string_by_pipe_char(
+        [
+            options.member_numeric_c_array_types,
+            "size_t",
+            "AcReal",
+            "AcReduceOp",
+        ]
+    )
+
+    # Custom preprocess of the code
+    options.srcmlcpp_options.code_preprocess_function = code_preprocess_function
+
+    # Exclude certain regions based on preprocessor macros
+    options.srcmlcpp_options.header_filter_preprocessor_regions = True
+    options.srcmlcpp_options.header_filter_acceptable__regex = (
+        header_filter_acceptable_function
+    )
+
+    return options
+
+
+def get_amalgamation_options(
+    input_header: Path,
+    output_header: Path,
+    base_dir: Path,
+    include_directories: list[Path],
+) -> AmalgamationOptions:
+    include_directories = list(map(lambda x: str(x), include_directories))
+
+    options = AmalgamationOptions()
+
+    options.base_dir = str(base_dir)
+    options.include_subdirs = include_directories
+    options.main_header_file = input_header
+    options.dst_amalgamated_header_file = output_header
+
+    return options
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(
+        __name__,
+        description=(
+            "Generator of Python bindings for Astaroth. DO NOT use directly"
+            " unless you know what you're doing. Otherwise, use CMake with the"
+            " -DBUILD_PYTHON_BINDINGS option."
+        ),
+    )
+
+    parser.add_argument(
+        "output_cpp_pydef_file",
+        type=Path,
+        metavar="OUT_PYDEF",
+        help="",
+    )
+    parser.add_argument(
+        "output_stub_pyi_file",
+        type=Path,
+        metavar="OUT_STUBS_PYI",
+        help="",
+    )
+    parser.add_argument(
+        "base_dir",
+        type=Path,
+        metavar="BASE_DIR",
+        help="",
+    )
+    parser.add_argument(
+        "include_directories",
+        nargs="+",
+        type=str,
+        metavar="HEADER_FILES",
+        help="",
+    )
+
+    args = parser.parse_args()
+
+    with tempfile.TemporaryDirectory(delete=False) as temp_dir:
+        amalgamated_header: Path = Path(temp_dir, ASTAROTH_PUBLIC_HEADER)
+
+        write_amalgamate_header_file(
+            get_amalgamation_options(
+                ASTAROTH_PUBLIC_HEADER,
+                amalgamated_header,
+                args.base_dir,
+                args.include_directories,
+            )
+        )
+
+        litgen.write_generated_code_for_files(
+            options=get_litgen_options(),
+            input_cpp_header_files=[str(amalgamated_header)],
+            output_cpp_pydef_file=str(args.output_cpp_pydef_file),
+            output_stub_pyi_file=str(args.output_stub_pyi_file),
+        )
+
+
+if __name__ == "__main__":
+    main()
