3ae4d765d1211dd183890623f6d80d7986d4dd6e
Miikka Väisälä <mvaisala@pm.me>
2026-06-07 11:27:54 +0300

    Adding more MPI config info to make it compile in my system.

--- config/samples/subsonic_forced_turbulence/build_with_cmake.sh
+++ config/samples/subsonic_forced_turbulence/build_with_cmake.sh
@@ -14,7 +14,7 @@ git rev-parse HEAD > COMMIT_CODE.log
 # the correct path to astaroth/CMakeLists.txt
 
 # Default cmake configuration
-CMAKE_FLAGS="-DOPTIMIZE_MEM_ACCESSES=ON -DDOUBLE_PRECISION=ON -DMPI_ENABLED=OFF -DUSE_CUDA_AWARE_MPI=OFF"
+CMAKE_FLAGS="-DOPTIMIZE_MEM_ACCESSES=ON -DDOUBLE_PRECISION=ON -DMPI_ENABLED=ON -DUSE_CUDA_AWARE_MPI=OFF"
 
 # Parse command-line arguments
 while [[ "$#" -gt 0 ]]; do
@@ -24,8 +24,17 @@ while [[ "$#" -gt 0 ]]; do
             ;;
         --fedora44)
             echo "Setting up for Fedora 44"
-            CMAKE_FLAGS="$CMAKE_FLAGS -DBUILD_SAMPLES=OFF -DUSE_HIP=OFF -DUSE_CUDA_AWARE_MPI=OFF -DDSL_MODULE_DIR=../../../acc-runtime/samples/mhd_modular/ -DCMAKE_CXX_COMPILER=$HOME/gcc15/bin/g++ -DCMAKE_C_COMPILER=$HOME/gcc15/bin/gcc -DCMAKE_CUDA_HOST_COMPILER=$HOME/gcc15/bin/g++"
-            CMAKE_FLAGS="$CMAKE_FLAGS -DMPI_C_INCLUDE_PATH=$HOME/openmpi/v5/include/ -DMPI_C_LIBRARIES=$HOME/openmpi/v5/lib/libmpi.so -DGPU_ARCH=100"
+            CMAKE_FLAGS="$CMAKE_FLAGS -DBUILD_SAMPLES=OFF -DUSE_HIP=OFF -DUSE_CUDA_AWARE_MPI=OFF -DDSL_MODULE_DIR=../../../acc-runtime/samples/mhd_modular/"
+            CMAKE_FLAGS="$CMAKE_FLAGS -DCMAKE_CXX_COMPILER=$HOME/gcc15/bin/g++ -DCMAKE_C_COMPILER=$HOME/gcc15/bin/gcc -DCMAKE_CUDA_HOST_COMPILER=$HOME/gcc15/bin/g++"
+            CMAKE_FLAGS="$CMAKE_FLAGS -DMPI_C_COMPILER=$HOME/openmpi/v5/bin/mpicc"
+            CMAKE_FLAGS="$CMAKE_FLAGS -DMPI_CXX_COMPILER=$HOME/openmpi/v5/bin/mpicxx"
+            CMAKE_FLAGS="$CMAKE_FLAGS -DMPI_Fortran_COMPILER=$HOME/openmpi/v5/bin/mpifort"
+            CMAKE_FLAGS="$CMAKE_FLAGS -DMPIEXEC=$HOME/openmpi/v5/bin/mpiexec"
+            CMAKE_FLAGS="$CMAKE_FLAGS -DMPI_C_INCLUDE_PATH=$HOME/openmpi/v5/include"
+            CMAKE_FLAGS="$CMAKE_FLAGS -DMPI_C_LIBRARIES=$HOME/openmpi/v5/lib/libmpi.so"
+            CMAKE_FLAGS="$CMAKE_FLAGS -DMPI_CXX_INCLUDE_PATH=$HOME/openmpi/v5/include"
+            CMAKE_FLAGS="$CMAKE_FLAGS -DMPI_CXX_LIBRARIES=$HOME/openmpi/v5/lib/libmpi.so"
+            CMAKE_FLAGS="$CMAKE_FLAGS -DGPU_ARCH=100"
             ;;
         *)
             echo "Unknown parameter passed: $1"
