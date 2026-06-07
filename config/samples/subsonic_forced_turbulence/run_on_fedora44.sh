6b06f2a942986b65f350e375961154516f44b275
Miikka Väisälä <mvaisala@pm.me>
2026-06-07 17:19:34 +0300

    Updating Astaroth.conf

--- config/samples/subsonic_forced_turbulence/run_on_fedora44.sh
+++ config/samples/subsonic_forced_turbulence/run_on_fedora44.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-( mpirun -np 1 ./ac_run_mpi --config astaroth.conf >& output.log & )
+( mpirun -np 1 ./ac_run_mpi --config astaroth.conf --run-init-kernel randomize >& output.log & )
+
+#mpirun -np 1 ./ac_run_mpi --config astaroth.conf --run-init-kernel randomize 
 
