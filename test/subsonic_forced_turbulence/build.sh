90309572ec12d36ee579207f57fa6614a0e03c41
ToxPuro <touko.puro@aalto.fi>
2026-06-08 14:15:26 +0300

    add a test for subsonic_forced_turbulence

---
+++ test/subsonic_forced_turbulence/build.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# This is a sample script. Please copy it to the directory you want to run the
+# code in and customize occordingly. 
+
+# The following write the commit indentifier corresponding to the simulation
+# run  into a file. This is to help keep track what version of the code was
+# used to perform the simulation.
+
+git rev-parse HEAD > COMMIT_CODE.log
+
+# Run cmake to construct makefiles
+# In the case you compile in astaroth/build/ directory. Otherwise change ".." to
+# the correct path to astaroth/CMakeLists.txt
+
+# Default cmake configuration
+CMAKE_FLAGS="-DOPTIMIZE_MEM_ACCESSES=ON -DDOUBLE_PRECISION=ON -DMPI_ENABLED=ON -DRUNTIME_COMPILATION=ON -DOPTIMIZE_INPUT_PARAMS=ON -DUSE_CUDA_AWARE_MPI=OFF"
+
+# Parse command-line arguments
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --old_tiara)
+            CMAKE_FLAGS="$CMAKE_FLAGS -DBUILD_SAMPLES=OFF -DUSE_HIP=OFF -DUSE_CUDA_AWARE_MPI=OFF -DDSL_MODULE_DIR=../../../acc-runtime/samples/mhd_modular/ -DCMAKE_CXX_COMPILER=/software/opt/gcc/9.1.0/bin/gcc -DCMAKE_C_COMPILER=/software/opt/gcc/9.1.0/bin/gcc"
+            ;;
+        --fedora44)
+            echo "Setting up for Fedora 44"
+            CMAKE_FLAGS="$CMAKE_FLAGS -DBUILD_SAMPLES=OFF -DUSE_HIP=OFF -DUSE_CUDA_AWARE_MPI=OFF -DDSL_MODULE_DIR=../../../acc-runtime/samples/mhd_modular/"
+            CMAKE_FLAGS="$CMAKE_FLAGS -DCMAKE_CXX_COMPILER=$HOME/gcc15/bin/g++ -DCMAKE_C_COMPILER=$HOME/gcc15/bin/gcc -DCMAKE_CUDA_HOST_COMPILER=$HOME/gcc15/bin/g++"
+            CMAKE_FLAGS="$CMAKE_FLAGS -DMPI_C_COMPILER=$HOME/openmpi/v5/bin/mpicc"
+            CMAKE_FLAGS="$CMAKE_FLAGS -DMPI_CXX_COMPILER=$HOME/openmpi/v5/bin/mpicxx"
+            CMAKE_FLAGS="$CMAKE_FLAGS -DMPI_Fortran_COMPILER=$HOME/openmpi/v5/bin/mpifort"
+            CMAKE_FLAGS="$CMAKE_FLAGS -DMPIEXEC=$HOME/openmpi/v5/bin/mpiexec"
+            CMAKE_FLAGS="$CMAKE_FLAGS -DMPI_C_INCLUDE_PATH=$HOME/openmpi/v5/include"
+            CMAKE_FLAGS="$CMAKE_FLAGS -DMPI_C_LIBRARIES=$HOME/openmpi/v5/lib/libmpi.so"
+            CMAKE_FLAGS="$CMAKE_FLAGS -DMPI_CXX_INCLUDE_PATH=$HOME/openmpi/v5/include"
+            CMAKE_FLAGS="$CMAKE_FLAGS -DMPI_CXX_LIBRARIES=$HOME/openmpi/v5/lib/libmpi.so"
+            CMAKE_FLAGS="$CMAKE_FLAGS -DGPU_ARCH=100"
+            ;;
+        *)
+            echo "Unknown parameter passed: $1"
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+echo "Buiilding CMake..."
+
+# Run cmake with the selected flags
+mkdir -p build && cd build
+cmake $CMAKE_FLAGS ../../..
+
+echo "Compiling..."
+
+# Standard compilation
+
+make -j
+
---
+++ test/subsonic_forced_turbulence/run.sh
@@ -0,0 +1,3 @@
+rm -f timeseries.ts
+mpiexec -n 1 build/ac_run_mpi --run-init-kernel randomize --config ./astaroth.conf
+python3 $AC_HOME/analysis/test_tools/verify.py reference.ts timeseries.ts
