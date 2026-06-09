c4b02bb1a347cd50eff432accde04c2389480ad6
Ondřej Míchal <harrymichal@seznam.cz>
2026-06-09 20:29:02 +0300

    acc-runtime/api: Support rocPRIM 4.0 and above more portably
    Because we use ROCm-specific symbols even when we don't compile for ROCm
    or with ROCm headers on the build machine, we need to a do a little
    dance before checking the version of rocPRIM and chosing the right
    symbol as the macro value.
    
    Continuation from 5abc6185cd99a20e10633918d6080271d083a673

--- acc-runtime/api/device_details.h
+++ acc-runtime/api/device_details.h
@@ -1,17 +1,14 @@
-#include <rocprim/rocprim_version.hpp>
-
-#if ROCPRIM_VERSION >= 400000
-#define rocprim__warpSize() rocprim::arch::wavefront::min_size()
-#else
-#define rocprim__warpSize() rocprim::device_warp_size()
+#if !AC_USE_HIP || AC_CPU_BUILD
+// OM: I used the same value as is used in stencil_accesses.cpp because I don't have the time now
+// to figure out how to de-duplicate that particular bit of code.
+#define rocprim__warpSize() 64
 #endif
+
 #define rocprim__warpId()   rocprim::warp_id()
 #define rocprim__warp_shuffle_down rocprim::warp_shuffle_down
 #define rocprim__warp_shuffle rocprim::warp_shuffle
 
-#if AC_CPU_BUILD
-
-#else
+#if !AC_CPU_BUILD
 
 #if AC_USE_HIP
 #include <string.h> // OM: At least with ROCm 7.2.1 and rocPRIM 7.2.0 on Fedora 44 rocPRIM sources
@@ -21,6 +18,13 @@
 #include <hip/hip_runtime.h> // Needed in files that include kernels
 #include <rocprim/rocprim.hpp>
 #include <hip/hip_cooperative_groups.h>
+
+#include <rocprim/rocprim_version.hpp>
+#if ROCPRIM_VERSION >= 400000
+#define rocprim__warpSize() rocprim::arch::wavefront::min_size()
+#else
+#define rocprim__warpSize() rocprim::device_warp_size()
+#endif
 #else
 #include <cooperative_groups.h>
 #endif
