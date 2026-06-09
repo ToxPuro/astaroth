ToxPuro/astaroth

commit e254afb48937aa43150a5bc1f32a169eb3db5a0c

test, test-builds: Run builds in build system-agnostic way
Since commit 259dc21afd38a12eebc3f5bfcd69dd0428c87511 Astaroth can be
build using the Ninja build system instead of just with Make. With this
patch we start using the CMAKE_GENERATOR [0] env variable to override
the default generator CMake will use for builds.

[0] https://cmake.org/cmake/help/latest/envvar/CMAKE_GENERATOR.html

test-builds/runtime-compilation/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #
-mkdir -p build && cd build && cmake -DRUNTIME_COMPILATION=ON -DOPTIMIZE_INPUT_PARAMS=ON -DOPTIMIZE_MEM_ACCESSES=ON -DCMAKE_BUILD_TYPE=Release -DDSL_MODULE_DIR="$AC_HOME/acc-runtime/samples/mhd_modular" -DBUILD_UTILS=ON -DBUILD_SAMPLES=ON -DDOUBLE_PRECISION=ON -DMULTIGPU_ENABLED=ON -DMPI_ENABLED=ON -DBUILD_ACM=ON -DCPU_BUILD=ON $AC_HOME && make -j
+cmake -B build -S  -DRUNTIME_COMPILATION=ON -DOPTIMIZE_INPUT_PARAMS=ON -DOPTIMIZE_MEM_ACCESSES=ON -DCMAKE_BUILD_TYPE=Release -DDSL_MODULE_DIR="$AC_HOME/acc-runtime/samples/mhd_modular" -DBUILD_UTILS=ON -DBUILD_SAMPLES=ON -DDOUBLE_PRECISION=ON -DMULTIGPU_ENABLED=ON -DMPI_ENABLED=ON -DBUILD_ACM=ON -DCPU_BUILD=ON && cmake --build build -j

test-builds/blur/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #
-mkdir -p build && cd build &&  cmake -DOPTIMIZE_MEM_ACCESSES=OFF -DMPI_ENABLED=ON -DBUILD_STANDALONE=OFF -DBUILD_MHD_SAMPLES=OFF -DPROGRAM_MODULE_DIR=${AC_HOME}/samples/blur -DDSL_MODULE_DIR=${AC_HOME}/acc-runtime/samples/blur $AC_HOME && make -j
+cmake -B build -S $AC_HOME -DOPTIMIZE_MEM_ACCESSES=OFF -DMPI_ENABLED=ON -DBUILD_STANDALONE=OFF -DBUILD_MHD_SAMPLES=OFF -DPROGRAM_MODULE_DIR=${AC_HOME}/samples/blur -DDSL_MODULE_DIR=${AC_HOME}/acc-runtime/samples/blur && cmake --build build -j

test-builds/cpu/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #
-mkdir -p build && cd build && cmake -DOPTIMIZE_MEM_ACCESSES=ON -DUSE_HIP=OFF -DCMAKE_BUILD_TYPE=Release -DDSL_MODULE_DIR="$AC_HOME/acc-runtime/samples/mhd_modular" -DBUILD_UTILS=ON -DBUILD_SAMPLES=ON -DDOUBLE_PRECISION=ON -DMULTIGPU_ENABLED=ON -DMPI_ENABLED=ON -DBUILD_ACM=ON -DCPU_BUILD=ON $AC_HOME && make -j
+cmake -B build -S $AC_HOME -DOPTIMIZE_MEM_ACCESSES=ON -DUSE_HIP=OFF -DCMAKE_BUILD_TYPE=Release -DDSL_MODULE_DIR="$AC_HOME/acc-runtime/samples/mhd_modular" -DBUILD_UTILS=ON -DBUILD_SAMPLES=ON -DDOUBLE_PRECISION=ON -DMULTIGPU_ENABLED=ON -DMPI_ENABLED=ON -DBUILD_ACM=ON -DCPU_BUILD=ON && cmake --build build -j

test-builds/debug/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mkdir -p build && cd build && cmake -DOPTIMIZE_MEM_ACCESSES=ON -DCMAKE_BUILD_TYPE=Debug -DBUILD_ACC_RUNTIME_LIBRARY=ON -DDSL_MODULE_DIR="$AC_HOME/acc-runtime/samples/mhd_modular" -DVERBOSE=ON -DBUILD_UTILS=ON -DBUILD_SAMPLES=ON -DDOUBLE_PRECISION=ON -DMULTIGPU_ENABLED=ON -DMPI_ENABLED=ON $AC_HOME && make -j
+cmake -B build -S $AC_HOME -DOPTIMIZE_MEM_ACCESSES=ON -DCMAKE_BUILD_TYPE=Debug -DBUILD_ACC_RUNTIME_LIBRARY=ON -DDSL_MODULE_DIR="$AC_HOME/acc-runtime/samples/mhd_modular" -DVERBOSE=ON -DBUILD_UTILS=ON -DBUILD_SAMPLES=ON -DDOUBLE_PRECISION=ON -DMULTIGPU_ENABLED=ON -DMPI_ENABLED=ON && cmake --build build -j

test-builds/double-precision/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #
-mkdir -p build && cd build && cmake -DOPTIMIZE_MEM_ACCESSES=ON -DUSE_HIP=OFF -DCMAKE_BUILD_TYPE=Release -DDSL_MODULE_DIR="$AC_HOME/acc-runtime/samples/mhd_modular" -DVERBOSE=ON -DBUILD_UTILS=ON -DBUILD_SAMPLES=ON -DDOUBLE_PRECISION=ON -DMULTIGPU_ENABLED=ON -DMPI_ENABLED=ON $AC_HOME && make -j
+cmake -B build -S $AC_HOME -DOPTIMIZE_MEM_ACCESSES=ON -DUSE_HIP=OFF -DCMAKE_BUILD_TYPE=Release -DDSL_MODULE_DIR="$AC_HOME/acc-runtime/samples/mhd_modular" -DVERBOSE=ON -DBUILD_UTILS=ON -DBUILD_SAMPLES=ON -DDOUBLE_PRECISION=ON -DMULTIGPU_ENABLED=ON -DMPI_ENABLED=ON && cmake --build build -j

test-builds/single-precision/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #
-mkdir -p build && cd build && cmake -DOPTIMIZE_MEM_ACCESSES=ON -DUSE_HIP=OFF -DBUILD_UTILS=ON -DBUILD_SAMPLES=ON -DDOUBLE_PRECISION=OFF -DMULTIGPU_ENABLED=ON -DMPI_ENABLED=OFF $AC_HOME && make -j
+cmake -B build -S $AC_HOME -DOPTIMIZE_MEM_ACCESSES=ON -DUSE_HIP=OFF -DBUILD_UTILS=ON -DBUILD_SAMPLES=ON -DDOUBLE_PRECISION=OFF -DMULTIGPU_ENABLED=ON -DMPI_ENABLED=OFF && cmake --build build -j

test/1d-test/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #
-mkdir -p build && cd build && cmake -DCMAKE_BUILD_TYPE=Debug -DMPI_ENABLED=ON -DOPTIMIZE_MEM_ACCESSES=ON -DRUNTIME_COMPILATION=on -DBUILD_TESTS=ON -DDSL_MODULE_DIR=../DSL $AC_HOME && make 1d-test -j
+cmake -B build -S $AC_HOME -DCMAKE_BUILD_TYPE=Debug -DMPI_ENABLED=ON -DOPTIMIZE_MEM_ACCESSES=ON -DRUNTIME_COMPILATION=on -DBUILD_TESTS=ON -DDSL_MODULE_DIR=../DSL && cmake --build build -t 1d-test -j

test/2d-test/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #
-mkdir -p build && cd build && cmake -DCMAKE_BUILD_TYPE=Debug -DMPI_ENABLED=ON -DOPTIMIZE_MEM_ACCESSES=ON -D2D=ON -DBUILD_TESTS=ON -DDSL_MODULE_DIR=../DSL $AC_HOME && make 2d-test -j
+cmake -B build -S $AC_HOME -DCMAKE_BUILD_TYPE=Debug -DMPI_ENABLED=ON -DOPTIMIZE_MEM_ACCESSES=ON -D2D=ON -DBUILD_TESTS=ON -DDSL_MODULE_DIR=../DSL && cmake --build build -t 2d-test -j

commit 767e65e459b520d5288eb7256e8813496b7b7e6a

test-builds: Add missing environment variable
Fallout from e254afb48937aa43150a5bc1f32a169eb3db5a0c

test-builds/runtime-compilation/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #
-cmake -B build -S  -DRUNTIME_COMPILATION=ON -DOPTIMIZE_INPUT_PARAMS=ON -DOPTIMIZE_MEM_ACCESSES=ON -DCMAKE_BUILD_TYPE=Release -DDSL_MODULE_DIR="$AC_HOME/acc-runtime/samples/mhd_modular" -DBUILD_UTILS=ON -DBUILD_SAMPLES=ON -DDOUBLE_PRECISION=ON -DMULTIGPU_ENABLED=ON -DMPI_ENABLED=ON -DBUILD_ACM=ON -DCPU_BUILD=ON && cmake --build build -j
+cmake -B build -S $AC_HOME -DRUNTIME_COMPILATION=ON -DOPTIMIZE_INPUT_PARAMS=ON -DOPTIMIZE_MEM_ACCESSES=ON -DCMAKE_BUILD_TYPE=Release -DDSL_MODULE_DIR="$AC_HOME/acc-runtime/samples/mhd_modular" -DBUILD_UTILS=ON -DBUILD_SAMPLES=ON -DDOUBLE_PRECISION=ON -DMULTIGPU_ENABLED=ON -DMPI_ENABLED=ON -DBUILD_ACM=ON -DCPU_BUILD=ON && cmake --build build -j