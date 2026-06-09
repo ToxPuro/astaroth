a4169ebbb45b1f9ba92a73f937c6beb79af73aa9
Miikka Väisälä <mvaisala@pm.me>
2026-06-09 20:53:59 +0300

    Setting some envioronmental variables on the run script.

--- config/samples/subsonic_forced_turbulence/run_on_fedora44.sh
+++ config/samples/subsonic_forced_turbulence/run_on_fedora44.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+export CC=/home/kayttaja/gcc15/bin/gcc
+export CXX=/home/kayttaja/gcc15/bin/g++
+export CUDAHOSTCXX=/home/kayttaja/gcc15/bin/g++
+
 ( mpirun -np 1 ./ac_run_mpi --config astaroth.conf --run-init-kernel randomize >& output.log & )
 
 #mpirun -np 1 ./ac_run_mpi --config astaroth.conf --run-init-kernel randomize 
