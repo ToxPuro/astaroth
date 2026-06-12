433c05e7bb648a424f209e5af651285612b6bc79
Ondřej Míchal <harrymichal@seznam.cz>
2026-06-12 14:18:01 +0300

    include: Actually use logging declarations in astaroth_logging.h

--- include/astaroth.h
+++ include/astaroth.h
@@ -92,18 +92,7 @@ FUNC_DEFINE(Node, acGetNode,(void));
  * =============================================================================
  */
 
-/* Log a message with a timestamp from the root proc (if pid == 0) */
-void acLogFromRootProc(const int pid, const char* msg, ...);
-void acVA_LogFromRootProc(const int pid, const char* msg, va_list args);
-
-/* Log a message with a timestamp from the root proc (if pid == 0) if the build flag VERBOSE is on
- */
-void acVerboseLogFromRootProc(const int pid, const char* msg, ...);
-void acVA_VerboseLogFromRootProc(const int pid, const char* msg, va_list args);
-
-/* Log a message with a timestamp from the root proc (if pid == 0) in a debug build */
-void acDebugFromRootProc(const int pid, const char* msg, ...);
-void acVA_DebugFromRootProc(const int pid, const char* msg, va_list arg);
+#include "astaroth_logging.h"
 
 int3
 acDecompose(const uint64_t target, const AcMeshInfo info);
