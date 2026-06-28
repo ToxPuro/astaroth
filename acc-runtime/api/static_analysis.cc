ToxPuro/astaroth

commit 520c1198b42f487f3eab688fd8118bc74127e02e

Set suffix of generated headers depending on context to .inc
Header files can be managed in many different ways. One way I found the
most helpful in large projects is when .h header files are treated as
self-contained files that pull in all other header files they might need
- they do not depend on the context in which they are included. All
other headers files should be renamed to .inc to signify that they are
meant to be included in-line - they do depend on the context in which
they are included.

Treating the headers according to these guidelines will make it simpler
to notice that the rules for including them are not the same as when
including other headers. Future refactorings will also be a little
easier when one has to second guess every change as much.

This is also the recommended practice as documented in the Google C++
Style Guide [0] which is the style one is supposed to use in Astaroth
(see CONTRIBUTING.md).

[0] https://google.github.io/styleguide/cppguide.html#Self_contained_Headers

src/core/compute_steps.cc
@@ -27,8 +27,8 @@ get_info()
 
 
 #include "taskgraph_kernels.h"
-#include "taskgraph_bc_handles.h"
-#include "taskgraph_kernel_bcs.h"
+#include "taskgraph_bc_handles.inc"
+#include "taskgraph_kernel_bcs.inc"
 
 static int ac_pid()
 {
@@ -280,7 +280,7 @@ bc_output_fields_overlap(const std::vector<BoundCond>& bcs,const std::vector<Ker
 	return false;
 }
 
-#include "user_loader_impls.h"
+#include "user_loader_impls.inc"
 
 void
 AC_INTERNAL_empty_loader(ParamLoadingInfo){}
@@ -289,7 +289,7 @@ typedef void (*loader_ptr)(ParamLoadingInfo);
 static loader_ptr
 get_loader(const int graph, const int call_index)
 {
-	#include "user_loaders.h"
+	#include "user_loaders.inc"
 	ERRCHK_ALWAYS(call_index < (int)DSLTaskGraphKernelLoaders[graph].size());
 	return  DSLTaskGraphKernelLoaders[graph][call_index];
 }
@@ -298,7 +298,7 @@ get_loader(const int graph, const int call_index)
 void
 call_all_user_loaders(ParamLoadingInfo p)
 {
-	#include "call_all_user_loaders.h"
+	#include "call_all_user_loaders.inc"
 	AC_INTERNAL_empty_loader(p);
 }
 **/
@@ -1783,7 +1783,7 @@ acGetDSLTaskGraph(const AcDSLTaskGraph graph)
 			to_volume(get_info()[AC_nmin]),
 			to_volume(get_info()[AC_nlocal_max]));
 }
-#include "user_constants.h"
+#include "user_constants.inc"
 static AcTaskDefinition
 gen_taskgraph_kernel_entry(const KernelCall call, int onion_level, FILE* stream, const KernelAnalysisInfo info)
 {

commit 68543d709c2f74c9f2f0b70da704ed297d7e9a41

Set suffix of generated headers depending on context to .inc (Part 2)
Fallout from 520c1198b42f487f3eab688fd8118bc74127e02e

acc-runtime/api/static_analysis.cc
@@ -1,6 +1,7 @@
 #include "acc_runtime.h"
-#include "stencil_accesses.h"
 #include "static_analysis.h"
+
+#include "stencil_accesses.inc"
 bool
 is_raytracing_kernel(const AcKernel kernel)
 {

src/core/compute_steps.cc
@@ -26,7 +26,7 @@ get_info()
 
 
 
-#include "taskgraph_kernels.h"
+#include "taskgraph_kernels.inc"
 #include "taskgraph_bc_handles.inc"
 #include "taskgraph_kernel_bcs.inc"
 