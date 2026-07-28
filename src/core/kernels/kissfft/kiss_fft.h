ToxPuro/astaroth

commit cdd997d8e03f02dfa08b317cffea9a6a4d33b8f9

include: Add macro for marking groups of C declarations
Why write more characters when we can write less and make it a little
more clear what does that section of code stand for. Replaces all cases
of 'extern "C" { ... }' blocks with the new macros.

This draws inspiration from a popular C library GLib [0] which uses the
same type of a macro for this.

[0] https://gitlab.gnome.org/GNOME/glib/-/blob/94d297fe6bd1348f0d761b8adf4634663620091b/glib/gmacros.h#L942-949

src/core/kernels/kissfft/kiss_fft.h
@@ -29,9 +29,7 @@
 # define KISS_FFT_API
 #endif
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+AC_BEGIN_C_DECLARATIONS
 
 /*
  ATTENTION!
@@ -153,8 +151,6 @@ int KISS_FFT_API kiss_fft_next_fast_size(int n);
 #define kiss_fftr_next_fast_size_real(n) \
         (kiss_fft_next_fast_size( ((n)+1)>>1)<<1)
 
-#ifdef __cplusplus
-} 
-#endif
+AC_END_C_DECLARATIONS
 
 #endif

src/core/kernels/kissfft/kiss_fftnd.h
@@ -11,16 +11,13 @@
 
 #include "kiss_fft.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+AC_BEGIN_C_DECLARATIONS
 
 typedef struct kiss_fftnd_state * kiss_fftnd_cfg;
     
 kiss_fftnd_cfg KISS_FFT_API kiss_fftnd_alloc(const int *dims,int ndims,int inverse_fft,void*mem,size_t*lenmem);
 void KISS_FFT_API kiss_fftnd(kiss_fftnd_cfg  cfg,const kiss_fft_cpx *fin,kiss_fft_cpx *fout);
 
-#ifdef __cplusplus
-}
-#endif
+AC_END_C_DECLARATIONS
+
 #endif

acc-runtime/acc/CMakeLists.txt
@@ -35,6 +35,7 @@ else()
 	find_package(BISON 3.0 REQUIRED)
 	find_package(FLEX)
 	add_library(tinyexpr STATIC tinyexpr.c)
+	target_include_directories(tinyexpr PUBLIC ${AC_BASE_PATH}/include)
 	target_link_libraries(tinyexpr PUBLIC m)
 	
 	bison_target(parser ac.y ${PROJECT_BINARY_DIR}/tab.c VERBOSE)# COMPILE_FLAGS -Wcounterexamples) # Counterexamples need relatively new Bison (at least 3.7 works)

acc-runtime/acc/hashtable.h
@@ -42,6 +42,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "func_define.h"
+
 #if (defined(_MSC_VER) && defined(__AVX__)) ||                                 \
     (!defined(_MSC_VER) && defined(__SSE4_2__))
 #define HASHMAP_X86_SSE42
@@ -155,9 +157,7 @@ typedef struct hashmap_create_options_s {
   hashmap_uint32_t _;
 } hashmap_create_options_t;
 
-#if defined(__cplusplus)
-extern "C" {
-#endif
+AC_BEGIN_C_DECLARATIONS
 
 /// @brief Create a hashmap.
 /// @param initial_capacity The initial capacity of the hashmap.
@@ -280,9 +280,7 @@ HASHMAP_WEAK int hashmap_rehash_iterator(void *const new_hash,
 HASHMAP_ALWAYS_INLINE int hashmap_rehash_helper(struct hashmap_s *const m);
 HASHMAP_ALWAYS_INLINE hashmap_uint32_t hashmap_clz(const hashmap_uint32_t x);
 
-#if defined(__cplusplus)
-}
-#endif
+AC_END_C_DECLARATIONS
 
 #if defined(__cplusplus)
 #define HASHMAP_CAST(type, x) static_cast<type>(x)

acc-runtime/acc/tinyexpr.h
@@ -26,12 +26,9 @@
 #ifndef TINYEXPR_H
 #define TINYEXPR_H
 
+#include "func_define.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-
+AC_BEGIN_C_DECLARATIONS
 
 typedef struct te_expr {
     int type;
@@ -83,9 +80,6 @@ void te_print(const te_expr *n);
 /* This is safe to call on NULL pointers. */
 void te_free(te_expr *n);
 
-
-#ifdef __cplusplus
-}
-#endif
+AC_END_C_DECLARATIONS
 
 #endif /*TINYEXPR_H*/

acc-runtime/api/acc_runtime.h
@@ -271,9 +271,8 @@ acGetFieldHalos(const AcMeshInfo info, const VertexBufferHandle vtxbuf)
 }
 
 typedef AcAutotuneMeasurement (*AcMeasurementGatherFunc)(const AcAutotuneMeasurement);
-  #ifdef __cplusplus
-  extern "C" {
-  #endif
+
+AC_BEGIN_C_DECLARATIONS
 
 #if AC_MPI_ENABLED
    FUNC_DEFINE(AcResult, acInitializeRuntimeMPI,(const int grid_pid, const int nprocs, AcMeasurementGatherFunc));
@@ -399,9 +398,9 @@ typedef AcAutotuneMeasurement (*AcMeasurementGatherFunc)(const AcAutotuneMeasure
   }
 #endif
 
-  #ifdef __cplusplus
-  } // extern "C"
-    //
+AC_END_C_DECLARATIONS
+
+#ifdef __cplusplus
 #if AC_RUNTIME_COMPILATION
 #else
 AcResult

acc-runtime/api/common_kernels.h
@@ -1,13 +1,14 @@
 #pragma once
+
 #include <stddef.h>
 
 #include "acreal.h"
 #include "device_headers.h"
+#include "func_define.h"
 #include "host_datatypes.h"
-#ifdef __cplusplus
-extern "C"
-{
-#endif
+
+AC_BEGIN_C_DECLARATIONS
+
 AcResult acMultiplyInplace(const AcReal value, const size_t count,
                            AcReal* array);
 AcResult
@@ -59,6 +60,4 @@ AcResult acKernelVolumeCopyComplexToReal(const cudaStream_t stream,             
                    const AcComplex* in, const Volume in_offset, const Volume in_volume, const Volume embedded_in_volume,//
                    AcReal* out,const Volume out_offset, const Volume out_volume, const Volume embedded_out_volume);
 
-#ifdef __cplusplus
-}
-#endif
+AC_END_C_DECLARATIONS

acc-runtime/api/static_analysis.h
@@ -2,10 +2,10 @@
 
 #include "builtin_enums.h"
 #include "datatypes.h"
-#ifdef __cplusplus
-extern "C"
-{
-#endif
+#include "func_define.h"
+
+AC_BEGIN_C_DECLARATIONS
+
 bool
 is_raytracing_kernel(const AcKernel kernel);
 int
@@ -20,6 +20,5 @@ bool
 is_coop_raytracing_kernel(const AcKernel kernel);
 bool
 profile_is_reduced(const Profile profile);
-#ifdef __cplusplus
-}
-#endif
+
+AC_END_C_DECLARATIONS

commit da78f602a842bd6f69e67f058f4009b87eb61685

src/core/kernels/kissfft: Add missing #include "func_define.h"
Fallout from commit cdd997d8e03f02dfa08b317cffea9a6a4d33b8f9.

src/core/kernels/kissfft/kiss_fft.h
@@ -14,6 +14,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "func_define.h"
+
 // Define KISS_FFT_SHARED macro to properly export symbols
 #ifdef KISS_FFT_SHARED
 # ifdef _WIN32

src/core/kernels/kissfft/kiss_fftnd.h
@@ -9,6 +9,7 @@
 #ifndef KISS_FFTND_H
 #define KISS_FFTND_H
 
+#include "func_define.h"
 #include "kiss_fft.h"
 
 AC_BEGIN_C_DECLARATIONS