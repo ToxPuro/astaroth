8c17f64ca19b47b547c229f9ba4d47158fb2aa47
ToxPuro <ToxPuro@users.noreply.github.com>
2026-08-07 09:56:45 +0000

    deploy: c348e71b084dfaa2033856f6c61e3c72206a3c4f

--- search/defines_4.js
+++ search/defines_4.js
@@ -1,4 +1,7 @@
 var searchData=
 [
-  ['device_5finline_0',['DEVICE_INLINE',['../acc__runtime_8h.html#a8888b6e919f4a14975d3110a7425407d',1,'acc_runtime.h']]]
+  ['decl_5fdevice_5fstore_5farray_0',['DECL_DEVICE_STORE_ARRAY',['../astaroth__device_8h.html#a4c5077b9a384ec91707d9dad7c0d9859',1,'astaroth_device.h']]],
+  ['decl_5fdevice_5fstore_5funiform_1',['DECL_DEVICE_STORE_UNIFORM',['../astaroth__device_8h.html#a751b533fee6217d97b3f6aa66f4b0a89',1,'astaroth_device.h']]],
+  ['device_5finline_2',['DEVICE_INLINE',['../acc__runtime_8h.html#a8888b6e919f4a14975d3110a7425407d',1,'acc_runtime.h']]],
+  ['device_5fload_5farray_5fdecl_3',['DEVICE_LOAD_ARRAY_DECL',['../astaroth__device_8h.html#a86108a816a52c8bd2bca4823fd84e179',1,'astaroth_device.h']]]
 ];
