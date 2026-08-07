b8e449c599b270e0245208d1f70ada971541d0e3
Ondřej Míchal <harrymichal@seznam.cz>
2026-08-07 11:40:49 +0300

    test/installation-test: Check for the presence of sudo
    Because the NVIDIA CUDA Ubuntu image used in our CI uses root as the
    default user they decided not to include sudo in the image to save
    space. Because of that we need to check for the presence of the command
    before using it.
    
    Also, the error in the CI was hard to catch because by default shell
    scripts do not exit when a command fails. It could be good to start
    using the 'set -xeuo pipefail' "strict mode" [0].
    
    [0] http://redsymbol.net/articles/unofficial-bash-strict-mode/

--- test/installation-test/build.sh
+++ test/installation-test/build.sh
@@ -1,9 +1,16 @@
-cmake -B ac_build -S $AC_HOME -DMPI_ENABLED=ON -DAC_STENCIL_ORDER=6 -DFFT_ENABLED=OFF -DOPTIMIZE_MEM_ACCESSES=ON -DBUILD_SAMPLES=OFF -DBUILD_STANDALONE=OFF -DBUILD_TESTS=OFF -DRUNTIME_COMPILATION=OFF -DDSL_MODULE_DIR=$AC_HOME/test/sor-test/DSL 
+#!/bin/bash
+
+# Taken from http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -xeuo pipefail
+
+SUDO_CMD="sudo"
+if ! command -v ${SUDO_CMD}; then
+    SUDO_CMD=""
+fi
+
+cmake -B ac_build -S $AC_HOME -DMPI_ENABLED=ON -DAC_STENCIL_ORDER=6 -DFFT_ENABLED=OFF -DOPTIMIZE_MEM_ACCESSES=ON -DBUILD_SAMPLES=OFF -DBUILD_STANDALONE=OFF -DBUILD_TESTS=OFF -DRUNTIME_COMPILATION=OFF -DDSL_MODULE_DIR=$AC_HOME/test/sor-test/DSL
 cmake --build ac_build --parallel
-sudo cmake --install ac_build
+${SUDO_CMD} cmake --install ac_build
 
 cmake -B sor-build -S .
 cmake --build sor-build
-
-
-
