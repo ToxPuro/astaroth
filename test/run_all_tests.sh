62579a512bf1e5bbd34a3b58379002c7142b9856
ToxPuro <touko.puro@aalto.fi>
2026-08-13 05:49:35 +0300

    make sure Astaroth is sourced when running all tests

--- test/run_all_tests.sh
+++ test/run_all_tests.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 cpu_only=OFF
+parallel=--parallel
+if [[ -v AC_HOME ]]; then
 while [[ $# -gt 0 ]]; do
     case "$1" in
         --cpu)
             cpu_only=ON
+            shift
+            ;;
+        --serial)
+            parallel=
             shift
             ;;
         *)
@@ -17,13 +23,19 @@ done
 export OMPI_ALLOW_RUN_AS_ROOT=1
 export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1
 if [[ "$cpu_only" == "OFF" ]]; then
-    cd $AC_HOME/test-builds && ac_build_tests --parallel
+    cd $AC_HOME/test-builds && ac_build_tests $parallel
     cd $AC_HOME/test-builds && ac_run_tests
     cd $AC_HOME/test && ac_build_tests 
 else
-    cd "$AC_HOME/test-builds" && ac_build_tests --cpu --parallel
+    cd "$AC_HOME/test-builds" && ac_build_tests --cpu $parallel
     cd "$AC_HOME/test-builds" && ac_run_tests
 fi
 cd $AC_HOME/test && ac_build_tests --cpu
 cd $AC_HOME/test && ac_run_tests
+
+else
+    echo "source Astaroth to run the auto-test!"
+    echo "You can source Astaroth by running source sourceme.sh from the root of the repo."
+    exit 1
+fi
 
