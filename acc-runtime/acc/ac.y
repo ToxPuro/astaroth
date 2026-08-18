ToxPuro/astaroth

commit 6f3c4207707aa83364a89698168aed90bdaf3d90

set soft limit of stack space to the hard limit

acc-runtime/acc/ac.y
@@ -842,6 +842,13 @@ main(int argc, char** argv)
         puts("Usage: ./acc [source file]");
         return EXIT_FAILURE;
     }
+
+    //Ask for maximum stack space
+    struct rlimit rlim;
+    getrlimit(RLIMIT_STACK, &rlim);
+    rlim.rlim_cur = rlim.rlim_max;
+    setrlimit(RLIMIT_STACK, &rlim);
+
     atexit(&cleanup);
     char* file = NULL;
     ACC_OVERRIDES_PATH      = argc == 2 ? NULL  : strip_whitespace_new(argv[argc-1]);

commit 7733b90f1570bd9e6a281555d10e33ae42c92db7

acc-runtime/acc: Add missing include sys/resource.h
Needed to get the definition of 'struct rlimit'.

Fallout from commit 6f3c4207707aa83364a89698168aed90bdaf3d90.

acc-runtime/acc/ac.y
@@ -8,6 +8,7 @@
 #include <math.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/resource.h>
 #include <sys/stat.h>
 #include <wordexp.h>
 