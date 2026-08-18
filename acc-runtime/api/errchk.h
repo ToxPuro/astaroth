05c8e2c85ae011b4027e536ee6c19a8cf00bbe6c
ToxPuro <touko.puro@aalto.fi>
2026-08-18 13:29:47 +0300

    make ERRCHK and WARNCHK work the expected way, i.e. the same way ERRCHK_CUDA works

--- acc-runtime/api/errchk.h
+++ acc-runtime/api/errchk.h
@@ -132,8 +132,8 @@ cuda_assert(cudaError_t code, const char* file, int line, bool should_abort)
 #ifdef NDEBUG
 #undef ERRCHK
 #undef WARNCHK
-#define ERRCHK(params)
-#define WARNCHK(params)
+#define ERRCHK(params) (void)params
+#define WARNCHK(params) (void)params
 #define ERRCHK_CUDA(params) (void)params
 #define WARNCHK_CUDA(params) params
 #define ERRCHK_CUDA_KERNEL()                                                   \
