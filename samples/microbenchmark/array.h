ToxPuro/astaroth

commit 5650ae169da6161db7181fa4e4317f8e8c74d022

use only rng sources we explicitly control to be cross-portable

samples/microbenchmark/array.h
@@ -56,7 +56,7 @@ arrayDestroy(Array* a)
 static real
 randd(void)
 {
-    return (real)rand() / RAND_MAX;
+   acRand();
 }
 
 static void

acc-runtime/api/math_utils.h
@@ -324,11 +324,6 @@ mod(const int a, const int b)
   return ((a%b) + b) % b;
 }
 
-static inline AcReal
-randr()
-{
-  return AcReal(rand()) / AcReal(RAND_MAX);
-}
 
 static inline bool
 is_power_of_two(const unsigned val)

acc-runtime/api/random.cuh
@@ -15,11 +15,15 @@
 #endif
 
 #if AC_CPU_BUILD
+#include <random>
 __device__ __forceinline__ AcReal
 rand_uniform()
 {
-	return (AcReal)(rand() / (RAND_MAX + 1.));
+	//rng returns an int from [0,2^32-1] so dividing by 2^32 gives a real in [0,1)
+        static std::mt19937 rng(321654987);
+	return static_cast<AcReal>(rng()) / AcReal(4294967296.0);
 }
+
 AcResult
 acRandInitAlt(const uint64_t, const size_t, const size_t)
 {

include/astaroth_helpers.h
@@ -52,6 +52,10 @@ FUNC_DEFINE(Volume, acGetMaxNN, (const AcMeshInfo info));
 FUNC_DEFINE(Volume, acGetGridMaxNN, (const AcMeshInfo info));
 FUNC_DEFINE(AcReal3, acGetLengths, (const AcMeshInfo info));
 
+AcResult acSrand(const size_t seed);
+AcReal   acRand();
+int      acRandInt(const int min, const int max);
+
 static inline size_t
 acVertexBufferSize(const AcMeshInfo info)
 {

samples/benchmark-device/main.cc
@@ -88,7 +88,7 @@ main(int argc, char** argv)
     const size_t pid   = 0;
     const size_t count = acVertexBufferCompdomainSize(info);
     acRandInitAlt(seed, count, pid);
-    srand(seed);
+    acSrand(seed);
 
     // Verify
     if (verify) {

samples/devicetest/main.cc
@@ -35,7 +35,7 @@ main(int argc, char* argv[])
     const int pid = 0;
 
     // Set random seed for reproducibility
-    srand(321654987);
+    acSrand(321654987);
 
     // CPU alloc
     AcMeshInfo info;
@@ -114,7 +114,7 @@ main(int argc, char* argv[])
             WARNCHK_ALWAYS(retval);
         }
 
-        srand(123567);
+        acSrand(123567);
         acHostMeshRandomize(&model);
         // acHostMeshSet((AcReal)1.0, &model);
         acHostMeshApplyPeriodicBounds(&model);

samples/heat-equation/main.c
@@ -218,7 +218,7 @@ main(int argc, char** argv)
     const size_t pid   = 0;
     const size_t count = acVertexBufferCompdomainSize(info);
     acRandInitAlt(seed, count, pid);
-    srand(seed);
+    acSrand(seed);
 
     // Benchmark configuration
     load_stencil_coefficients(device, dims, radius);

samples/integrator/main.cc
@@ -47,11 +47,6 @@ acAbort(void)
     if (!finalized)
         MPI_Abort(acGridMPIComm(), EXIT_FAILURE);
 }
-double
-drand()
-{
-	return (double)(rand()) / (double)(rand());
-}
 
 void
 read_data_to_arr(AcMeshInfo& info, const AcRealArrayParam arr, const char* filename, const AcIntParam N_param)
@@ -512,7 +507,7 @@ void integrate(
     MPI_Comm_rank(MPI_COMM_WORLD, &pid);
 
     // Set random seed for reproducibility
-    srand(321654987);
+    acSrand(321654987);
     AcMeshInfo info = acInitInfo();
     populate_config(config, info);
 
@@ -548,7 +543,7 @@ main(int argc, char* argv[])
     MPI_Comm_rank(MPI_COMM_WORLD, &pid);
 
     // Set random seed for reproducibility
-    srand(321654987);
+    acSrand(321654987);
 
 
     // CPU alloc

commit 318b97896fd944ca22ff64987b935524df946cd7

samples/microbenchmark: Add missing return
Fallout from 5650ae169da6161db7181fa4e4317f8e8c74d022.

samples/microbenchmark/array.h
@@ -57,7 +57,7 @@ arrayDestroy(Array* a)
 static real
 randd(void)
 {
-   acRand();
+   return acRand();
 }
 
 static void